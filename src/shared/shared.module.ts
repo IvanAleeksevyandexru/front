e282d42359338b444ba05e1d82cc0c7999070f7b
taliev <taliev@luxoft.com>
2020-07-28 17:44:48 +0300

    [EPGU-249] - create component container; - adaptive exist layout;

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNameComponent } from './components/page-name/page-name.component';
+import { ScreenContainerComponent } from './components/screen-container/screen-container.component';
 
 const COMPONENTS = [
   PageNameComponent,
+  ScreenContainerComponent
 ]
 
 @NgModule({
