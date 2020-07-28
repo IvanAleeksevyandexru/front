8b6eff20304b585829561ee64e3d4cd4507d19d7
taliev <taliev@luxoft.com>
2020-07-28 17:20:19 +0300

    [EPGU-254] - create new screen confirm-personal-user-email

---
+++ src/constructor/modules/confirm-personal-user-email/confirm-personal-user-email/confirm-personal-user-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-personal-user-email',
+  templateUrl: './confirm-personal-user-email.component.html',
+  styleUrls: ['./confirm-personal-user-email.component.scss']
+})
+export class ConfirmPersonalUserEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
