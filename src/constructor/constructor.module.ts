b743c78548a6196d7fbe9a90e441019aab1ff443
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-07-28 16:53:45 +0300

    feat: add view and styles of component requirements-list [ref feature/EPGU-252]

--- src/constructor/constructor.module.ts
+++ src/constructor/constructor.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WelcomeScreenComponent} from './components/welcome-screen/welcome-screen.component';
+import {RequirementsListComponent} from './components/requirements-list/requirements-list.component';
 import {EpguLibModule} from 'epgu-lib';
 import {ConstructorComponent} from './constructor.component';
 
 const COMPONENTS = [
   WelcomeScreenComponent,
+  RequirementsListComponent,
   ConstructorComponent
 ]
 
