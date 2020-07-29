ceaf76fdb1da361c571b627c04b5d39567c4c843
Andrei Arkhipov <andrei@awb.pw>
2020-07-29 17:06:48 +0300

    http client

--- src/services/epgu.service.ts
+++ src/services/epgu.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpguService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getData(): Observable<any> {
+    return this.http.get('http://188.93.23.27:8080/getService', {
+      withCredentials: true
+    });
+  }
 }
