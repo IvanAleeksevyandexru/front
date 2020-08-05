8466fbe61a8a0d7bae1bdedcdf443f659c516877
Matvii Babych <mbabych@luxoft.com>
2020-08-05 12:07:42 +0300

    EPGUCORE-35033: Fixed 'PersonLookup' according to JSON

--- projects/epgu-constructor/src/app/modules/screen/components/add-passport/add-passport-screen.component.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/add-passport/add-passport-screen.component.ts
@@ -1,36 +1,56 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 /* eslint-disable import/no-extraneous-dependencies */
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { EgpuResponseDisplayInterface } from '../../../../../interfaces/epgu.service.interface';
-import { ScreenComponentService } from '../../service/screen-component/screen-component.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-passport-screen',
   templateUrl: './add-passport-screen.component.html',
   styleUrls: ['./add-passport-screen.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddPassportScreenComponent implements OnInit {
-  @Input() data: EgpuResponseDisplayInterface;
+export class AddPassportScreenComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe$: Subject<any> = new Subject<any>();
+
+  @Input() data: any;
   @Input() header: string;
   @Input() submitLabel: string;
-  @Output() nextStepEvent = new EventEmitter();
+  @Output() valueChangedEvent = new EventEmitter();
 
   passportForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private screenComponentService: ScreenComponentService) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.passportForm = this.fb.group({
-      series: this.fb.control({}),
-      number: this.fb.control({}),
+    const controls = {};
+
+    this.data.attrs.fields.forEach((field) => {
+      controls[field.fieldName] = this.fb.control(null, [Validators.required]);
     });
+
+    this.passportForm = this.fb.group(controls);
+    this.passportForm.valueChanges
+      .pipe(takeUntil(this.ngUnsubscribe$), debounceTime(300))
+      .subscribe((value) => {
+        if (!this.passportForm.valid) {
+          return;
+        }
+
+        this.valueChangedEvent.emit(value);
+      });
   }
 
-  nextStep() {
-    if (!this.passportForm.valid) {
-      return;
-    }
-
-    this.nextStepEvent.emit(this.passportForm.value);
+  ngOnDestroy() {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
   }
 }
