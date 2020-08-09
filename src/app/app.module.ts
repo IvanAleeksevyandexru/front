0adb59d4283ea41e07c4a472efb0b16ed62b65ec
taliev <taliev@luxoft.com>
2020-08-10 00:15:17 +0300

    [EPGUCORE-35171] - remove routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ConstructorModule} from '../constructor/constructor.module';
 import {EpguLibCommonModule} from 'epgu-lib';
 import { DirectivesModule } from './directives/directives.module';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: ConstructorModule},
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +19,10 @@ import { DirectivesModule } from './directives/directives.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     ConstructorModule,
     EpguLibCommonModule,
-    DirectivesModule
+    DirectivesModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
