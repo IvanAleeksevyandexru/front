98e9d8c244227a136781f1ea17046e1825cfdeb6
taliev <taliev@luxoft.com>
2020-08-14 16:22:00 +0300

    [EPGUCORE-35319] - add dadata to temporary-registration-addr.component

--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/temporary-registration-addr/components/temporary-registration-addr/temporary-registration-addr.component.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/temporary-registration-addr/components/temporary-registration-addr/temporary-registration-addr.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EgpuResponseTemporaryRegistrationAddrDisplayComponentInterface } from '../../../../../../../../../interfaces/temporary-registration-addr.interface';
+import { ConstructorConfigService } from '../../../../../../../../services/config/constructor-config.service';
 
 @Component({
   selector: 'app-temporary-registration-addr',
@@ -10,6 +11,8 @@ export class TemporaryRegistrationAddrComponent {
   date = new Date();
   forms = {};
   @Input() data: EgpuResponseTemporaryRegistrationAddrDisplayComponentInterface;
+
+  constructor(public constructorConfigService: ConstructorConfigService) {}
 
   hintClick(timestamp: number) {
     this.date = new Date(new Date().getTime() + timestamp);
