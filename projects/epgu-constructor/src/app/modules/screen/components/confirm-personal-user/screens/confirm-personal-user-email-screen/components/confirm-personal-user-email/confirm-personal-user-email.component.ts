fbd1a09af220ae1c86d6baca9ec03a8ba077acfe
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-08-19 17:46:16 +0300

    [EPGUCORE-35699]: add editable logic and validation to confirm-user-email and confirm-user-phone components

--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-email-screen/components/confirm-personal-user-email/confirm-personal-user-email.component.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-email-screen/components/confirm-personal-user-email/confirm-personal-user-email.component.ts
@@ -1,13 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidationShowOn } from 'epgu-lib';
+import { takeUntil } from 'rxjs/operators';
+import { UnsubscribeService } from '../../../../../../../../services/unsubscribe/unsubscribe.service';
 
 @Component({
   selector: 'app-confirm-personal-user-email',
   templateUrl: './confirm-personal-user-email.component.html',
   styleUrls: ['./confirm-personal-user-email.component.scss'],
+  providers: [UnsubscribeService],
 })
 export class ConfirmPersonalUserEmailComponent implements OnInit {
-  @Input() name: string;
-  @Input() content: string;
+  @Input() label: string;
+  @Input() data: string;
+  @Input() isEditable: boolean;
+  @Output() dataChanged = new EventEmitter();
 
-  ngOnInit(): void {}
+  emailForm: FormGroup;
+  validationShowOn = ValidationShowOn.TOUCHED_UNFOCUSED;
+
+  constructor(private formBuilder: FormBuilder, private ngUnsubscribe$: UnsubscribeService) {}
+
+  ngOnInit(): void {
+    this.emailForm = this.formBuilder.group({
+      email: this.formBuilder.control(
+        {
+          value: this.data || '',
+          disabled: !this.isEditable,
+        },
+        {
+          validators: Validators.compose([Validators.required, Validators.email]),
+        },
+      ),
+    });
+
+    this.emailForm.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((change) => {
+      this.dataChanged.emit(change);
+    });
+  }
+
+  ngOnChanges(): void {
+    if (this.emailForm) {
+      if (this.isEditable) {
+        this.emailForm.controls.email.enable();
+      } else {
+        this.emailForm.controls.email.disable();
+      }
+    }
+  }
 }
--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-phone-screen/components/confirm-personal-user-phone/confirm-personal-user-phone.component.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-phone-screen/components/confirm-personal-user-phone/confirm-personal-user-phone.component.ts
@@ -1,13 +1,71 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidationShowOn } from 'epgu-lib';
+import { takeUntil } from 'rxjs/operators';
+import { UnsubscribeService } from '../../../../../../../../services/unsubscribe/unsubscribe.service';
 
 @Component({
   selector: 'app-confirm-personal-user-phone',
   templateUrl: './confirm-personal-user-phone.component.html',
   styleUrls: ['./confirm-personal-user-phone.component.scss'],
+  providers: [UnsubscribeService],
 })
-export class ConfirmPersonalUserPhoneComponent implements OnInit {
-  @Input() name: string;
-  @Input() content: string;
+export class ConfirmPersonalUserPhoneComponent implements OnInit, OnChanges {
+  @Input() label: string;
+  @Input() data: string;
+  @Input() isEditable: boolean;
+  @Output() dataChanged = new EventEmitter();
 
-  ngOnInit(): void {}
+  phoneForm: FormGroup;
+  validationShowOn = ValidationShowOn.TOUCHED_UNFOCUSED;
+  phoneMask = [
+    '+',
+    /[7]/,
+    ' ',
+    '(',
+    /[1-9]/,
+    /\d/,
+    /\d/,
+    ')',
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    '-',
+    /\d/,
+    /\d/,
+    '-',
+    /\d/,
+    /\d/,
+  ];
+
+  constructor(private formBuilder: FormBuilder, private ngUnsubscribe$: UnsubscribeService) {}
+
+  ngOnInit(): void {
+    this.phoneForm = this.formBuilder.group({
+      phone: this.formBuilder.control(
+        {
+          value: this.data || '',
+          disabled: !this.isEditable,
+        },
+        {
+          validators: Validators.compose([Validators.required, Validators.minLength(18)]),
+        },
+      ),
+    });
+
+    this.phoneForm.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((change) => {
+      this.dataChanged.emit(change);
+    });
+  }
+
+  ngOnChanges(): void {
+    if (this.phoneForm) {
+      if (this.isEditable) {
+        this.phoneForm.controls.phone.enable();
+      } else {
+        this.phoneForm.controls.phone.disable();
+      }
+    }
+  }
 }
