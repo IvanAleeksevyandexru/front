9ab1cb31a1853e20068d4326d62d20d549cb8552
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-19 19:59:38 +0600

    EPGUCORE-35265 fix rest service test

---
+++ projects/epgu-constructor/src/app/services/config/constructor-config.service.stub.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {ConstructorConfigInterface} from '../../../interfaces/constructor-config.interface';
+
+@Injectable()
+export class ConstructorConfigServiceStub {
+  config: ConstructorConfigInterface = {
+    apiUrl: '',
+    dictionaryUrl: '',
+    serviceId: '',
+    externalApiUrl: ''
+  };
+  constructor() {}
+}
--- projects/epgu-constructor/src/app/services/rest/rest.service.spec.ts
+++ projects/epgu-constructor/src/app/services/rest/rest.service.spec.ts
@@ -1,14 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import {async, TestBed} from '@angular/core/testing';
 
 import { RestService } from './rest.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {ConstructorConfigService} from '../config/constructor-config.service';
+import {ConstructorConfigServiceStub} from '../config/constructor-config.service.stub';
 
 describe('RestService', () => {
   let service: RestService;
+  let http: HttpTestingController;
+  let cnstrctrConfigSrv: ConstructorConfigService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        RestService,
+        { provide: ConstructorConfigService, useClass: ConstructorConfigServiceStub }
+      ]
+    });
     service = TestBed.inject(RestService);
-  });
+    http = TestBed.inject(HttpTestingController);
+    cnstrctrConfigSrv = TestBed.inject(ConstructorConfigService);
+  }));
+
+  afterEach(async(() => http.verify()));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
