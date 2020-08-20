8e62bd6213229ac073180ba80a8720fb9ea8fce9
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 16:50:00 +0600

    EPGUCORE-35265 fix CustomScreenComponent test

--- projects/epgu-constructor/src/app/modules/custom/components/custom-screen/custom-screen.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/custom/components/custom-screen/custom-screen.component.spec.ts
@@ -1,21 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomScreenComponent } from './custom-screen.component';
+import { RestService } from '../../../../services/rest/rest.service'
+import { RestServiceStub } from '../../../../services/rest/rest.service.stub'
+import { NavigationService } from '../../../../layout/service/navigation/navigation.service'
+import { ConstructorService } from '../../../../services/constructor/constructor.service'
+import { ConstructorServiceStub } from '../../../../services/constructor/constructor.service.stub'
+import { EgpuResponseCustomComponentDisplayInterface } from '../../../../../interfaces/custom-component.interface'
+import { COMPONENT_TYPE } from '../../../../../constant/global'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 
 describe('CustomScreenComponent', () => {
   let component: CustomScreenComponent;
   let fixture: ComponentFixture<CustomScreenComponent>;
+  let restService: RestService;
+  let navService: NavigationService;
+  let constructorService: ConstructorService;
+  let mockData: EgpuResponseCustomComponentDisplayInterface = {
+    components: [],
+    header: '',
+    id: '',
+    name: '',
+    submitLabel: '',
+    type: COMPONENT_TYPE.COMPONENT
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomScreenComponent ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
+      declarations: [ CustomScreenComponent ],
+      providers: [
+        NavigationService,
+        {provide: RestService, useClass: RestServiceStub},
+        {provide: ConstructorService, useClass: ConstructorServiceStub},
+      ]
     })
     .compileComponents();
+    restService = TestBed.inject(RestService);
+    navService = TestBed.inject(NavigationService);
+    constructorService = TestBed.inject(ConstructorService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomScreenComponent);
     component = fixture.componentInstance;
+    component.data = mockData;
     fixture.detectChanges();
   });
 
