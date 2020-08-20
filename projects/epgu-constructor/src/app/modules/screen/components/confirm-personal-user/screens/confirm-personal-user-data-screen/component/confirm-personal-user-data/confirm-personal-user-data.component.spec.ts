742bb387ef056e6dc0fcc55a077a535bc5a26fc1
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 22:59:24 +0600

    EPGUCORE-35265 fix ConfirmPersonalUserDataComponent test

--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-data-screen/component/confirm-personal-user-data/confirm-personal-user-data.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-data-screen/component/confirm-personal-user-data/confirm-personal-user-data.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmPersonalUserDataComponent } from './confirm-personal-user-data.component';
+import { ConfirmUserDataInterface } from '../../../../../../../../../interfaces/confirm-user-data.interface'
+import { ToJsonPipe } from '../../../../../../../../shared-module/pipe/toJson/to-json.pipe'
 
 describe('ConfirmPersonalUserDataComponent', () => {
   let component: ConfirmPersonalUserDataComponent;
   let fixture: ComponentFixture<ConfirmPersonalUserDataComponent>;
+  let mockData: ConfirmUserDataInterface = {
+    attrs: {
+      actions: [
+        {
+          label: '',
+          method: ''
+        }
+      ],
+      fields: [
+        {
+          fieldName: 'birthDate',
+          label: 'Birthday Date'
+        }
+      ]
+    },
+    type: '',
+    value: '',
+    label: '',
+    id: ''
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmPersonalUserDataComponent ]
+      declarations: [ ConfirmPersonalUserDataComponent, ToJsonPipe ]
     })
     .compileComponents();
   }));
@@ -16,6 +38,7 @@ describe('ConfirmPersonalUserDataComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfirmPersonalUserDataComponent);
     component = fixture.componentInstance;
+    component.data = mockData;
     fixture.detectChanges();
   });
 
