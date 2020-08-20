c415bcd1a88f9d8a3f8e323060fc6673b4627d4a
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 13:11:14 +0600

    EPGUCORE-35265 fix RootComponent test

--- projects/epgu-constructor/src/app/modules/invitation/components/root/root.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/invitation/components/root/root.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RootComponent } from './root.component';
+import {ConstructorService} from '../../../../services/constructor/constructor.service';
+import {ConstructorServiceStub} from '../../../../services/constructor/constructor.service.stub';
+import {NavigationService} from '../../../../layout/service/navigation/navigation.service';
 
 describe('RootComponent', () => {
   let component: RootComponent;
   let fixture: ComponentFixture<RootComponent>;
+  let navService: NavigationService;
+  let constructorService: ConstructorService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RootComponent ]
+      declarations: [ RootComponent ],
+      providers: [NavigationService, { provide: ConstructorService, useClass: ConstructorServiceStub }]
     })
     .compileComponents();
+    navService = TestBed.inject(NavigationService);
+    constructorService = TestBed.inject(ConstructorService);
   }));
 
   beforeEach(() => {
