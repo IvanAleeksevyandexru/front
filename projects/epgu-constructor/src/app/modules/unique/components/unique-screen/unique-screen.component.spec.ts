3ce1748f41f973902ea1e666aa04117e0f2450ea
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 17:35:28 +0600

    EPGUCORE-35265 fix UniqueScreenComponent test

--- projects/epgu-constructor/src/app/modules/unique/components/unique-screen/unique-screen.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/unique/components/unique-screen/unique-screen.component.spec.ts
@@ -1,21 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UniqueScreenComponent } from './unique-screen.component';
+import { NavigationService } from '../../../../layout/service/navigation/navigation.service'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import {
+  EgpuResponseComponentInterface,
+  EgpuResponseDisplayInterface
+} from '../../../../../interfaces/epgu.service.interface'
+import { COMPONENT_TYPE } from '../../../../../constant/global'
 
 describe('UniqueScreenComponent', () => {
   let component: UniqueScreenComponent;
   let fixture: ComponentFixture<UniqueScreenComponent>;
+  let navService: NavigationService;
+  let mockData: EgpuResponseDisplayInterface = {
+    components: [{
+      attrs: {},
+      id: '',
+      label: '',
+      type: '',
+      value: ''
+    }],
+    header: '',
+    id: '',
+    name: '',
+    submitLabel: '',
+    type: COMPONENT_TYPE.COMPONENT
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UniqueScreenComponent ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
+      declarations: [ UniqueScreenComponent ],
+      providers: [NavigationService]
     })
     .compileComponents();
+    navService = TestBed.inject(NavigationService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UniqueScreenComponent);
     component = fixture.componentInstance;
+    component.data = mockData;
     fixture.detectChanges();
   });
 
