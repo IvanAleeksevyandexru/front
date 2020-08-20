f0646824393525e9154fcdcfc5a75000c168d619
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 23:17:12 +0600

    EPGUCORE-35265 fix ConfirmPersonalUserAddressComponent test

--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-address-screen/components/confirm-personal-user-address/confirm-personal-user-address.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-address-screen/components/confirm-personal-user-address/confirm-personal-user-address.component.spec.ts
@@ -1,14 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ConfirmPersonalUserAddressComponent } from './confirm-personal-user-address.component';
+import { ConfirmPersonalUserAddressComponent } from './confirm-personal-user-address.component'
+import { ConfirmAddressInterface } from '../../interface/confirm-address.interface'
+import { SCREEN_COMPONENT_NAME } from '../../../../../../../../../constant/global'
+import { UnsubscribeService } from '../../../../../../../../services/unsubscribe/unsubscribe.service'
+import { ConstructorConfigServiceStub } from '../../../../../../../../services/config/constructor-config.service.stub'
+import { ConstructorConfigService } from '../../../../../../../../services/config/constructor-config.service'
+import { FormsModule } from '@angular/forms'
 
 describe('ConfirmPersonalUserAddressComponent', () => {
   let component: ConfirmPersonalUserAddressComponent;
   let fixture: ComponentFixture<ConfirmPersonalUserAddressComponent>;
+  let mockData: ConfirmAddressInterface = {
+    attrs: {
+      actions: [],
+      fields: []
+    },
+    id: '',
+    value: '{}',
+    label: '',
+    type: SCREEN_COMPONENT_NAME.confirmPersonalUserRegAddr
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmPersonalUserAddressComponent ]
+      imports: [FormsModule],
+      declarations: [ ConfirmPersonalUserAddressComponent ],
+      providers: [
+        UnsubscribeService,
+        {provide: ConstructorConfigService, useClass: ConstructorConfigServiceStub}
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +37,8 @@ describe('ConfirmPersonalUserAddressComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfirmPersonalUserAddressComponent);
     component = fixture.componentInstance;
+    component.data = mockData;
+    component.isEditable = false;
     fixture.detectChanges();
   });
 
