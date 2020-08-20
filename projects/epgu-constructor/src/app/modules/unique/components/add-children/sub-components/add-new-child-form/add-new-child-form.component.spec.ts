6f5037cb1faf7d26a443aefb0d43df54ecffb843
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 16:07:49 +0600

    EPGUCORE-35265 fix AddNewChildFormComponent test

--- projects/epgu-constructor/src/app/modules/unique/components/add-children/sub-components/add-new-child-form/add-new-child-form.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/unique/components/add-children/sub-components/add-new-child-form/add-new-child-form.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AddNewChildFormComponent } from './add-new-child-form.component';
+
 
 describe('AddNewChildFormComponent', () => {
   let component: AddNewChildFormComponent;
   let fixture: ComponentFixture<AddNewChildFormComponent>;
+  let mockData = {
+    childrenList: [ { birthDate: '', id: '' } ],
+    child: [ { birthDate: '', id: '' } ]
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
+      imports: [ FormsModule ],
       declarations: [ AddNewChildFormComponent ]
     })
     .compileComponents();
@@ -16,6 +25,7 @@ describe('AddNewChildFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddNewChildFormComponent);
     component = fixture.componentInstance;
+    component.data = mockData;
     fixture.detectChanges();
   });
 
