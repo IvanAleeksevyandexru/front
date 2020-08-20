5e3b71cfe6a47b9493f37dfcaa35b94b8b19a1b4
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-08-20 16:51:52 +0300

    [EPGUCORE-35701]: extra hotfix and add mock service

--- projects/epgu-constructor/src/app/services/rest/rest.service.ts
+++ projects/epgu-constructor/src/app/services/rest/rest.service.ts
@@ -3,26 +3,27 @@ import { Injectable } from '@angular/core';
 import { DictionaryOptionsInterface, DictionaryResponse } from '../../../interfaces/dictionary-options.interface';
 import { EgpuResponseInterface } from '../../../interfaces/epgu.service.interface';
 import { ConstructorConfigService } from '../config/constructor-config.service';
+import { MockService } from '../mock/mock.service';
 
 @Injectable()
 export class RestService {
   apiUrl: string;
   serviceId: string;
   dictionaryUrl: string;
-
-
-  userId = '1000299353'; // Николаев
-  // userId = '1000415878'; // Федоров
-  // eslint-disable-next-line max-len
-  token = 'eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImFjY2VzcyIsImFsZyI6IlJTMjU2In0.eyJuYmYiOjE1OTc5Mjc2NTIsInNjb3BlIjoiaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL3Vzcl90cm0_b2lkPTEwMDAyOTkzNTMmbW9kZT13IGh0dHA6XC9cL2VzaWEuZ29zdXNsdWdpLnJ1XC91c3JfaW5mP29pZD0xMDAwMjk5MzUzJm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvdXNyX3NlYz9tb2RlPXcmb2lkPTEwMDAyOTkzNTMiLCJpc3MiOiJodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvIiwidXJuOmVzaWE6c2lkIjoiYTRiNTE0NTFjY2Q2NzcxOGE1YzhlMWMwODgzYjEzMmY5YzBmZjZlMGJjYjVmMDg2YTBhYTMzMzZkMWI0MDM1MSIsInVybjplc2lhOnNial9pZCI6MTAwMDI5OTM1MywiZXhwIjoxNTk4MDE0MDUyLCJpYXQiOjE1OTc5Mjc2NTIsImNsaWVudF9pZCI6IlBHVSJ9.b5A9qoB6AyOamGrJN0tyF840OxLTElFBvQiT7PqhN2qse-bCAirdMja3X8Haf9jenpcMBxh-X9hbQC2cieu4jiGO2wqGNMIGPUANGiGTPsaIp7OGsRm0Yk58TNT-zQq1_HhS8B4vVnRkuI-qvpHm8ADjkVgjVC3LbePhi0M_8Ndjroo2dtornOwds2EGfJ8AxvPKc9taKwvbiBE3eB_DfH6CSClNOgdr3x1C2KeM0uvp_YR_ejDPxq_LqmYHj69N9eVUfMirQkIRtf8WRUh2-yupasPRsVUxpRUl4dNnXg-lZRyHJu6EpCKrbTIGfMhZOuDTXPEZwuPyGgLR4lnUJQ';
+  currentUserId: string;
+  currentUserToken: string;
 
   constructor(
     private http: HttpClient,
-    private cnstrctrConfigSrv: ConstructorConfigService
+    private constructorConfigService: ConstructorConfigService,
+    private mockService: MockService,
   ) {
-    this.apiUrl = cnstrctrConfigSrv.config.apiUrl;
-    this.serviceId = cnstrctrConfigSrv.config.serviceId;
-    this.dictionaryUrl = cnstrctrConfigSrv.config.dictionaryUrl;
+    this.apiUrl = constructorConfigService.config.apiUrl;
+    this.serviceId = constructorConfigService.config.serviceId;
+    this.dictionaryUrl = constructorConfigService.config.dictionaryUrl;
+    // TODO: add fetch current user data for prod env
+    this.currentUserId = constructorConfigService.config.isProd ? this.currentUserId : this.mockService.currentUserId;
+    this.currentUserToken = constructorConfigService.config.isProd ? this.currentUserToken : this.mockService.currentUserToken;
   }
 
   public getData() {
@@ -36,8 +37,8 @@ export class RestService {
     const path = `${this.apiUrl}/service/${this.serviceId}/scenario/getNextStep`;
     return this.http.post<EgpuResponseInterface>(path, {
       ...data,
-      userId: this.userId,
-      token: this.token,
+      userId: this.currentUserId,
+      token: this.currentUserToken,
     }, {
       withCredentials: false
     });
@@ -47,8 +48,8 @@ export class RestService {
     const path = `${this.apiUrl}/service/${this.serviceId}/scenario/getPrevStep`;
     return this.http.post<EgpuResponseInterface>(path, {
       ...data,
-      userId: this.userId,
-      token: this.token,
+      userId: this.currentUserId,
+      token: this.currentUserToken,
     }, {
       withCredentials: false
     });
