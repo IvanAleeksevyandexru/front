fb9ddbc5feeb5ee379d8ee2cc437d3ff8185bba9
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-20 20:19:40 +0600

    EPGUCORE-35265 fix ConstructorComponent test

--- projects/epgu-constructor/src/app/constructor.component.spec.ts
+++ projects/epgu-constructor/src/app/constructor.component.spec.ts
@@ -1,17 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ConstructorComponent } from './constructor.component';
+import { ConstructorService } from './services/constructor/constructor.service'
+import { ConstructorServiceStub } from './services/constructor/constructor.service.stub'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 
 describe('ConstructorComponent', () => {
+  let constructorService: ConstructorService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
       imports: [
         RouterTestingModule
       ],
       declarations: [
         ConstructorComponent
       ],
+      providers: [{provide: ConstructorService, useClass: ConstructorServiceStub}]
     }).compileComponents();
+    constructorService = TestBed.inject(ConstructorService);
   }));
 
   it('should create the app', () => {
