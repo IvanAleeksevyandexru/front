ab8716aef2ac49ca9d695762b764e1b10de4f1f0
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-25 15:20:20 +0600

    EPGUCORE-35855 fix TerabyteService test

--- projects/epgu-constructor/src/app/services/rest/terabyte.service.spec.ts
+++ projects/epgu-constructor/src/app/services/rest/terabyte.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TerabyteService } from './terabyte.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ConstructorConfigService } from '../config/constructor-config.service'
+import { ConstructorConfigServiceStub } from '../config/constructor-config.service.stub'
 
 describe('TerabyteService', () => {
   let service: TerabyteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        TerabyteService,
+        {provide: ConstructorConfigService, useClass: ConstructorConfigServiceStub}
+      ]
+    });
     service = TestBed.inject(TerabyteService);
   });
 
