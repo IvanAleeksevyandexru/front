40376edeeaf12c98eb67f5a82c77a0567f747b25
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-26 19:32:57 +0600

    EPGUCORE-36022 fix ConfirmPersonalUserPhoneScreenComponent test

--- projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-phone-screen/confirm-personal-user-phone-screen.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/screen/components/confirm-personal-user/screens/confirm-personal-user-phone-screen/confirm-personal-user-phone-screen.component.spec.ts
@@ -1,14 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks'
+
 import { ConfirmPersonalUserPhoneScreenComponent } from './confirm-personal-user-phone-screen.component';
 import { ComponentInterface } from '../../../../../../../interfaces/epgu.service.interface'
 import { ScreenComponentService } from '../../../../service/screen-component/screen-component.service'
-
+// eslint-disable-next-line max-len
+import { ConfirmPersonalUserScreenLayoutComponent } from '../../sub-components/confirm-personal-user-screen-layout/confirm-personal-user-screen-layout.component'
+import { ConfirmPersonalUserPhoneComponent } from './components/confirm-personal-user-phone/confirm-personal-user-phone.component'
 
 
 describe('ConfirmPersonalUserPhoneScreenComponent', () => {
   let component: ConfirmPersonalUserPhoneScreenComponent;
   let fixture: ComponentFixture<ConfirmPersonalUserPhoneScreenComponent>;
+  let ConfirmPersonalUserPhoneComponentMock = MockComponent(ConfirmPersonalUserPhoneComponent);
   const mockData: ComponentInterface = {
     attrs: {},
     id: '',
@@ -19,8 +23,11 @@ describe('ConfirmPersonalUserPhoneScreenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
-      declarations: [ ConfirmPersonalUserPhoneScreenComponent ],
+      declarations: [
+        ConfirmPersonalUserPhoneScreenComponent,
+        ConfirmPersonalUserPhoneComponentMock,
+        ConfirmPersonalUserScreenLayoutComponent
+      ],
       providers: [ ScreenComponentService ]
     })
     .compileComponents();
