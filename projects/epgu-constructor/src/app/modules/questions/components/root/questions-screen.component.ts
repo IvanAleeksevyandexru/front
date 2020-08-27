36abeb75e8cd438d9b00567d71619526694cfbfa
taliev <taliev@luxoft.com>
2020-08-27 14:35:05 +0300

    EPGUCORE-35855 - add modal in questions-screen; - fix bug with hidden id;

--- projects/epgu-constructor/src/app/modules/questions/components/root/questions-screen.component.ts
+++ projects/epgu-constructor/src/app/modules/questions/components/root/questions-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 import { UnsubscribeService } from '../../../../services/unsubscribe/unsubscribe.service';
 import { NavigationService } from '../../../../shared-module/service/navigation/navigation.service';
@@ -6,6 +6,9 @@ import {
   QuestionsComponentActionsInterface,
   QuestionsDisplayInterface,
 } from '../interface/question-block.interface';
+import { ConfirmationModalComponent } from '../../../../shared-module/components/confirmation-modal/confirmation-modal.component';
+import { ModalService } from '../../../../services/modal/modal.service';
+import { QuestionScreenModalParams } from './questions-screen.constant';
 
 @Component({
   selector: 'epgu-constructor-question-screen',
@@ -13,16 +16,25 @@ import {
   styleUrls: ['./questions-screen.component.scss'],
   providers: [UnsubscribeService],
 })
-export class QuestionsScreenComponent {
+export class QuestionsScreenComponent implements AfterViewInit {
   @Input() data: QuestionsDisplayInterface;
   @Input() errors: object;
   @Output() nextStepEvent = new EventEmitter();
   @Output() prevStepEvent = new EventEmitter();
 
-  constructor(private navService: NavigationService, private ngUnsubscribe$: UnsubscribeService) {
+  constructor(
+    private modalService: ModalService,
+    private navService: NavigationService,
+    private ngUnsubscribe$: UnsubscribeService,
+  ) {
     this.navService.clickToBack$
       .pipe(takeUntil(this.ngUnsubscribe$))
       .subscribe(() => this.goPrevStepEvent());
+  }
+
+  ngAfterViewInit(): void {
+    const arr = (this.data.components[0]?.attrs as any)?.clarifications || [];
+    this.addListenerForAllItemInHtmlFromServer(arr);
   }
 
   goPrevStepEvent() {
@@ -31,5 +43,19 @@ export class QuestionsScreenComponent {
 
   answerChoose(answer: QuestionsComponentActionsInterface): void {
     this.nextStepEvent.emit(answer.value);
+  }
+
+  private addListenerForAllItemInHtmlFromServer(arr: any) {
+    Object.entries(arr)?.forEach(([id, modalData]) => {
+      const link = document.getElementById(id);
+      link.addEventListener('click', () => this.showModal(modalData));
+    });
+  }
+
+  showModal(params) {
+    this.modalService.openModal(ConfirmationModalComponent, {
+      ...QuestionScreenModalParams,
+      ...params,
+    });
   }
 }
