ce931509bfb4e9e0174a0928dd1ae49c6a104f4b
Anstertum <anstertum2@gmail.com>
2020-08-27 04:59:19 -0700

    Change file upload component

--- projects/epgu-constructor/src/app/modules/unique/unique-screen.module.ts
+++ projects/epgu-constructor/src/app/modules/unique/unique-screen.module.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EpguLibModule } from 'epgu-lib';
-import { WebcamModule } from 'ngx-webcam';
 import { SharedModule } from '../../shared-module/shared-components.module';
 import { AddChildrenScreenModule } from './components/add-children/screens/add-children-screen/add-children-screen.module';
-import { FileUploadScreenComponent } from './components/file-upload-screen/file-upload-screen.component';
-import { FileUploadItemComponent } from './components/file-upload-screen/sub-components/file-upload-item/file-upload-item.component';
-import { FileUploadComponent } from './components/file-upload-screen/sub-components/file-upload/file-upload.component';
-import { WebcamShootComponent } from './components/file-upload-screen/sub-components/webcam-shoot/webcam-shoot.component';
+import { FileUploadModule } from './components/file-upload-screen/file-upload.module';
 import { RepeatableFieldsComponent } from './components/repeatable-fields/repeatable-fields.component';
 import { SelectMapObjectModule } from './components/select-map-object/select-map-object.module';
 import { UniqueScreenComponent } from './components/unique-screen/unique-screen.component';
@@ -19,10 +15,6 @@ const COMPONENTS = [
 @NgModule({
   declarations: [
     ...COMPONENTS,
-    FileUploadScreenComponent,
-    FileUploadComponent,
-    FileUploadItemComponent,
-    WebcamShootComponent,
     RepeatableFieldsComponent,
   ],
   exports: [
@@ -32,7 +24,7 @@ const COMPONENTS = [
     CommonModule,
     AddChildrenScreenModule,
     SharedModule,
-    WebcamModule,
+    FileUploadModule,
     EpguLibModule.forChild(),
     SelectMapObjectModule,
   ],
