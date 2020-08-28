7edf023cd95fbb12c31f3f4ee534a4fff95659c2
Svetlana <7vet@mail.ru>
2020-08-28 13:38:15 +0300

    empty.component.ts removed services

--- projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.spec.ts
@@ -1,22 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SCREEN_TYPE } from '../../../../../constant/global';
 import { DisplayInterface } from '../../../../../interfaces/epgu.service.interface';
-import { ConstructorServiceStub } from '../../../../services/constructor/constructor.service.stub';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { ScreenContainerComponent } from '../../../../shared-module/components/screen-container/screen-container.component'
 import { EmptyComponent } from './empty.component';
-import { NavigationService } from '../../../../shared-module/service/navigation/navigation.service';
-import { ConstructorService } from '../../../../services/constructor/constructor.service';
-import { ScreenComponentService } from '../../../screen/service/screen-component/screen-component.service';
 
 
 // TODO: Need to refactoring component
 describe.skip('EmptyComponent', () => {
 	let component: EmptyComponent;
 	let fixture: ComponentFixture<EmptyComponent>;
-	let navService: NavigationService;
-	let constructorService: ConstructorService;
-	let screenComponentService: ScreenComponentService;
 	const mockData: DisplayInterface = {
 		components: [{
 			attrs: {},
@@ -35,17 +27,9 @@ describe.skip('EmptyComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
-			declarations: [EmptyComponent, ScreenContainerComponent],
-			providers: [
-				NavigationService,
-				{ provide: ConstructorService, useClass: ConstructorServiceStub },
-				ScreenComponentService
-			]
+			declarations: [EmptyComponent],
 		})
 			.compileComponents();
-		navService = TestBed.inject(NavigationService);
-		constructorService = TestBed.inject(ConstructorService);
-		screenComponentService = TestBed.inject(ScreenComponentService);
 	}));
 
 	beforeEach(() => {
--- projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.ts
+++ projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.ts
@@ -1,54 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { takeUntil } from 'rxjs/operators';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { EMPTY_SCREEN_COMPONENT } from '../../../../../constant/global';
 import { DisplayInterface } from '../../../../../interfaces/epgu.service.interface';
-import { ConstructorService } from '../../../../services/constructor/constructor.service';
 import { UnsubscribeService } from '../../../../services/unsubscribe/unsubscribe.service';
-import { ScreenComponentService } from '../../../screen/service/screen-component/screen-component.service';
-import { NavigationService } from '../../../../shared-module/service/navigation/navigation.service';
 
 @Component({
   selector: 'epgu-constructor-empty',
   templateUrl: './empty.component.html',
   providers: [UnsubscribeService],
 })
-export class EmptyComponent implements OnInit {
+export class EmptyComponent {
   // <-- constant
   emptyComponentName = EMPTY_SCREEN_COMPONENT;
-
-  // <-- variables
-  componentData = null;
 
   @Input() data: DisplayInterface;
   @Output() nextStepEvent = new EventEmitter();
   @Output() prevStepEvent = new EventEmitter();
-
-  constructor(
-    public constructorService: ConstructorService,
-    private navService: NavigationService,
-    public screenComponentService: ScreenComponentService,
-    private ngUnsubscribe$: UnsubscribeService,
-  ) {
-    this.navService.clickToBack$
-      .pipe(takeUntil(this.ngUnsubscribe$))
-      .subscribe(() => this.prevStep());
-  }
-
-  ngOnInit(): void {}
-
-  prevStep() {
-    this.prevStepEvent.emit();
-  }
-
-  nextStep() {
-    if (typeof this.screenComponentService.dataToSend === 'object') {
-      this.nextStepEvent.emit(JSON.stringify(this.screenComponentService.dataToSend));
-    } else {
-      this.nextStepEvent.emit(this.screenComponentService.dataToSend);
-    }
-  }
-
-  changedComponentData(value) {
-    this.componentData = value;
-  }
 }
