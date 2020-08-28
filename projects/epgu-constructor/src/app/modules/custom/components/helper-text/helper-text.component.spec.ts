77c9f15033e8f3410410d0eeb3eaaa2158a6bc2c
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-08-28 16:57:53 +0600

    EPGUCORE-36150 add tests for HelperTextComponent

--- projects/epgu-constructor/src/app/modules/custom/components/helper-text/helper-text.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/custom/components/helper-text/helper-text.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelperTextComponent } from './helper-text.component';
+import { By } from '@angular/platform-browser'
 
 describe('HelperTextComponent', () => {
   let component: HelperTextComponent;
   let fixture: ComponentFixture<HelperTextComponent>;
+  let helperTextMock = 'Awesome useful helper text.';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,18 @@ describe('HelperTextComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HelperTextComponent);
     component = fixture.componentInstance;
+    component.text = helperTextMock;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should match snapshot', () => {
+    expect(fixture).toMatchSnapshot();
   });
+
+  it('component should render passed text', () => {
+    const helperText = fixture.debugElement
+      .query(By.css('.helper-text')).nativeElement.textContent;
+    expect(helperText).toEqual(helperTextMock);
+  });
+
 });
