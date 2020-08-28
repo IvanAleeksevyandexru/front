0aa54e53ccc630924c9fa676f3497578e19590e4
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-08-28 16:55:03 +0300

    [EPGUCORE-35025] hotfix question screen

--- projects/epgu-constructor/src/app/modules/questions/components/root/questions-screen.component.ts
+++ projects/epgu-constructor/src/app/modules/questions/components/root/questions-screen.component.ts
@@ -5,10 +5,10 @@ import {
   QuestionsDisplayInterface,
 } from '../../../../../interfaces/question-block.interface';
 import { ConstructorService } from '../../../../services/constructor/constructor.service';
-import { UnsubscribeService } from '../../../../services/unsubscribe/unsubscribe.service';
-import { NavigationService } from '../../../../shared-module/service/navigation/navigation.service';
-import { ConfirmationModalComponent } from '../../../../shared-module/components/confirmation-modal/confirmation-modal.component';
 import { ModalService } from '../../../../services/modal/modal.service';
+import { UnsubscribeService } from '../../../../services/unsubscribe/unsubscribe.service';
+import { ConfirmationModalComponent } from '../../../../shared-module/components/confirmation-modal/confirmation-modal.component';
+import { NavigationService } from '../../../../shared-module/service/navigation/navigation.service';
 import { QuestionScreenModalParams } from './questions-screen.constant';
 @Component({
   selector: 'epgu-constructor-question-screen',
@@ -23,7 +23,6 @@ export class QuestionsScreenComponent implements OnInit {
   private readonly currentCycledFields = this.constructorService.response?.scenarioDto
     ?.currentCycledFields;
   private readonly cycledFieldsKeys = Object.keys(this.currentCycledFields || {});
-  private readonly flattenCycledFieldsValues = { ...this.cycledValues };
 
   @Input() data: QuestionsDisplayInterface;
   @Input() errors: object;
@@ -59,8 +58,8 @@ export class QuestionsScreenComponent implements OnInit {
     const responseData = answer.value;
     if (this.isCycledFields) {
       const [currentCycledFieldsKey] = this.cycledFieldsKeys;
-      const fieldNameRef = this.data.components[0].attrs.fields[0].fieldName;
-      const cycledValuesPrepared = this.flattenCycledFieldsValues;
+      const fieldNameRef = this.data.components[0]?.attrs?.fields[0]?.fieldName;
+      const cycledValuesPrepared = { ...this.cycledValues };
       const mergedCycledAndAnswerValues = { ...cycledValuesPrepared, [fieldNameRef]: answer.value };
       responseData[currentCycledFieldsKey] = {
         visited: true,
