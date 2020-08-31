30eff8ae19c1692f71f867756a285616278520de
Svetlana <7vet@mail.ru>
2020-08-31 14:49:06 +0300

    empty.component.spec

--- projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.spec.ts
+++ projects/epgu-constructor/src/app/modules/empty/components/root/empty.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SCREEN_TYPE } from '../../../../../constant/global';
 import { DisplayInterface } from '../../../../../interfaces/epgu.service.interface';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { EmptyComponent } from './empty.component';
 
 
