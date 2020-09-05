2995d0ce0d424f33cbbfee0fa3fcd02550420f55
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-09-05 17:46:28 +0600

    EPGUCORE-36181 move ConfirmUserData types to component-screen layer

---
+++ projects/epgu-constructor/src/app/screen/component-screen/types/confirm-user-data.types.ts
@@ -0,0 +1,29 @@
+import { ComponentForm } from '../../../services/api/form-player-api/form-player-api.types';
+
+export interface ConfirmUserData extends ComponentForm{
+  attrs: ConfirmUserDataAttrs;
+}
+export interface ConfirmUserDataAttrs {
+  fields: Array<ConfirmUserDataField>;
+  actions: Array<ConfirmUserDataActions>;
+}
+
+export interface ConfirmUserDataActions {
+  label: string;
+  method: string;
+}
+
+export interface ConfirmUserDataField {
+  fieldName: string;
+  label: string;
+}
+
+export interface ConfirmUserDataAdaptiveField {
+  label: string;
+  value: string;
+}
+
+export interface ConfirmUserDataFieldsState {
+  groupName: string;
+  list: Array<ConfirmUserDataAdaptiveField>;
+}
