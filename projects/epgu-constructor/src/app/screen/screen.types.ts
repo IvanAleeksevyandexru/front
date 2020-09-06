ab07dbdc4e12d8ddb3097320acebad35a08f3b3b
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-09-06 18:56:36 +0600

    EPGUCORE-36181 move SCREEN_COMPONENTS to screen.types

--- projects/epgu-constructor/src/app/screen/screen.types.ts
+++ projects/epgu-constructor/src/app/screen/screen.types.ts
@@ -1,5 +1,12 @@
 import { ApplicantAnswers, Display, Gender } from '../services/api/form-player-api/form-player-api.types';
 import { NavigationPayload } from '../form-player.types';
+import { InfoScreenComponent } from './info-screen/info-screen.component';
+import { QuestionsScreenComponent } from './questions-screen/questions-screen.component';
+import { ComponentScreenComponent } from './component-screen/component-screen.component';
+import { CustomScreenComponent } from './custom-screen/custom-screen.component';
+import { UniqueScreenComponent } from './unique-screen/unique-screen.component';
+import { InvitationErrorScreenComponent } from './invitation-error-screen/invitation-error-screen.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
 
 
 export interface ScreenStore {
@@ -15,3 +22,13 @@ export interface Screen {
   prevStep: (data?: NavigationPayload) => void,
   nextStep: (data?: NavigationPayload) => void,
 }
+
+export const SCREEN_COMPONENTS: Readonly<{ [key: string]: any }> = {
+  INFO: InfoScreenComponent,
+  QUESTION: QuestionsScreenComponent,
+  COMPONENT: ComponentScreenComponent,
+  CUSTOM: CustomScreenComponent,
+  UNIQUE: UniqueScreenComponent,
+  INVITATION_ERROR: InvitationErrorScreenComponent,
+  EMPTY: EmptyScreenComponent,
+};
