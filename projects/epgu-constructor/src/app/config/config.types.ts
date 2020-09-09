29d1dfad1a9def50a8045f9b4bcc3d898189615f
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-09-09 21:56:02 +0600

    EPGUCORE-36774 add js doc for Config interface

--- projects/epgu-constructor/src/app/config/config.types.ts
+++ projects/epgu-constructor/src/app/config/config.types.ts
@@ -1,5 +1,12 @@
 /**
- * Интерфейс конфигурации модуля
+ * @property {string}apiUrl - url до форм плеер сервиса, как правило apiHostName/api
+ * @property {string}dictionaryUrl - url до сервиса словарей, как правило apiHostName/api/nsi/v1/dictionary
+ * @property {string}externalApiUrl - url до сервиса dadata, как правило apiHostName/api/nsi/v1
+ * @property {string}timeSlotApiUrl - url до сервиса тайм слотов, как правило apiHostName/api/lk/v1/equeue/agg
+ * @property {string}uinApiUrl - url до сервиса по получению uin для платежей, как правило apiHostName/api/lk/v1/paygate/uin
+ * @property {string}paymentUrl - url до сервиса оплаты, как правило apiHostName
+ * @property {string}fileUploadApiUrl - url до хранилища файлов, как правило apiHostName/api/storage/v1/files
+ * @property {string}yandexMapsApiKey - токен для яндекс карт
  */
 export interface Config {
   apiUrl: string;
@@ -9,6 +16,6 @@ export interface Config {
   externalLkUrl: string;
   uinApiUrl: string;
   paymentUrl: string;
-  yandexMapsApiKey: string;
   fileUploadApiUrl: string;
+  yandexMapsApiKey: string;
 }
