ea7005fd6cc340e8458d6ee07a5121559cecadd6
Stanislav Korolev <SSKorolev@luxoft.com>
2020-09-10 15:47:21 +0300

    отображать календарь на текущий месяц до загрузки слотов, т.к. сервисы не всегда отдают дату сразу

--- projects/epgu-constructor/src/app/screen/unique-screen/components/time-slots/time-slots.component.ts
+++ projects/epgu-constructor/src/app/screen/unique-screen/components/time-slots/time-slots.component.ts
@@ -9,7 +9,7 @@ import { GibddTimeSlotsService } from './gibdd-time-slots.service';
 import { MvdTimeSlotsService } from './mvd-time-slots.service';
 import { ModalService } from '../../../../services/modal/modal.service';
 import { TimeSlotsConstants } from './time-slots.constants';
-import { SlotInterface, TimeSlotValueInterface } from './time-slots.types';
+import { SlotInterface } from './time-slots.types';
 import { Display } from '../../../screen.types';
 import { ConfirmationModal } from '../../../../shared/components/modal/confirmation-modal/confirmation-modal.interface';
 import { ConfirmationModalComponent } from '../../../../shared/components/modal/confirmation-modal/confirmation-modal.component';
@@ -226,11 +226,10 @@ export class TimeSlotsComponent implements OnInit {
     this.modalService.openModal(ConfirmationModalComponent, params);
   }
 
-  initCalendar(data: TimeSlotValueInterface) {
-    const slotsPeriod = JSON.parse(data.slotsPeriod).value.substring(0, 7);
-    const [activeYearNumber, activeMonthNumber] = slotsPeriod.split('-');
-    this.activeMonthNumber = parseInt(activeMonthNumber, 10) - 1;
-    this.activeYearNumber = parseInt(activeYearNumber, 10);
+  initCalendar() {
+    const initDate = new Date();
+    this.activeMonthNumber = initDate.getMonth();
+    this.activeYearNumber = initDate.getFullYear();
     this.renderSingleMonthGrid(this.weeks);
   }
 
@@ -239,7 +238,7 @@ export class TimeSlotsComponent implements OnInit {
       this.inProgress = true;
       this.label = this.data.components[0].label;
       const value = JSON.parse(this.data.components[0].value);
-      this.initCalendar(value);
+      this.initCalendar();
       this.currentService = this.timeSlotServices[value.timeSlotType];
       this.currentService.init(value).subscribe(
         () => {
@@ -252,6 +251,7 @@ export class TimeSlotsComponent implements OnInit {
             this.showError(`${this.constants.errorInitialiseService} (${this.errorMessage})`);
           } else {
             this.errorMessage = undefined;
+            this.monthsYears = [];
             this.activeMonthNumber = this.currentService.getCurrentMonth();
             this.activeYearNumber = this.currentService.getCurrentYear();
 
