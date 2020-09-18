f69dbf6dc2738c3f31676b3080679a90205d7087
Anstertum <anstertum2@gmail.com>
2020-09-18 02:04:22 -0700

    Add fixes

--- projects/epgu-constructor/src/app/services/utils/compression.service.ts
+++ projects/epgu-constructor/src/app/services/utils/compression.service.ts
@@ -12,6 +12,40 @@ interface Options {
 export class CompressionService {
 
   /**
+  * @returns {boolean}
+  */
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined';
+  }
+
+  /**
+  * @returns {Promise<boolean>}
+  */
+  private async isAutoOrientationInBrowser(): Promise<boolean> {
+    const testImageURL =
+    'data:image/jpeg;base64,/9j/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAYAAAA' +
+    'AAAD/2wCEAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBA' +
+    'QEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQE' +
+    'BAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAf/AABEIAAEAAgMBEQACEQEDEQH/x' +
+    'ABKAAEAAAAAAAAAAAAAAAAAAAALEAEAAAAAAAAAAAAAAAAAAAAAAQEAAAAAAAAAAAAAAAA' +
+    'AAAAAEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwA/8H//2Q==';
+    const testImageFile = await this.getFilefromDataUrl(testImageURL, 'image.jpg', Date.now());
+    const testImageCanvas = (await this.drawFileInCanvas(testImageFile))[1];
+    const testImageFile2 = await this.canvasToFile(
+      testImageCanvas, testImageFile['type'],
+      testImageFile['name'],
+      testImageFile['lastModified']
+    );
+
+    this.cleanupCanvasMemory(testImageCanvas);
+
+    const testImageURL2 = await this.getDataUrlFromFile(testImageFile2);
+    const img = await this.loadImage(testImageURL2);
+
+    return img.width === 1 && img.height === 2;
+  }
+
+  /**
   * @param {File | Blob} file
   * @returns {Promise<string>}
   */
@@ -30,7 +64,7 @@ export class CompressionService {
   * @param {number} [lastModified=Date.now()]
   * @returns {Promise<File | Blob>}
   */
-  private getFilefromDataUrl(dataUrl: string, filename: string, lastModified: number = Date.now()) {
+  private getFilefromDataUrl(dataUrl: string, filename: string, lastModified: number = Date.now()): Promise<File | Blob> {
     return new Promise((resolve) => {
       const arr = dataUrl.split(',');
       const mime = arr[0].match(/:(.*?);/)[1];
@@ -65,33 +99,33 @@ export class CompressionService {
   * @returns {HTMLCanvasElement | OffscreenCanvas}
   */
   private drawImageInCanvas(img: HTMLImageElement): HTMLCanvasElement | OffscreenCanvas {
-    const [canvas, ctx] = this.getNewCanvasAndCtx(img.width, img.height);
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    const [canvas, ctx] = this.getNewCanvasAndCtx(img['width'], img['height']);
+    ctx.drawImage(img, 0, 0, canvas['width'], canvas['height']);
     return canvas;
   }
 
   private async createImageBitmap(data: Blob | ImageData) {
-		return new Promise((resolve, reject) => {
-			let dataURL;
-			if (data instanceof Blob) {
-				dataURL = URL.createObjectURL(data);
-			} else if (data instanceof ImageData) {
-				const canvas = document.createElement('canvas');
-				const ctx = canvas.getContext('2d');
-				canvas.width = data.width;
-				canvas.height = data.height;
-				ctx.putImageData(data,0,0);
-				dataURL = canvas.toDataURL();
-			} else {
-				throw new Error('createImageBitmap does not handle the provided image source type');
-			}
-			const img = document.createElement('img');
-			img.addEventListener('load', () => {
-				resolve(this);
-			});
-			img.src = dataURL;
-		});
-	};
+    return new Promise((resolve, reject) => {
+      let dataURL;
+      if (data instanceof Blob) {
+        dataURL = URL.createObjectURL(data);
+      } else if (data instanceof ImageData) {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas['width'] = data['width'];
+        canvas['height'] = data['height'];
+        ctx.putImageData(data,0,0);
+        dataURL = canvas.toDataURL();
+      } else {
+        throw new Error('createImageBitmap does not handle the provided image source type');
+      }
+      const img = document.createElement('img');
+      img.addEventListener('load', () => {
+        resolve(this);
+      });
+      img.src = dataURL;
+    });
+  };
 
   /**
   * @param {File | Blob} file
@@ -114,7 +148,7 @@ export class CompressionService {
   * @param {string} fileType
   * @param {string} fileName
   * @param {number} fileLastModified
-  * @param {number} [quality]
+  * @param {number} [quality] = 1
   * @returns {Promise<File | Blob>}
   */
   private async canvasToFile(
@@ -126,8 +160,8 @@ export class CompressionService {
     let file;
     if (typeof OffscreenCanvas === 'function' && canvas instanceof OffscreenCanvas) {
       file = await canvas.convertToBlob({ type: fileType, quality });
-      file.name = fileName;
-      file.lastModified = fileLastModified;
+      file['name'] = fileName;
+      file['lastModified'] = fileLastModified;
     } else {
       const dataUrl = canvas.toDataURL(fileType, quality);
       file = await this.getFilefromDataUrl(dataUrl, fileName, fileLastModified);
@@ -138,6 +172,7 @@ export class CompressionService {
   /**
   * @param {File | Blob} file
   * @returns {Promise<number>}
+  * see: https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side/32490603#32490603
   */
   private getExifOrientation(file: File | Blob): Promise<number> {
     return new Promise((resolve, reject) => {
@@ -185,10 +220,10 @@ export class CompressionService {
   * @param options
   * @returns {HTMLCanvasElement | OffscreenCanvas}
   */
-  private handleMaxWidthOrHeight(canvas: HTMLCanvasElement | OffscreenCanvas, options): HTMLCanvasElement | OffscreenCanvas {
-    const width = canvas.width;
-    const height = canvas.height;
-    const maxWidthOrHeight = options.maxWidthOrHeight;
+  private handleMaxWidthOrHeight(canvas: HTMLCanvasElement | OffscreenCanvas, options: Options): HTMLCanvasElement | OffscreenCanvas {
+    const width = canvas['width'];
+    const height = canvas['height'];
+    const maxWidthOrHeight = options['maxWidthOrHeight'];
 
     const needToHandle = isFinite(maxWidthOrHeight) && (width > maxWidthOrHeight || height > maxWidthOrHeight);
 
@@ -198,13 +233,13 @@ export class CompressionService {
     if (needToHandle) {
       [newCanvas, ctx] = this.getNewCanvasAndCtx(width, height);
       if (width > height) {
-        newCanvas.width = maxWidthOrHeight;
-        newCanvas.height = (height / width) * maxWidthOrHeight;
+        newCanvas['width'] = maxWidthOrHeight;
+        newCanvas['height'] = (height / width) * maxWidthOrHeight;
       } else {
-        newCanvas.width = (width / height) * maxWidthOrHeight;
-        newCanvas.height = maxWidthOrHeight;
+        newCanvas['width'] = (width / height) * maxWidthOrHeight;
+        newCanvas['height'] = maxWidthOrHeight;
       }
-      ctx.drawImage(canvas, 0, 0, newCanvas.width, newCanvas.height);
+      ctx.drawImage(canvas, 0, 0, newCanvas['width'], newCanvas['height']);
 
       this.cleanupCanvasMemory(canvas);
     }
@@ -218,17 +253,17 @@ export class CompressionService {
   * @returns {HTMLCanvasElement | OffscreenCanvas}
   */
   private followExifOrientation(canvas: HTMLCanvasElement | OffscreenCanvas, exifOrientation: number): HTMLCanvasElement | OffscreenCanvas {
-    const width = canvas.width;
-    const height = canvas.height;
+    const width = canvas['width'];
+    const height = canvas['height'];
 
     const [newCanvas, ctx] = this.getNewCanvasAndCtx(width, height);
 
     if (4 < exifOrientation && exifOrientation < 9) {
-      newCanvas.width = height;
-      newCanvas.height = width;
+      newCanvas['width'] = height;
+      newCanvas['height'] = width;
     } else {
-      newCanvas.width = width;
-      newCanvas.height = height;
+      newCanvas['width'] = width;
+      newCanvas['height'] = height;
     }
 
     switch (exifOrientation) {
@@ -253,7 +288,7 @@ export class CompressionService {
   * @param {number} height
   * @returns {[HTMLCanvasElement | OffscreenCanvas, CanvasRenderingContext2D]}
   */
-  private getNewCanvasAndCtx(width: number, height: number) {
+  private getNewCanvasAndCtx(width: number, height: number): [HTMLCanvasElement | OffscreenCanvas, CanvasRenderingContext2D] {
     let canvas;
     let ctx;
     try {
@@ -266,8 +301,8 @@ export class CompressionService {
       canvas = document.createElement('canvas') as HTMLCanvasElement;
       ctx = canvas.getContext('2d');
     }
-    canvas.width = width;
-    canvas.height = height;
+    canvas['width'] = width;
+    canvas['height'] = height;
     return [canvas, ctx];
   }
 
@@ -276,8 +311,8 @@ export class CompressionService {
  * @returns void
  */
   private cleanupCanvasMemory(canvas: HTMLCanvasElement | OffscreenCanvas): void {
-    canvas.width = 0;
-    canvas.height = 0;
+    canvas['width'] = 0;
+    canvas['height'] = 0;
   }
 
   /**
@@ -291,50 +326,66 @@ export class CompressionService {
   * @returns {Promise<File | Blob>}
   */
   private async compress(file: File, options: Options): Promise<File | Blob> {
-    let remainingTrials = options.maxIteration || 10;
-    const maxSizeByte = options.maxSizeMB * 1024 * 1024;
+    let remainingTrials = options['maxIteration'] || 10;
+    const maxSizeByte = options['maxSizeMB'] * 1024 * 1024;
 
     let [img, origCanvas] = await this.drawFileInCanvas(file);
 
     const maxWidthOrHeightFixedCanvas = this.handleMaxWidthOrHeight(origCanvas, options);
 
-    options.exifOrientation = options.exifOrientation || await this.getExifOrientation(file);
-    const orientationFixedCanvas = maxWidthOrHeightFixedCanvas;
+    options['exifOrientation'] = options['exifOrientation'] || await this.getExifOrientation(file);
+
+    const orientationFixedCanvas = (await this.isAutoOrientationInBrowser()) && this.isBrowser()
+      ? maxWidthOrHeightFixedCanvas :
+        this.followExifOrientation(maxWidthOrHeightFixedCanvas, options.exifOrientation);
+
     let quality = 1;
 
-    let tempFile = await this.canvasToFile(orientationFixedCanvas, options.fileType || file.type, file.name, file.lastModified, quality);
+    let tempFile = await this.canvasToFile(
+      orientationFixedCanvas,
+      options['fileType'] || file['type'],
+      file['name'],
+      file['lastModified'],
+      quality
+    );
 
-    const origExceedMaxSize = tempFile.size > maxSizeByte;
-    const sizeBecomeLarger = tempFile.size > file.size;
+    const origExceedMaxSize = tempFile['size'] > maxSizeByte;
+    const sizeBecomeLarger = tempFile['size'] > file['size'];
 
     if (!origExceedMaxSize && !sizeBecomeLarger) {
       return tempFile;
     }
 
-    const sourceSize = file.size;
-    const renderedSize = tempFile.size;
+    const sourceSize = file['size'];
+    const renderedSize = tempFile['size'];
     let currentSize = renderedSize;
     let compressedFile;
     let newCanvas;
     let ctx;
     let canvas = orientationFixedCanvas;
     while (remainingTrials-- && (currentSize > maxSizeByte || currentSize > sourceSize)) {
-      const newWidth = origExceedMaxSize ? canvas.width * 0.95 : canvas.width;
-      const newHeight = origExceedMaxSize ? canvas.height * 0.95 : canvas.height;
+      const newWidth = origExceedMaxSize ? canvas['width'] * 0.95 : canvas['width'];
+      const newHeight = origExceedMaxSize ? canvas['height'] * 0.95 : canvas['height'];
       [newCanvas, ctx] = this.getNewCanvasAndCtx(newWidth, newHeight);
 
       ctx.drawImage(canvas, 0, 0, newWidth, newHeight);
 
-      if (file.type === 'image/jpeg') {
+      if (file['type'] === 'image/jpeg') {
         quality *= 0.95;
       }
 
-      compressedFile = await this.canvasToFile(newCanvas, options.fileType || file.type, file.name, file.lastModified, quality);
+      compressedFile = await this.canvasToFile(
+        newCanvas,
+        options['fileType'] || file['type'],
+        file['name'],
+        file['lastModified'],
+        quality
+      );
 
       this.cleanupCanvasMemory(canvas);
 
       canvas = newCanvas;
-      currentSize = compressedFile.size;
+      currentSize = compressedFile['size'];
     }
 
     this.cleanupCanvasMemory(canvas);
@@ -361,15 +412,15 @@ export class CompressionService {
 
     if (!(file instanceof Blob)) {
       throw new Error('The file given is not an instance of Blob or File');
-    } else if (!/^image/.test(file.type)) {
+    } else if (!/^image/.test(file['type'])) {
       throw new Error('The file given is not an image');
     }
 
     compressedFile = await this.compress(file, options);
 
     try {
-      compressedFile.name = file.name;
-      compressedFile.lastModified = file.lastModified;
+      compressedFile['name'] = file['name'];
+      compressedFile['lastModified'] = file['lastModified'];
     } catch (e) {};
 
     return compressedFile;
