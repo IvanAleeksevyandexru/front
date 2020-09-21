179e2a16803fe511fe20bb43fda4deed8333393d
shibkoas@elocont.ru <shibkoas@elocont.ru>
2020-09-21 16:42:13 +0300

    EPGUCORE-36863/EPGUCORE-36866:fix innInputChange

--- projects/epgu-constructor/src/app/screen/custom-screen/components-list/components-list.component.ts
+++ projects/epgu-constructor/src/app/screen/custom-screen/components-list/components-list.component.ts
@@ -191,22 +191,27 @@ export class ComponentsListComponent implements OnInit, OnChanges {
     this.changes.emit(prepareStateForSending);
   }
 
-  innInputChange($event: Event, component: CustomComponent, type: string): void {
+  innInputChange(
+    $event: Event,
+    component: CustomComponent,
+    type: CustomScreenComponentTypes,
+  ): void {
     const { value } = $event.target as HTMLInputElement;
     this.state[component.id].value = value;
     const inputValidationResult =
-      type === 'PersonInnInput' ? checkPersonInn(value) : checkLegalInn(value);
+      type === CustomScreenComponentTypes.PersonInnInput
+        ? checkPersonInn(value)
+        : checkLegalInn(value);
     this.setValidationAndValueState(inputValidationResult, component.id, value);
     this.emmitChanges(component);
   }
 
-  switchInnType(type: string) {
-    const hasInnType = ['LegalInnInput', 'PersonInnInput'].includes(type);
-    if (hasInnType) {
-      return type === 'LegalInnInput' ? 'LegalInnInput' : 'PersonInnInput';
-    }
-
-    return '';
+  switchInnType(type: CustomScreenComponentTypes) {
+    const hasInnType = [
+      CustomScreenComponentTypes.LegalInnInput,
+      CustomScreenComponentTypes.PersonInnInput,
+    ].includes(type);
+    return hasInnType ? type : '';
   }
 
   /**
