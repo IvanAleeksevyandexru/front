4b6b55f43bcc7e88e0ac9e6f57560eb7fc070c1b
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-09-22 18:19:32 +0600

    EPGUCORE-37100 remove first non alphabetic or numeral symbols

--- projects/epgu-constructor/src/app/shared/directives/trim/trim.directive.ts
+++ projects/epgu-constructor/src/app/shared/directives/trim/trim.directive.ts
@@ -6,6 +6,18 @@ import { Directive, HostListener } from '@angular/core';
 export class TrimDirective {
   @HostListener('focusout', ['$event.target'])
   onFocusout(target) {
-    target.value = target.value.trim();
+    let value = this.removeNonAlphabeticSymbolsFromBeginning(target.value);
+    value = this.removeExtraSpacesBetweenWords(value);
+    target.value = value.trim();
   }
+
+  removeNonAlphabeticOrNumeralSymbolsFromBeginning(value: string): string {
+    const index = value.match(/[0-9a-zA-Za-яА-Я]/)?.index;
+    return index ? value.slice(index) : value;
+  }
+
+  removeExtraSpacesBetweenWords(value: string): string {
+    return value.replace(/\s+/g, ' ');
+  }
+
 }
