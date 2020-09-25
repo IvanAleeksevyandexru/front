4ad18a4023a34193a668e0813d1a467268f0f49d
Stanislav Korolev <stanislav.korolev@gmail.com>
2020-09-25 11:05:27 +0300

    EPGUCORE-36024: добавлен listPaymentsApiUrl в environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
   dictionaryUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/nsi/v1/dictionary',
   externalApiUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/nsi/v1',
   timeSlotApiUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/lk/v1/equeue/agg',
+  listPaymentsApiUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/lk/v1/orders/listpaymentsinfo',
   uinApiUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/lk/v1/paygate/uin',
   billsApiUrl: 'https://pgu-dev-fed.test.gosuslugi.ru/api/pay/v1/bills',
   yandexMapsApiKey: '9e8e2fc4-5970-4ca6-95c5-3e620095e8e3',
