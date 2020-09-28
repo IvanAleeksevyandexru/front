de7d43baa66bd2abe0d1a7f7b2e18d59e239ab4d
akomarov@luxoft.com <akomarov@luxoft.com>
2020-09-28 11:53:38 +0300

    feat: added supporting for hidden block in confirmation modal + refactoring [ref EPGUCORE-37332]

--- projects/epgu-constructor/src/app/shared/components/modal/confirmation-modal/confirmation-modal.component.ts
+++ projects/epgu-constructor/src/app/shared/components/modal/confirmation-modal/confirmation-modal.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { ModalBaseComponent } from '../modal-base/modal-base.component';
 import { ConfirmationModal } from './confirmation-modal.interface';
 import { ModalService } from '../../../../services/modal/modal.service';
+import { getHiddenBlock } from '../../../constants/uttils';
 
 @Component({
   selector: 'epgu-constructor-confirmation-modal',
@@ -28,6 +29,12 @@ export class ConfirmationModalComponent extends ModalBaseComponent implements Af
   }
 
   ngAfterViewInit(): void {
+    this.setElemByIdHandler();
+    this.setDefaultCloseAction();
+    this.setCustomButtons();
+  }
+
+  setElemByIdHandler(): void {
     if (this.clarifications) {
       this.elemEventHandlers = Object.entries(this.clarifications).map(([id, data]) => {
         const clarifications = { ...this.clarifications };
@@ -47,18 +54,21 @@ export class ConfirmationModalComponent extends ModalBaseComponent implements Af
     this.elemEventHandlers.forEach(({ elemId, event, handler }) => {
       const elem = this.elemRef.nativeElement.querySelector(`#${elemId}`);
       if (elem) {
-        elem.classList.add('cursor-pointer');
         elem.addEventListener(event, handler);
       }
     });
+  }
 
+  setDefaultCloseAction(): void {
     if (this.showCloseButton) {
       this.buttons.push({
         label: 'Закрыть',
         closeModal: true,
       });
     }
+  }
 
+  setCustomButtons(): void {
     this.buttons.forEach(({ handler, closeModal }, index) => {
       this.buttons[index].handler = () => {
         if (handler) {
@@ -69,5 +79,17 @@ export class ConfirmationModalComponent extends ModalBaseComponent implements Af
         }
       };
     });
+  }
+
+  toggleHiddenBlock($event: MouseEvent, el: HTMLElement): void {
+    const targetElementId = ($event.target as HTMLElement).id;
+    let hiddenBlock = null;
+
+    if (targetElementId) {
+      hiddenBlock = getHiddenBlock(el, targetElementId);
+    }
+    if (hiddenBlock) {
+      hiddenBlock.hidden = !hiddenBlock.hidden;
+    }
   }
 }
