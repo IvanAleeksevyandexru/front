2bf329216efdec98d7547e61a44a73d53f0836c5
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-10-07 14:34:32 +0300

    fix: rename add-children component to select-children component [ref EPGUCORE-38025]

--- projects/epgu-constructor/src/app/screen/component-screen/components/select-children/screens/select-children-screen/select-children-screen.component.spec.ts
+++ projects/epgu-constructor/src/app/screen/component-screen/components/select-children/screens/select-children-screen/select-children-screen.component.spec.ts
@@ -1,65 +1,60 @@
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { MockComponent } from 'ng-mocks';
-// import { ScreenService } from '../../../../../../screen/screen.service';
-// import { UnsubscribeService } from '../../../../../../services/unsubscribe/unsubscribe.service';
-// import { PageNameComponent } from '../../../../../../shared/components/base/page-name/page-name.component';
-// import { NavigationComponent } from '../../../../../../shared/components/navigation/navigation.component';
-// import { ScreenContainerComponent } from '../../../../../../shared/components/screen-container/screen-container.component';
-// import { ScreenPadComponent } from '../../../../../../shared/components/screen-pad/screen-pad.component';
-// import { CurrentAnswersService } from '../../../../../current-answers.service';
-// import { SelectChildrenScreenComponent } from './select-children-screen.component';
-// import { SelectChildrenInterface } from './select-children-screen.type';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockComponent } from 'ng-mocks';
+import { UnsubscribeService } from '../../../../../../services/unsubscribe/unsubscribe.service';
+import { PageNameComponent } from '../../../../../../shared/components/base/page-name/page-name.component';
+import { NavigationComponent } from '../../../../../../shared/components/navigation/navigation.component';
+import { ScreenContainerComponent } from '../../../../../../shared/components/screen-container/screen-container.component';
+import { ScreenPadComponent } from '../../../../../../shared/components/screen-pad/screen-pad.component';
+import { CurrentAnswersService } from '../../../../../current-answers.service';
+import { SelectChildrenScreenComponent } from './select-children-screen.component';
 
 describe('SelectChildrenScreenComponent', () => {
-  // let component: SelectChildrenScreenComponent;
-  // let fixture: ComponentFixture<SelectChildrenScreenComponent>;
-  // let NavigationComponentMock = MockComponent(NavigationComponent);
-  // const mockData: SelectChildrenInterface = {
-  //   attrs: [],
-  //   id: '',
-  //   label: '',
-  //   type: '',
-  //   value: '{}', // json-string friendly
-  //   visited: false,
-  // };
+  let component: SelectChildrenScreenComponent;
+  let fixture: ComponentFixture<SelectChildrenScreenComponent>;
+  let currentAnswersService: CurrentAnswersService;
+  let NavigationComponentMock = MockComponent(NavigationComponent);
+  const mockData = {
+    attrs: [],
+    id: '',
+    label: '',
+    type: '',
+    value: '{}', // json-string friendly
+    visited: false,
+  };
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [
-  //       SelectChildrenScreenComponent,
-  //       NavigationComponentMock,
-  //       PageNameComponent,
-  //       ScreenPadComponent,
-  //       ScreenContainerComponent,
-  //       NavigationComponentMock,
-  //     ],
-  //     schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
-  //     imports: [
-  //       ReactiveFormsModule,
-  //     ],
-  //     providers: [
-  //       CurrentAnswersService,
-  //       UnsubscribeService,
-  //       ScreenService,
-  //     ]
-  //   })
-  //   .compileComponents();
-  // });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SelectChildrenScreenComponent,
+        NavigationComponentMock,
+        PageNameComponent,
+        ScreenPadComponent,
+        ScreenContainerComponent,
+        NavigationComponentMock,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
+      imports: [
+        ReactiveFormsModule,
+      ],
+      providers: [
+        CurrentAnswersService,
+        UnsubscribeService,
+      ]
+    })
+    .compileComponents();
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(SelectChildrenScreenComponent);
-  //   fixture.debugElement.injector.get(CurrentAnswersService);
-  //   fixture.debugElement.injector.get(UnsubscribeService);
-  //   fixture.debugElement.injector.get(ScreenService);
-  //   component = fixture.componentInstance;
-  //   component.data = mockData;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SelectChildrenScreenComponent);
+    component = fixture.componentInstance;
+    component.data = mockData;
+    currentAnswersService = fixture.debugElement.injector.get(CurrentAnswersService);
+    component.addMoreChild();
+  });
 
   it('should create', () => {
-    // TODO: fix unclear infinite test loop here
-    expect(true).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
