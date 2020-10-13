7b6bd6258c878338d2ca9c7ba5e7bc6f027fbb9a
Svetlana <7vet@mail.ru>
2020-10-13 21:02:56 +0300

    feat: invitation-error.component update [EPGUCORE-38516]

--- projects/epgu-constructor/src/app/screen/invitation-error-screen/components/error/invitation-error.component.ts
+++ projects/epgu-constructor/src/app/screen/invitation-error-screen/components/error/invitation-error.component.ts
@@ -1,7 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 /* eslint-disable import/no-extraneous-dependencies */
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { ValidationShowOn } from 'epgu-lib';
+import { ScenarioDto } from 'projects/epgu-constructor/src/app/services/api/form-player-api/form-player-api.types';
+import { UnsubscribeService } from 'projects/epgu-constructor/src/app/services/unsubscribe/unsubscribe.service';
+import { takeUntil, tap } from 'rxjs/operators';
 import { ConfigService } from '../../../../config/config.service';
+import { ValidationService } from '../../../custom-screen/services/validation.service';
 
 @Component({
   selector: 'epgu-constructor-invitation-error',
@@ -9,19 +15,63 @@ import { ConfigService } from '../../../../config/config.service';
   styleUrls: ['./invitation-error.component.scss'],
 })
 export class InvitationErrorComponent implements OnInit {
-  readyToRegistration: boolean;
-  email: FormControl = new FormControl('');
-
   @Input() data: any;
-  @Output() getEmail: EventEmitter<string> = new EventEmitter<string>();
+  @Input() scenarioDto: ScenarioDto;
+  @Output() nextStepEvent: EventEmitter<string> = new EventEmitter<string>();
+  public email: FormControl = new FormControl('', {
+    validators: Validators.required,
+  });
 
-  constructor(public config: ConfigService) {}
+  public validationShowOn = ValidationShowOn.TOUCHED_UNFOCUSED;
+  public emailSent = false;
+  public success = false;
+  private requestOptions = { withCredentials: true };
 
-  sendEmail(): void {
-    this.getEmail.emit(this.email.value);
-    // TODO: добавить бизнес-логику отправки email на ручку /sendInvitation
-    window.location.reload();
+  constructor(
+    public config: ConfigService,
+    private http: HttpClient,
+    private validationService: ValidationService,
+    private ngUnsubscribe$: UnsubscribeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.email.setValidators(this.validationService.customValidator(this.data));
   }
 
-  ngOnInit(): void {}
+  sendEmail(): void {
+    const value = this.scenarioDto.applicantAnswers.sn1a?.value;
+    if (!value) {
+      this.emailSent = true;
+      return;
+    }
+    const snils = JSON.parse(value).inputSnils;
+    const userData = [
+      {
+        type: 'SNILS',
+        id: snils,
+        email: this.email.value,
+      },
+    ];
+    const urlPrefix = this.config.mocks.includes('payment')
+      ? `${this.config.mockUrl}/lk/v1`
+      : `${this.config.invitationUrl}`;
+    const path = `${urlPrefix}/orders/${this.scenarioDto.orderId}/invitations/inviteToSign/send`;
+    this.http
+      .post(path, userData, this.requestOptions)
+      .pipe(
+        tap(() => {
+          this.emailSent = true;
+        }),
+        takeUntil(this.ngUnsubscribe$),
+      )
+      .subscribe(
+        () => {
+          this.success = true;
+        },
+        (error) => console.error(error),
+      );
+  }
+  redirectToLK() {
+    window.location.href = this.config.lkUrl;
+  }
 }
