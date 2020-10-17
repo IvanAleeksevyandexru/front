826d958806548e25be2a1c1d2712c7e310595c19
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-10-17 14:30:28 +0300

    fix: поправил логику определения перевого экрана в navigation компоненте [ref EPGUCORE-38636]

--- projects/epgu-constructor/src/app/shared/components/navigation/navigation.component.ts
+++ projects/epgu-constructor/src/app/shared/components/navigation/navigation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationService } from '../../services/navigation/navigation.service';
 import { ScreenService } from '../../../screen/screen.service';
+import { NavigationService } from '../../services/navigation/navigation.service';
 
 @Component({
   selector: 'epgu-constructor-navigation',
@@ -10,13 +10,13 @@ import { ScreenService } from '../../../screen/screen.service';
 export class NavigationComponent implements OnInit {
   // TODO КОСТЫЛЬ как быстрое решение. Требоуется доработка от backend(-a)
   //  на добавление флага о том что страница первая
-  isFirstScreen = () => Object.keys(this.screenService.applicantAnswers).length;
+  isFirstScreen = () => !Object.keys(this.screenService.applicantAnswers).length;
   constructor(private navService: NavigationService, private screenService: ScreenService) {}
 
   ngOnInit(): void {}
 
   clickGoBack() {
-    if (this.isFirstScreen) {
+    if (this.isFirstScreen()) {
       this.navService.redirectToHome();
     } else {
       this.navService.clickToBack.next();
