5fbcadab73c21e8d78fb7f5bef983ef52365b598
Valentin Perov <perovva@elocont.ru>
2020-10-22 14:12:29 +0400

    fix: исправил доступную кнопку далее на экране "Укажите адрес регистрации" а также простановку значения при возврате на экран [ref EPGUCORE-39533]

--- projects/epgu-constructor/src/app/screen/component-screen/components/confirm-personal-user/screens/temporary-registration-addr/components/temporary-registration-addr/temporary-registration-addr.component.ts
+++ projects/epgu-constructor/src/app/screen/component-screen/components/confirm-personal-user/screens/temporary-registration-addr/components/temporary-registration-addr/temporary-registration-addr.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ValidationShowOn } from 'epgu-lib';
-import { takeUntil } from 'rxjs/operators';
+import { startWith, takeUntil } from 'rxjs/operators';
 import { ConfigService } from '../../../../../../../../config/config.service';
 import { UnsubscribeService } from '../../../../../../../../services/unsubscribe/unsubscribe.service';
 import { CurrentAnswersService } from '../../../../../../../current-answers.service';
@@ -34,8 +34,10 @@ export class TemporaryRegistrationAddrComponent implements OnInit {
 
   initFormGroup(): void {
     const controls = {};
+    const initValue = this.getInitValue(this.data?.value);
+
     this.data?.attrs?.fields.forEach((field) => {
-      controls[field.fieldName] = this.fb.control(null, this.getValidatorsForField(field));
+      controls[field.fieldName] = this.fb.control(initValue, this.getValidatorsForField(field));
     });
     this.redAddrForm = this.fb.group(controls);
   }
@@ -66,13 +68,23 @@ export class TemporaryRegistrationAddrComponent implements OnInit {
   }
 
   private subscribeToFormChanges(): void {
-    this.redAddrForm.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((changes) => {
-      if (this.redAddrForm.invalid) {
-        this.currentAnswersService.isValid = false;
-      } else {
-        this.currentAnswersService.isValid = true;
-        this.currentAnswersService.state = changes;
-      }
-    });
+    this.redAddrForm.valueChanges
+      .pipe(takeUntil(this.ngUnsubscribe$), startWith(this.redAddrForm.value))
+      .subscribe((changes) => {
+        if (this.redAddrForm.invalid) {
+          this.currentAnswersService.isValid = false;
+        } else {
+          this.currentAnswersService.isValid = true;
+          this.currentAnswersService.state = changes;
+        }
+      });
+  }
+
+  /**
+   * метод парсит (при наличие) строку с объектом от dadata-widget и возвращает в него fullAddress
+   * @param value строка с JSON объектом от dadata-widget
+   */
+  private getInitValue(value: string) {
+    return (value && JSON.parse(value).regAddr.fullAddress) || null;
   }
 }
