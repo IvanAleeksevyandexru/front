208994a9fa56a350b5bf9bd59b808798b712fc42
Roman Dashevsky <dashevsky.roman@gmail.com>
2020-10-30 18:12:17 +0600

    EPGUCORE-40251 фикс дял экшен апи

--- projects/epgu-constructor/src/app/services/api/action-api/action-api.service.ts
+++ projects/epgu-constructor/src/app/services/api/action-api/action-api.service.ts
@@ -3,10 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ActionApiDTO, ActionApiResponse } from './action-api.types';
 import { apiUrlDefault } from '../form-player-api/form-player-api.service';
+import { LoadService } from 'epgu-lib';
 
 @Injectable()
 export class ActionApiService {
-  constructor(private http: HttpClient) {}
+  private apiUrl = apiUrlDefault;
+
+  constructor(
+    private http: HttpClient,
+    private loadService: LoadService
+  ) {
+    this.loadService.loaded.subscribe(() => {
+      const coreApiUrl = this.loadService.config.newSfApiUrl;
+      this.apiUrl = coreApiUrl ?? apiUrlDefault;
+    });
+  }
 
   /**
    * Метод для отправки action на бэкенд. На бэке есть дополнительные эндпоинты для каждго экшена
@@ -14,6 +25,6 @@ export class ActionApiService {
    * @param body - тело запроса
    */
   send<T>(path: string, body: ActionApiDTO): Observable<ActionApiResponse<T>> {
-    return this.http.post<ActionApiResponse<T>>(`${apiUrlDefault}/${path}`, body);
+    return this.http.post<ActionApiResponse<T>>(`${this.apiUrl}/${path}`, body);
   }
 }
--- projects/epgu-constructor/src/app/services/api/form-player-config-api/form-player-config-api.service.ts
+++ projects/epgu-constructor/src/app/services/api/form-player-config-api/form-player-config-api.service.ts
@@ -3,25 +3,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Config } from '../../../config/config.types';
 import { LoadService } from 'epgu-lib';
-
-export const apiConfigUrlDefault = '/api';
+import { apiUrlDefault } from '../form-player-api/form-player-api.service';
 
 @Injectable()
 export class FormPlayerConfigApiService {
-  private apiConfigUrl = apiConfigUrlDefault;
+  private apiUrl = apiUrlDefault;
 
   constructor(
     private http: HttpClient,
     private loadService: LoadService
   ) {
     this.loadService.loaded.subscribe(() => {
-      const coreConfigApiUrl = this.loadService.config.newSfConfigApiUrl;
-      this.apiConfigUrl = coreConfigApiUrl ?? apiConfigUrlDefault;
+      const coreApiUrl = this.loadService.config.newSfApiUrl;
+      this.apiUrl = coreApiUrl ?? apiUrlDefault;
     });
   }
 
   public getFormPlayerConfig(): Observable<Config> {
-    const path = `${this.apiConfigUrl}/pgu-service-config/config/default-config`;
+    const path = `${this.apiUrl}/pgu-service-config/config/default-config`;
     return this.http.get<Config>(path);
   }
 
