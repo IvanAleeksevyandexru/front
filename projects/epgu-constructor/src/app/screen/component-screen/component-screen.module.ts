70ff6a8e7e3b67c31415c748916275d7256044c1
Aleksandr Kushnerevich <alexx.orlovv@gmail.com>
2020-11-09 16:48:48 +0300

    [EPGUCORE-40767] fix default regDate

--- projects/epgu-constructor/src/app/screen/component-screen/component-screen.module.ts
+++ projects/epgu-constructor/src/app/screen/component-screen/component-screen.module.ts
@@ -4,7 +4,7 @@ import { ComponentScreenComponent } from './component-screen.component';
 import { ComponentScreenComponentsModule } from '../../component/component-screen/component-screen-components.module';
 import { CoreModule } from '../../core/core.module';
 import { ComponentsListModule } from '../../component/components-list/components-list.module';
-
+import { DatePipe } from '@angular/common';
 
 const COMPONENTS = [
   ComponentScreenComponent,
@@ -19,6 +19,6 @@ const COMPONENTS = [
     ComponentScreenComponentsModule,
     ComponentsListModule,
   ],
-  providers: []
+  providers: [DatePipe]
 })
 export class ComponentScreenModule {}
