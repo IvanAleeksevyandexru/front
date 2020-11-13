ea1d479ad8a2189466d765fe1c92d66ae126731c
mark <mark.moskalenko@gmail.com>
2020-11-13 16:10:02 +0300

    [EPGUCORE-40830] Добавил вывод надписи о том что тайм-слоты заняты

--- projects/epgu-constructor/src/app/shared/components/base/helper-text/helper-text.component.ts
+++ projects/epgu-constructor/src/app/shared/components/base/helper-text/helper-text.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+enum Size {
+  sm = 'sm',
+  lg = 'lg',
+}
 
 @Component({
   selector: 'epgu-constructor-helper-text',
   templateUrl: './helper-text.component.html',
   styleUrls: ['./helper-text.component.scss'],
 })
-export class HelperTextComponent {}
+export class HelperTextComponent {
+  @Input() size: Size = Size.lg;
+  @Input() color: Size = Size.lg;
+}
