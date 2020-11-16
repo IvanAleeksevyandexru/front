3200c20de6c62fb71a34d4e365b7a0e4a134b750
Valentin Perov <perovva@elocont.ru>
2020-11-16 14:37:39 +0400

    feat: реализовал сохранение детей на загран паспорте [ref EPGUCORE-41016]

--- projects/epgu-constructor/src/app/component/component-screen/components/select-children/select-children-screen.component.ts
+++ projects/epgu-constructor/src/app/component/component-screen/components/select-children/select-children-screen.component.ts
@@ -1,18 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ListItem } from 'epgu-lib';
-import { takeUntil } from 'rxjs/operators';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as uuid from 'uuid';
-import { UnsubscribeService } from '../../../../core/services/unsubscribe/unsubscribe.service';
 import { CurrentAnswersService } from '../../../../screen/current-answers.service';
 import { ScreenService } from '../../../../screen/screen.service';
 import { ComponentBase } from '../../../../screen/screen.types';
-
-enum ItemActions {
-  selectChild = 'selectChild',
-  removeItem = 'removeItem',
-}
 
 enum ItemStatus {
   invalid = 'INVALID',
@@ -23,17 +16,15 @@ enum ItemStatus {
   selector: 'epgu-constructor-select-children-screen',
   templateUrl: './select-children-screen.component.html',
   styleUrls: ['./select-children-screen.component.scss'],
-  providers: [UnsubscribeService],
 })
 export class SelectChildrenScreenComponent implements OnInit {
   @Input() data: ComponentBase;
   @Output() nextStepEvent: EventEmitter<string> = new EventEmitter<string>();
 
   valueParsed: any;
-  itemsList: any = [];
-  itemsToSelect: Array<Partial<ListItem>>;
-  selectedItems: { [id: string]: { [ref: string]: any } } = {};
-  items: Array<string> = [];
+  itemsToSelect: Array<ListItem>; // Дети для выпадающего списка
+  items: Array<any> = []; // Выбранные дети
+  itemsComponents = []; // Компоненты для кастомных детей
   selectChildrenForm = new FormGroup({});
   firstNameRef: string;
   idRef: string;
@@ -43,34 +34,45 @@ export class SelectChildrenScreenComponent implements OnInit {
 
   constructor(
     private currentAnswersService: CurrentAnswersService,
-    private ngUnsubscribe$: UnsubscribeService,
     public screenService: ScreenService,
   ) {}
 
   ngOnInit(): void {
-    this.valueParsed = JSON.parse(this.data.value);
-    this.itemsList = this.valueParsed?.items || [];
+    const component = this.screenService.getCompFromDisplay(this.data.id);
+    const itemsList = component ? JSON.parse(component.presetValue) : [];
     this.firstNameRef = this.getRefFromComponent('firstName');
     this.isNewRef = this.getRefFromComponent('isNew');
     this.idRef = this.getRefFromComponent('id');
-    this.itemsToSelect = [
-      ...this.itemsList.map((child) => {
-        return {
-          id: child[this.idRef],
-          text: child[this.firstNameRef],
-        };
-      }),
-      { id: 'new', text: 'Добавить нового ребенка' },
-    ];
-    this.selectedItems = {};
-    this.passDataToSend(Object.values(this.selectedItems));
-
-    this.selectChildrenForm.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe(() => {
-      this.items = Object.keys(this.selectChildrenForm.controls);
-      this.updateCurrentAnswerServiceValidation();
+    this.itemsToSelect = itemsList.map((child) => {
+      const childForSelect = { ...child };
+      childForSelect.id = child[this.idRef];
+      childForSelect.text = child[this.firstNameRef];
+      return childForSelect;
     });
+    const newChildObj = { id: 'new', text: 'Добавить нового ребенка' };
+    newChildObj[this.idRef] = 'new';
+    this.itemsToSelect.push(newChildObj as ListItem);
+    this.initStartValues();
+  }
 
-    this.generateFirstFormItem();
+  initStartValues(): void {
+    const cachedValue = this.screenService.getCompValueFromCachedAnswers(this.data.id);
+    if (cachedValue) {
+      const children = JSON.parse(cachedValue);
+      children.forEach((child, index) => {
+        const isNew = JSON.parse(child[this.isNewRef]);
+        const childId = isNew ? 'new' : child[this.idRef];
+        // По ID получаем ребенка для подстановки в formControl
+        const childFromList = this.itemsToSelect.find((item) => item[this.idRef] === childId);
+        const newChild = this.addMoreChild(childFromList, child);
+        if (isNew) {
+          this.selectChildrenForm.controls[newChild.controlId].disable();
+        }
+        this.handleSelect(child, index);
+      });
+    } else {
+      this.addMoreChild();
+    }
   }
 
   updateCurrentAnswerServiceValidation(): void {
@@ -89,8 +91,11 @@ export class SelectChildrenScreenComponent implements OnInit {
     )?.id;
   }
 
-  addNewChild(item): void {
-    const id = item;
+  /**
+   * Создание кастомного ребенка
+   */
+  addNewChild(index: number): void {
+    const id = uuid.v4();
     const newChild: any = {
       ...this.screenService.component?.attrs?.components?.reduce(
         (accum, value: any) => ({
@@ -99,26 +104,44 @@ export class SelectChildrenScreenComponent implements OnInit {
         }),
         {},
       ),
-      [this.isNewRef]: true,
+      [this.isNewRef]: 'true',
       [this.idRef]: id,
     };
     this.newChildren[id] = true;
-    this.itemsList.push(newChild);
-    this.handleSelect({ id }, item);
+    this.handleSelect(newChild, index);
   }
 
-  removeChild(item: string): void {
-    this.updateItems(ItemActions.removeItem, item);
-    this.passDataToSend(Object.values(this.selectedItems));
+  removeChild(index: number): void {
+    const { controlId } = this.items[index];
+    this.selectChildrenForm.removeControl(controlId);
+    this.items.splice(index, 1);
+    this.itemsComponents.splice(index, 1);
+    this.newChildren[controlId] = false;
+    this.passDataToSend(this.items);
   }
 
-  passDataToSend(selectedItems): void {
-    this.valueParsed.items = selectedItems;
-    this.currentAnswersService.state = this.valueParsed;
+  /**
+   * Сохраняем данные для отправки, удаляя лишние поля
+   */
+  passDataToSend(items): void {
+    const itemsToSend = items.map((child) => {
+      const childToSend = { ...child };
+      delete childToSend.controlId;
+      delete childToSend.id;
+      delete childToSend.text;
+      return childToSend;
+    });
+    this.currentAnswersService.state = itemsToSend;
+    this.setHideStateToSelectedItems();
   }
 
-  updateChild(childData, item): void {
-    const childItem = this.itemsList.find((value) => value[this.idRef] === item);
+  /**
+   * Метод обновляет поля кастомного ребенка
+   * @param childData поля-компоненты для заполенения кастомного ребенка
+   * @param item сам ребенок
+   * @param index индекс массива детей
+   */
+  updateChild(childData, item, index): void {
     const formattedChildData = Object.keys(childData).reduce(
       (accum, key) => ({
         ...accum,
@@ -129,39 +152,41 @@ export class SelectChildrenScreenComponent implements OnInit {
     const updatedChildData = {
       ...formattedChildData,
       ...{
-        [this.idRef]: childItem[this.idRef],
-        [this.isNewRef]: childItem[this.isNewRef],
+        [this.idRef]: item[this.idRef],
+        [this.isNewRef]: item[this.isNewRef],
       },
     };
-    this.selectChildrenForm.controls[item].setValue(updatedChildData);
-    this.selectedItems[item] = updatedChildData;
-    this.passDataToSend(Object.values(this.selectedItems));
-    this.setHideStateToSelectedItems();
+    Object.assign(this.items[index], updatedChildData);
+    this.passDataToSend(this.items);
   }
 
-  addMoreChild(): void {
-    this.addFormControl(uuid.v4());
+  /**
+   * метод добавляет нового ребенка в массив и возвращает его
+   * @param initValue значальное значение для контрола
+   * @param childFromCache ребенок из кэша. Используется для заполнения полей кастомного беренка
+   */
+  addMoreChild(initValue?: any, childFromCache: any = {}): any {
+    const index = this.items.length;
+    const controlId = `child_${uuid.v4()}`;
+    this.addFormControl(controlId, initValue);
+    this.items.push({ controlId });
+    this.itemsComponents[index] = this.prepareItemComponents(childFromCache);
+    return this.items[index];
   }
 
-  handleSelect(event, itemId: string): void {
-    const { id } = event;
-    if (id === 'new') {
-      this.addNewChild(itemId);
+  /**
+   *
+   * @param event объект-ребенок
+   * @param index индекс массива детей
+   */
+  handleSelect(event, index?: number): void {
+    Object.assign(this.items[index], event);
+    if (event[this.idRef] === 'new') {
+      this.addNewChild(index);
     } else {
-      this.updateItems(ItemActions.selectChild, itemId, id);
-      this.passDataToSend(Object.values(this.selectedItems));
+      this.updateCurrentAnswerServiceValidation();
+      this.passDataToSend(this.items);
     }
-  }
-
-  updateItems(action: ItemActions, itemId: string, childId?: string): void {
-    if (action === ItemActions.selectChild && childId) {
-      this.selectedItems[itemId] = this.itemsList.find((child) => child[this.idRef] === childId);
-    } else if (action === ItemActions.removeItem) {
-      this.selectChildrenForm.removeControl(itemId);
-      this.newChildren[itemId] = false;
-      delete this.selectedItems[itemId];
-    }
-    this.setHideStateToSelectedItems();
   }
 
   isScreensAvailable(): boolean {
@@ -172,24 +197,28 @@ export class SelectChildrenScreenComponent implements OnInit {
   }
 
   private setHideStateToSelectedItems(): void {
-    const selectedItemsIds = Object.values(this.selectedItems).map(
-      (selectedItem: { [ref: string]: any }) => selectedItem[this.idRef],
-    );
-
-    this.itemsToSelect = this.itemsToSelect.map((item: ListItem) => {
+    this.itemsToSelect = this.itemsToSelect.map((child) => {
       // eslint-disable-next-line no-param-reassign
-      item.hidden = Boolean(selectedItemsIds.includes(item.id));
-      return item;
+      child.hidden = this.items.some((selectedChild) => {
+        return selectedChild[this.idRef] === child[this.idRef];
+      });
+      return child;
     });
   }
 
-  private generateFirstFormItem(): void {
-    const id = uuid.v4();
-    this.items.push(id);
-    this.addFormControl(id);
+  private addFormControl(id, value = null): void {
+    this.selectChildrenForm.addControl(id, new FormControl(value, [Validators.required]));
   }
 
-  private addFormControl(id): void {
-    this.selectChildrenForm.addControl(id, new FormControl(null, [Validators.required]));
+  /**
+   * метод формирует и возвращает массив компонентов кастомного ребенка
+   * @param child - сохраненный ранее ребенок. Используется для заполнения полей
+   */
+  private prepareItemComponents(child = {}) {
+    return this.screenService.component?.attrs?.components.map((component) => {
+      const componentWithValue = { ...component };
+      componentWithValue.value = child[componentWithValue.id];
+      return componentWithValue;
+    });
   }
 }
