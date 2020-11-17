aa2599e7c0e349c9d6d074f33ac4143a7f72d745
rdashevskiy@luxoft.com <rdashevskiy@luxoft.com>
2020-11-17 09:47:31 +0000

    Merge branch 'EPGUCORE-39844' into 'develop'
    feat: Новый способ обработки сведений об оплате на экране Квитанции на оплату и самой оплаты. Также добавлено скачивание квитанции. [ref: EPGUCORE-39844]
    
    See merge request luxoft/epgu2-form-frontend!728
    
    (cherry picked from commit 2430a138371e8fc7902f88bdbc86ebb44694ebad)
    
    1cd5ac68 feat: Создание компонента счета для оплаты пошлины
    99f42411 Merge branch 'develop' into EPGUCORE-39844
    4cff0455 Merge branch 'develop' into EPGUCORE-39844
    7ec84700 feat: Создание компонента счета для оплаты пошлины через счет и правка...
    f8a2f95f feat: Создание компонента счета для оплаты пошлины через счет и правка...
    6fa68f19 feat: Создание компонента счета для оплаты пошлины через счет и правка...
    dbac9d2b feat: Создание компонента счета для оплаты пошлины через счет и правка...
    2fb6c317 Merge branch 'develop' into EPGUCORE-39844
    f2334d14 fix: value проверка при оплате
    a6627972 fix: Исправлено полечение значение поля value и верно формируется запрос по номеру начисления
    1a5827e9 Merge branch 'develop' into EPGUCORE-39844
    22cf223e fix: Подправлена совместимость скачивания файлов с задачей EPGUCORE-39035,...
    e8ea7874 Merge branch 'develop' into EPGUCORE-39844
    c1196ccd fix: Внесены правки по замечаниям из мерж реквеста
    11d834ba fix: Внесены правки по замечаниям из мерж реквеста
    97687cc0 Merge branch 'develop' into EPGUCORE-39844
    0f352e79 fix: Внесены правки по мерж реквесту, сокращен синтаксис компонентов оплаты,...

---
+++ projects/epgu-constructor/src/app/component/unique-screen/components/payment/components/payment/payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, Injector } from '@angular/core';
+import { UnsubscribeService } from '../../../../../../core/services/unsubscribe/unsubscribe.service';
+import { AbstractPaymentComponent } from '../../abstractpayment.component';
+
+@Component({
+  selector: 'epgu-constructor-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss'],
+  providers: [UnsubscribeService],
+})
+export class PaymentComponent extends AbstractPaymentComponent {
+  constructor(public injector: Injector) {
+    super(injector);
+  }
+}
