5c0b8d699ca3045e98eb46e8b1ac6087de999159
Aleksandr Kushnerevich <alexx.orlovv@gmail.com>
2020-11-17 14:44:49 +0300

    added for not requred text

--- projects/epgu-constructor/src/app/shared/constants/helper-texts.ts
+++ projects/epgu-constructor/src/app/shared/constants/helper-texts.ts
@@ -1,3 +1,4 @@
 export const OPTIONAL_FIELD = 'Необязательно';
+export const SPECIAL_OPTIONAL_FIELD = 'При наличии';
 export const REQUIRED_FIELD = 'Обязательно для заполнения';
 export const INVALID_FORMAT_FIELD = 'Некорректный формат';
