f26ce4cac856d9e789eb37b08c5bf3a3c7dbc6be
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2020-11-17 10:29:23 +0300

    feat: добавил тип modalRedirectTo и показ этой модалки в QuestionScr компонентах [ref EPGUCORE-41205]

--- projects/epgu-constructor/src/app/screen/questions-screen/questions-screen.component.ts
+++ projects/epgu-constructor/src/app/screen/questions-screen/questions-screen.component.ts
@@ -1,8 +1,12 @@
 import { Component, Injector } from '@angular/core';
-import { NavigationPayload } from '../../form-player/form-player.types';
+import { takeUntil } from 'rxjs/operators';
 import { UnsubscribeService } from '../../core/services/unsubscribe/unsubscribe.service';
-import { QuestionsComponentActions } from './questions-screen.types';
+import { NavigationPayload } from '../../form-player/form-player.types';
+import { ConfirmationModalComponent } from '../../modal/confirmation-modal/confirmation-modal.component';
+import { ConfirmationModal } from '../../modal/confirmation-modal/confirmation-modal.interface';
+import { ModalService } from '../../modal/modal.service';
 import { ScreenBase } from '../screenBase';
+import { QuestionsComponentActions } from './questions-screen.types';
 
 @Component({
   selector: 'epgu-constructor-question-screen',
@@ -11,7 +15,7 @@ import { ScreenBase } from '../screenBase';
   providers: [UnsubscribeService],
 })
 export class QuestionsScreenComponent extends ScreenBase {
-  constructor(public injector: Injector) {
+  constructor(public injector: Injector, private modalService: ModalService) {
     super(injector);
   }
 
@@ -19,18 +23,57 @@ export class QuestionsScreenComponent extends ScreenBase {
     this.navigationService.nextStep.next({ payload });
   }
 
-  answerChoose(answer: QuestionsComponentActions): void {
-    if (answer.disabled) {
+  answerChoose(action: QuestionsComponentActions): void {
+    const { disabled, type, value } = action;
+
+    if (disabled) {
       return;
     }
+
+    if (type === 'modalRedirectTo') {
+      this.showModalRedirectTo(action);
+      return;
+    }
+
     const data: NavigationPayload = {};
 
     const componentId = this.screenService.component.id;
     data[componentId] = {
       visited: true,
-      value: answer.value || '',
+      value: value || '',
     };
 
     this.nextStep(data);
+  }
+
+  showModalRedirectTo(action) {
+    const modalResult$ = this.modalService.openModal<boolean, ConfirmationModal>(
+      ConfirmationModalComponent,
+      {
+        text: `<div><img style="display:block; margin: 24px auto" src="{staticDomainAssetsPath}/assets/icons/svg/warn.svg">
+        <h4 style="text-align: center">Переход на старый портал</h4>
+        <p class="helper-text" style="text-align: center; margin-top: 8px;">Раздел пока доступен только в старой версии портала</p></div>`,
+        showCloseButton: false,
+        showCrossButton: true,
+        buttons: [
+          {
+            label: 'Остаться',
+            color: 'white',
+            closeModal: true,
+          },
+          {
+            label: 'Перейти',
+            closeModal: true,
+            value: action.link,
+          },
+        ],
+        isShortModal: true,
+      },
+    );
+    modalResult$.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((result) => {
+      if (typeof result === 'string') {
+        window.location.href = result;
+      }
+    });
   }
 }
