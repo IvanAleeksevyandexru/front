56b6702773994d60f3d11fdf52645c0afc1d1091
Edward Yudinkov <anstertum2@gmail.com>
2020-11-23 13:36:52 +0300

    Fix

--- projects/epgu-constructor/src/app/shared/services/utils/utils.service.ts
+++ projects/epgu-constructor/src/app/shared/services/utils/utils.service.ts
@@ -6,6 +6,43 @@ interface TranslitAlphabet {
   [propName: string]: string;
 }
 
+/*eslint quote-props: ["error", "always"]*/
+const LETTERS = {
+  'а': 'a',
+  'б': 'b',
+  'в': 'v',
+  'г': 'g',
+  'д': 'd',
+  'е': 'e',
+  'ё': 'e',
+  'ж': 'zh',
+  'з': 'z',
+  'и': 'i',
+  'й': 'i',
+  'к': 'k',
+  'л': 'l',
+  'м': 'm',
+  'н': 'n',
+  'о': 'o',
+  'п': 'p',
+  'р': 'r',
+  'с': 's',
+  'т': 't',
+  'у': 'u',
+  'ф': 'f',
+  'х': 'kh',
+  'ч': 'ch',
+  'ц': 'ts',
+  'щ': 'shch',
+  'ш': 'sh',
+  'ъ': 'ie',
+  'ы': 'y',
+  'э': 'e',
+  'ю': 'iu',
+  'я': 'ia',
+  'ь': ''
+} as TranslitAlphabet;
+
 @Injectable()
 export class UtilsService {
   // TODO: add shared utils
@@ -151,47 +188,11 @@ export class UtilsService {
    * @param str 
    */
   public cyrillicToLatin(word: string): string {
-    /*eslint quote-props: ["error", "always"]*/
-    const letters = {
-      'а': 'a',
-      'б': 'b',
-      'в': 'v',
-      'г': 'g',
-      'д': 'd',
-      'е': 'e',
-      'ё': 'e',
-      'ж': 'zh',
-      'з': 'z',
-      'и': 'i',
-      'й': 'i',
-      'к': 'k',
-      'л': 'l',
-      'м': 'm',
-      'н': 'n',
-      'о': 'o',
-      'п': 'p',
-      'р': 'r',
-      'с': 's',
-      'т': 't',
-      'у': 'u',
-      'ф': 'f',
-      'х': 'kh',
-      'ч': 'ch',
-      'ц': 'ts',
-      'щ': 'shch',
-      'ш': 'sh',
-      'ъ': 'ie',
-      'ы': 'y',
-      'э': 'e',
-      'ю': 'iu',
-      'я': 'ia',
-      'ь': ''
-    } as TranslitAlphabet;
     let newStr = '';
     
     for (const char of word) {
       const isUpperCase = char === char.toUpperCase();
-      const translitChar = letters[char.toLowerCase()];
+      const translitChar = LETTERS[char.toLowerCase()];
       if (translitChar === undefined) {
         newStr += char;
       } else {
