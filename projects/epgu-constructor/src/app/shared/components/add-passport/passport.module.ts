777b79a12933a4cdf586e938aa2810531c82898c
shibkoas@elocont.ru <shibkoas@elocont.ru>
2020-12-02 10:29:07 +0300

    EPGUCORE-42096: fix test

--- projects/epgu-constructor/src/app/shared/components/add-passport/passport.module.ts
+++ projects/epgu-constructor/src/app/shared/components/add-passport/passport.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared.module';
 import { CoreModule } from '../../../core/core.module';
 import { PassportComponent } from './passport.component';
+import { ConstructorMaskedInputModule } from '../epgu-lib/constructor-masked-input/constructor-masked-input.module';
 
 @NgModule({
   declarations: [PassportComponent],
-  imports: [CommonModule, SharedModule, CoreModule],
+  imports: [CommonModule, SharedModule, CoreModule, ConstructorMaskedInputModule],
   exports: [PassportComponent],
 })
 export class PassportModule {}
