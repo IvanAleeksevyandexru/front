3fa4f6beab2556dc1acaa8904fbeacfb1188773e
shibkoas@elocont.ru <shibkoas@elocont.ru>
2020-12-03 17:38:34 +0300

    EPGUCORE-42911: добавлены тесты для ConfirmEmailComponent

--- projects/epgu-constructor/src/app/modal/screen-modal/components/unique-component-modal/confirm-email/confirm-email.component.spec.ts
+++ projects/epgu-constructor/src/app/modal/screen-modal/components/unique-component-modal/confirm-email/confirm-email.component.spec.ts
@@ -1,29 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { interval } from 'rxjs';
+
 import { ConfirmEmailComponent } from './confirm-email.component';
 import { ScreenService } from '../../../../../screen/screen.service';
 import { UnsubscribeService } from '../../../../../core/services/unsubscribe/unsubscribe.service';
 import { NavigationService } from '../../../../../core/services/navigation/navigation.service';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ScreenServiceStub } from '../../../../../screen/screen.service.stub';
 import { ConfigService } from '../../../../../core/config/config.service';
 import { ConfigServiceStub } from '../../../../../core/config/config.service.stub';
+import { CoreModule } from '../../../../../core/core.module';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { NavigationModalService } from '../../../../../core/services/navigation-modal/navigation-modal.service';
 
-xdescribe('ConfirmEmailComponent', () => {
+describe('ConfirmEmailComponent', () => {
   let component: ConfirmEmailComponent;
   let fixture: ComponentFixture<ConfirmEmailComponent>;
-
+  let navigationModalService: NavigationModalService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
-      declarations: [ ConfirmEmailComponent ],
+      declarations: [ConfirmEmailComponent],
+      imports: [CoreModule, SharedModule, RouterTestingModule],
       providers: [
         UnsubscribeService,
         NavigationService,
-        { provide: ScreenService,useClass: ScreenServiceStub },
-        { provide: ConfigService, useClass: ConfigServiceStub }
-      ]
-    })
-    .compileComponents();
+        { provide: ScreenService, useClass: ScreenServiceStub },
+        { provide: ConfigService, useClass: ConfigServiceStub },
+      ],
+    }).compileComponents();
+
+    navigationModalService = TestBed.inject(NavigationModalService);
   });
 
   beforeEach(() => {
@@ -35,7 +41,35 @@ xdescribe('ConfirmEmailComponent', () => {
     fixture.debugElement.injector.get(NavigationService);
   });
 
-  it('nothing', () => {
-    expect(true).toBeTruthy();
+  it('should be call this.navModalService.next() after 5sec', () => {
+    const navigationModalServiceNextFn = jest.spyOn(navigationModalService, 'next');
+    interval(5000).subscribe(() => {
+      expect(navigationModalServiceNextFn).toHaveBeenCalled();
+    });
+  });
+
+  describe('timerChange()', () => {
+    it('should be change this.timer', () => {
+      const timer = 4;
+      component.timerChange(timer);
+
+      expect(component.timer).toBe(timer);
+    });
+
+    it('should be change this.isTimerShow', () => {
+      component.timerChange(0);
+
+      expect(component.isTimerShow).toBe(false);
+    });
+  });
+
+  describe('resendEmailConfirmation()', () => {
+    it('should be call this.navModalService.next()', () => {
+      const navigationModalServiceNextFn = jest.spyOn(navigationModalService, 'next');
+      component.resendEmailConfirmation();
+
+      expect(navigationModalServiceNextFn).toHaveBeenCalled();
+      expect(component.isTimerShow).toBe(true);
+    });
   });
 });
