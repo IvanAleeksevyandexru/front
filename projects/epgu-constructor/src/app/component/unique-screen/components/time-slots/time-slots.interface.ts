73b7663b842d6592f75b1895340278189914a5a9
Valentin Perov <perovva@elocont.ru>
2020-12-07 13:01:10 +0400

    fix: добавил восстановление значения из кэша для таймслотов. Проверку уже забронированного слота [ref EPGUCORE-42878]

--- projects/epgu-constructor/src/app/component/unique-screen/components/time-slots/time-slots.interface.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/time-slots/time-slots.interface.ts
@@ -2,18 +2,19 @@ import { Observable } from 'rxjs';
 import {
   SlotInterface,
   SmevBookResponseInterface,
-  SmevSlotInterface,
   TimeSlotValueInterface
 } from './time-slots.types';
 
 export interface TimeSlotsServiceInterface {
+  bookId: string;
   init(data: TimeSlotValueInterface): Observable<void>;
   getCurrentYear(): number;
   getCurrentMonth(): number;
   getAvailableMonths(): string[];
   isDateLocked(date: Date): boolean;
-  getAvailableSlots(selectedDay: Date): Observable<SmevSlotInterface[]>;
+  getAvailableSlots(selectedDay: Date): Observable<SlotInterface[]>;
   getBookedSlot(): SlotInterface;
+  setBookedSlot(bookedSlot: SlotInterface): void;
   checkBooking(selectedSlot: SlotInterface): Observable<SmevBookResponseInterface>;
   book(selectedSlot: SlotInterface): Observable<SmevBookResponseInterface>;
   hasError(): boolean;
