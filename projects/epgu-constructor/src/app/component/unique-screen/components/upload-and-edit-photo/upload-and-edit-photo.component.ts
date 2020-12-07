854f09de9c6959d969e299a6c7ba6cdbea27fc32
Edward Yudinkov <anstertum2@gmail.com>
2020-12-07 11:59:27 +0300

    Add mime type for image cropper component

--- projects/epgu-constructor/src/app/component/unique-screen/components/upload-and-edit-photo/upload-and-edit-photo.component.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/upload-and-edit-photo/upload-and-edit-photo.component.ts
@@ -315,7 +315,7 @@ export class UploadAndEditPhotoComponent implements OnInit, OnDestroy {
         ? this.terabyteService.deleteFile(requestData).pipe(catchError(() => of(null)))
         : of(null);
     const compressFile = () => {
-      const blobFile = TerraByteApiService.base64toBlob(this.croppedImageUrl, 'image/jpeg');
+      const blobFile = TerraByteApiService.base64toBlob(this.croppedImageUrl);
       return fromPromise(this.compressionService.imageCompression(blobFile, { maxSizeMB: 5 }));
     };
     const uploadFile = (file: Blob | File) => {
