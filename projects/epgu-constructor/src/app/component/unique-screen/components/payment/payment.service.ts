e96cc220883ef6a97242c26cbee2a17caf1c212e
Alexander <a_zakusilo@mail.ru>
2020-12-08 17:16:36 +0300

    fix: Подправлена проверка локальной заглушки для оплаты брака / разбрака

--- projects/epgu-constructor/src/app/component/unique-screen/components/payment/payment.service.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/payment/payment.service.ts
@@ -58,7 +58,7 @@ export class PaymentService {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getUinByOrderId(orderId: string, code: number = 1, attributeValues: PaymentInfoInterface): Observable<any> {
     // TODO: Хардкод для локальной отладки подмена суммы оплаты пошлины
-    if (location.hostname.includes('test.gosuslugi.ru')) {
+    if (location.hostname.includes('local.test.gosuslugi.ru')) {
       attributeValues.sum = '200';
     }
 
