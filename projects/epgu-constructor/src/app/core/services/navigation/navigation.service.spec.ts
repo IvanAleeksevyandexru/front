355bf329d64e984fef57ee1c65231d8aaa8b2b8b
Aleksandr Kushnerevich <alexx.orlovv@gmail.com>
2020-12-09 19:14:34 +0300

    [EPGUCORE-43276] created tests for module core

--- projects/epgu-constructor/src/app/core/services/navigation/navigation.service.spec.ts
+++ projects/epgu-constructor/src/app/core/services/navigation/navigation.service.spec.ts
@@ -5,9 +5,15 @@ import { ConfigServiceStub } from '../../config/config.service.stub';
 import { ConfigService } from '../../config/config.service';
 import { DeviceDetectorService } from '../device-detector/device-detector.service';
 import { DeviceDetectorServiceStub } from '../device-detector/device-detector.service.stub';
+import { SmuEventsService } from 'epgu-lib';
+import { SmuEventsServiceStub } from '../device-detector/smu-events.service.stub';
+import { MobilViewEvents } from '../../../shared/constants/redirect-event';
 
-xdescribe('NavigationService', () => {
-  let service: NavigationService;
+describe('NavigationService', () => {
+  let navigationService: NavigationService;
+  let deviceDetectorService: DeviceDetectorService;
+  let configService: ConfigService;
+  let smuEventsService: SmuEventsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,12 +21,64 @@ xdescribe('NavigationService', () => {
         NavigationService,
         { provide: DeviceDetectorService, useClass: DeviceDetectorServiceStub },
         { provide: ConfigService, useClass: ConfigServiceStub },
-      ]
+        { provide: SmuEventsService, useClass: SmuEventsServiceStub },
+      ],
     });
-    service = TestBed.inject(NavigationService);
+    delete window.location;
+    window.location = { href: '' } as Location;
+    deviceDetectorService = TestBed.inject(DeviceDetectorService);
+    configService = TestBed.inject(ConfigService);
+    smuEventsService = TestBed.inject(SmuEventsService);
+    navigationService = TestBed.inject(NavigationService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('test skip', (done) => {
+    navigationService.skipStep$.subscribe((v) => {
+      expect(v).toBe(null);
+      done();
+    });
+    navigationService.skip(null);
+  });
+
+  it('test next', (done) => {
+    navigationService.nextStep$.subscribe((v) => {
+      expect(v).toBe(null);
+      done();
+    });
+    navigationService.next(null);
+  });
+
+  it('test prev', (done) => {
+    navigationService.prevStep$.subscribe((v) => {
+      expect(v).toBe(null);
+      done();
+    });
+    navigationService.prev(null);
+  });
+  it('test redirectToProfileEdit', () => {
+    navigationService.isWebView = true;
+    navigationService.redirectToProfileEdit();
+    expect(window.location.href).toBe('/profile/user');
+    navigationService.isWebView = false;
+    navigationService.redirectToProfileEdit();
+    expect(window.location.href).toBe(`${configService.lkUrl}/profile/personal`);
+  });
+  it('test redirectToLK', () => {
+    navigationService.isWebView = false;
+    navigationService.redirectToLK();
+    expect(window.location.href).toBe(`${configService.lkUrl}/orders/all`);
+    navigationService.isWebView = true;
+    spyOn(smuEventsService, 'notify').and.callThrough();
+    navigationService.redirectToLK();
+    expect(smuEventsService.notify).toHaveBeenCalledWith(MobilViewEvents.feed);
+  });
+  it('test redirectToHome', () => {
+    navigationService.isWebView = false;
+    navigationService.redirectToHome();
+    expect(window.location.href).toBe('/');
+    navigationService.isWebView = true;
+    spyOn(smuEventsService, 'notify').and.callThrough();
+    navigationService.redirectToHome();
+    expect(smuEventsService.notify).toHaveBeenCalledWith(MobilViewEvents.exit);
   });
 });
