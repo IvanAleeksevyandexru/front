f4a8f8096026b491905899179e24a2e8b37a053c
shibkoas@elocont.ru <shibkoas@elocont.ru>
2020-12-14 10:18:26 +0300

    EPGUCORE-43643: добавлена директива validation-type

---
+++ projects/epgu-constructor/src/app/shared/directives/validation-type/validation-type.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidationTypeDirective } from './validation-type.directive';
+
+describe('ValidationTypeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidationTypeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/epgu-constructor/src/app/shared/directives/validation-type/validation-type.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, HostListener, Input } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+import { CustomComponent } from '../../../component/components-list/components-list.types';
+import { ValidationService } from '../../../component/components-list/services/validation.service';
+
+@Directive({
+  selector: '[epgu-constructor-validation-type]',
+})
+export class ValidationTypeDirective {
+  @Input() component: CustomComponent;
+  @HostListener('blur')
+  blur(): void {
+    console.log(this.component);
+    this.validateOnBlur();
+  }
+
+  constructor(private validationService: ValidationService, private control: NgControl) {}
+
+  private validateOnBlur(): void {
+    const onBlurValidatorFns = this.validationService.customAsyncValidator(this.component, 'blur');
+    this.control.control.setAsyncValidators(onBlurValidatorFns);
+    this.control.control.updateValueAndValidity();
+    this.control.control.clearAsyncValidators();
+  }
+}
