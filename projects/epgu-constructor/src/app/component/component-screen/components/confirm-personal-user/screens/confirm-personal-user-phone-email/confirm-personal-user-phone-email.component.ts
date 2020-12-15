9c28e33268229b1860b976c52c8be60591a8dcc6
mark <mark.moskalenko@gmail.com>
2020-12-15 08:48:15 +0300

    feat: рефакторинг component-screen [ref EPGUCORE-43577]

--- projects/epgu-constructor/src/app/component/component-screen/components/confirm-personal-user/screens/confirm-personal-user-phone-email/confirm-personal-user-phone-email.component.ts
+++ projects/epgu-constructor/src/app/component/component-screen/components/confirm-personal-user/screens/confirm-personal-user-phone-email/confirm-personal-user-phone-email.component.ts
@@ -1,31 +1,41 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { ConfigService } from '../../../../../../core/config/config.service';
 import { CurrentAnswersService } from '../../../../../../screen/current-answers.service';
 import { ScreenService } from '../../../../../../screen/screen.service';
 import { ComponentBase } from '../../../../../../screen/screen.types';
 import { ComponentScreenComponentTypes } from '../../../../component-screen-components.types';
 import { DTOActionAction } from '../../../../../../form-player/services/form-player-api/form-player-api.types';
+import { UnsubscribeService } from '../../../../../../core/services/unsubscribe/unsubscribe.service';
 
 @Component({
   selector: 'epgu-constructor-confirm-personal-user-phone-email',
   templateUrl: './confirm-personal-user-phone-email.component.html',
   styleUrls: ['./confirm-personal-user-phone-email.component.scss'],
 })
-export class ConfirmPersonalUserPhoneEmailComponent implements OnChanges {
+export class ConfirmPersonalUserPhoneEmailComponent implements OnChanges, OnInit {
   data$: Observable<ComponentBase> = this.screenService.component$;
+  data: ComponentBase;
   isEditContactAction: boolean;
   componentScreenComponentTypes = ComponentScreenComponentTypes;
 
   constructor(
     private currentAnswersService: CurrentAnswersService,
+    private ngUnsubscribe$: UnsubscribeService,
     public screenService: ScreenService,
     public config: ConfigService,
   ) {}
 
+  ngOnInit(): void {
+    this.data$.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((data) => {
+      this.data = data;
+      this.isEditContactAction = this.getIsEditContactAction();
+    });
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     const { value } = changes.data.currentValue;
-    this.isEditContactAction = this.getIsEditContactAction();
     if (value) {
       this.currentAnswersService.isValid = true;
       this.currentAnswersService.state = this.data?.value;
