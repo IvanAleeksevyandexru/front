de69fad613a5e1e555d27d4d878d32e4fc6d139a
Roman Dashevskiy <rdashevskiy@luxoft.com>
2020-12-15 18:57:47 +0600

    feat: добавил regex [EPGUCORE-44023]

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-min-length': [2, 'always', 20],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(feat|fix|perf|test|BREAKING CHANGE):.*\[REF-(\d{3,}|N\\A)\] \S+ \S+ \S+ \S+ \S+/,
+      headerCorrespondence: ['type', 'scope', 'subject']
+    }
+  }
 };
