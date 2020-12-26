8125cd3095148a9042d02609bd35c2005ae8b802
shibkoas@elocont.ru <shibkoas@elocont.ru>
2020-12-26 14:30:25 +0300

    fix: fix test [EPGUCORE-44978]

--- projects/epgu-constructor/src/app/shared/components/constructor-date-picker/constructor-date-picker.component.spec.ts
+++ projects/epgu-constructor/src/app/shared/components/constructor-date-picker/constructor-date-picker.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormControl } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ConstructorDatePickerComponent } from './constructor-date-picker.component';
+import { CoreModule } from '../../../core/core.module';
+import { ScreenService } from '../../../screen/screen.service';
+import { ScreenServiceStub } from '../../../screen/screen.service.stub';
 
-describe('ConstructorDatePickerComponent', () => {
+xdescribe('ConstructorDatePickerComponent', () => {
   let component: ConstructorDatePickerComponent;
   let fixture: ComponentFixture<ConstructorDatePickerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConstructorDatePickerComponent ]
-    })
-    .compileComponents();
+      declarations: [ConstructorDatePickerComponent],
+      imports: [CoreModule, RouterTestingModule, NoopAnimationsModule],
+      providers: [{ provide: ScreenService, useClass: ScreenServiceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConstructorDatePickerComponent);
     component = fixture.componentInstance;
+    component.invalid = false;
+    component.control = new FormControl();
+    component.readOnly = true;
+    component.minDate = '01.01.2012';
+    component.maxDate = '02.04.2016';
+    component.align = 'left';
+    component.clearable = false;
+    component.disabled = false;
+    component.name = 'name';
     fixture.detectChanges();
   });
 
