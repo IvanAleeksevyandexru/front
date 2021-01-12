0e98806507ad75dd140804ab09473c607b023eec
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-01-12 22:41:18 +0600

    fix: поправил дефолтные значения targetId для обвертки [EPGUCORE-00000]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serviceId: '10000000100',
-  targetId: '10000000100',
+  targetId: '-10000000100',
   orderId: '',
   invited: false,
   core: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   serviceId: '10000000100',
-  targetId: '10000000100',
+  targetId: '-10000000100',
   orderId: '',
   invited: false,
   core: {}
