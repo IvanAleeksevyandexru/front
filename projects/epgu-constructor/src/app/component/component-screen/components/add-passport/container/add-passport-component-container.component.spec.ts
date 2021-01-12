a74ef91ff720a697d330da023da77ab217074da6
shibkoas@elocont.ru <shibkoas@elocont.ru>
2021-01-12 14:46:47 +0300

    feat: fix test [EPGUCORE-45357]

--- projects/epgu-constructor/src/app/component/component-screen/components/add-passport/container/add-passport-component-container.component.spec.ts
+++ projects/epgu-constructor/src/app/component/component-screen/components/add-passport/container/add-passport-component-container.component.spec.ts
@@ -2,24 +2,23 @@ import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HealthService } from 'epgu-lib';
 import { of } from 'rxjs';
-import { UnsubscribeService } from '../../../../../core/services/unsubscribe/unsubscribe.service';
-import { EventBusService } from '../../../../../form-player/services/event-bus/event-bus.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CurrentAnswersService } from '../../../../../screen/current-answers.service';
 import { ScreenService } from '../../../../../screen/screen.service';
 import { ScreenServiceStub } from '../../../../../screen/screen.service.stub';
 import { ComponentBase } from '../../../../../screen/screen.types';
 import { PassportModule } from '../../../../../shared/components/add-passport/passport.module';
-import { ScreenPadComponent } from '../../../../../shared/components/screen-pad/screen-pad.component';
-import { ComponentListToolsService } from '../../../../components-list/services/component-list-tools/component-list-tools.service';
 import { AddPassportContainerComponent } from './add-passport-component-container.component';
-import { PassportFormFields } from '../../../../../shared/components/add-passport/passport.interface';
 import { AddPassportComponent } from '../component/add-passport.component';
+import { Passport } from '../add-passport.models';
+import { ScreenPadComponent } from '../../../../../shared/components/screen-pad/screen-pad.component';
+import { EventBusService } from '../../../../../form-player/services/event-bus/event-bus.service';
 
 describe('AddPassportContainerComponent', () => {
   let component: AddPassportContainerComponent;
   let fixture: ComponentFixture<AddPassportContainerComponent>;
   let currentAnswersService: CurrentAnswersService;
-  let eventBusService: EventBusService;
   const mockData: ComponentBase = {
     attrs: { fields: [] },
     id: '',
@@ -31,15 +30,13 @@ describe('AddPassportContainerComponent', () => {
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [AddPassportContainerComponent, ScreenPadComponent, AddPassportComponent],
-      imports: [RouterTestingModule, PassportModule],
+      declarations: [AddPassportContainerComponent, AddPassportComponent, ScreenPadComponent],
+      imports: [RouterTestingModule, PassportModule, ReactiveFormsModule, FormsModule],
       providers: [
         CurrentAnswersService,
-        ComponentListToolsService,
         HealthService,
-        { provide: ScreenService, useClass: ScreenServiceStub },
         EventBusService,
-        UnsubscribeService,
+        { provide: ScreenService, useClass: ScreenServiceStub },
       ],
     }).compileComponents();
   }));
@@ -49,7 +46,6 @@ describe('AddPassportContainerComponent', () => {
     component = fixture.componentInstance;
     component.data$ = of(mockData);
     currentAnswersService = TestBed.inject(CurrentAnswersService);
-    eventBusService = TestBed.inject(EventBusService);
     fixture.detectChanges();
   });
 
@@ -57,18 +53,16 @@ describe('AddPassportContainerComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should be call onPassportDataChange()', () => {
-    spyOn(component, 'onPassportDataChange').and.callThrough();
-    eventBusService.emit('passportValueChangedEvent');
-    expect(component.onPassportDataChange).toHaveBeenCalled();
+  it('should change currentAnswersService state', () => {
+    const data: Passport = {
+      value: {
+        rfPasportNumber: '435345',
+        rfPasportSeries: '3454',
+      },
+      isValid: true,
+    };
+    component.onPassportDataChange(data);
+    expect(currentAnswersService.state).toEqual(JSON.stringify(data.value));
+    expect(currentAnswersService.isValid).toEqual(data.isValid);
   });
-
-  // it('should change currentAnswersService state', () => {
-  //   const data: PassportFormFields = {
-  //     rfPasportNumber: '435345',
-  //     rfPasportSeries: '3454',
-  //   };
-  //   component.onPassportDataChange(data);
-  //   expect(currentAnswersService.state).toEqual(data);
-  // });
 });
