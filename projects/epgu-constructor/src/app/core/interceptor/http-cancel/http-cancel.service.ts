70577ea1378c394993708a73561091f3c92f4d01
Edward Yudinkov <anstertum2@gmail.com>
2021-01-21 17:16:34 +0300

    [feature] Add request cancellation [ref EPGUCORE-46186]

---
+++ projects/epgu-constructor/src/app/core/interceptor/http-cancel/http-cancel.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpCancelService {
+  private cancelPendingRequests$ = new Subject<void>();
+
+  constructor() {}
+
+  public cancelPendingRequests(): void {
+    this.cancelPendingRequests$.next();
+  }
+
+  public onCancelPendingRequests(): Observable<void> {
+    return this.cancelPendingRequests$.asObservable();
+  }
+}
