a0688e7413d2cbbd6d02d2d36aad07c3bc85d130
Ohotnikov.A5 <ohotnikov.a5@wildberries.ru>
2021-01-29 18:06:27 +0300

    feat: add clickable label, refactor [EPGUCORE-46433]

--- projects/epgu-constructor/src/app/shared/components/output-html/output-html.module.ts
+++ projects/epgu-constructor/src/app/shared/components/output-html/output-html.module.ts
@@ -4,10 +4,11 @@ import { OutputHtmlComponent } from './output-html.component';
 import { SafeModule } from '../../pipes/safe/safe.module';
 import { ImgPrefixerModule } from '../../pipes/img-prefixer/img-prefixer.module';
 import { ActionModule } from '../../directives/action/action.module';
+import { ClickableLabelModule } from '../../directives/clickable-label/clickable-label.module';
 
 @NgModule({
   declarations: [OutputHtmlComponent],
-  imports: [CommonModule, ActionModule, SafeModule, ImgPrefixerModule],
+  imports: [CommonModule, ActionModule, SafeModule, ImgPrefixerModule, ClickableLabelModule],
   exports: [OutputHtmlComponent],
 })
 export class OutputHtmlModule {}
