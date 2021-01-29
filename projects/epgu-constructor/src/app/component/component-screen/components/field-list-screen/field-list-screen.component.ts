bab45c8dccbe9ca9647349cbad9fde16172e6eb2
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-01-29 12:38:38 +0300

    fix: недоступнка кнопка перехода[EPGUCORE-46760]

--- projects/epgu-constructor/src/app/component/component-screen/components/field-list-screen/field-list-screen.component.ts
+++ projects/epgu-constructor/src/app/component/component-screen/components/field-list-screen/field-list-screen.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UnsubscribeService } from '../../../../core/services/unsubscribe/unsubscribe.service';
 import { CurrentAnswersService } from '../../../../screen/current-answers.service';
 import { ScreenService } from '../../../../screen/screen.service';
+import { ComponentActionDto } from '../../../../form-player/services/form-player-api/form-player-api.types';
+import { NEXT_STEP_ACTION } from '../../../../shared/constants/actions';
 
 @Component({
   selector: 'epgu-constructor-field-list-screen',
@@ -11,6 +13,8 @@ import { ScreenService } from '../../../../screen/screen.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FieldListScreenComponent {
+  nextStepAction: ComponentActionDto = NEXT_STEP_ACTION;
+
   constructor(
     public currentAnswersService: CurrentAnswersService,
     public screenService: ScreenService,
