f02ab054793684aa7603c8410119cac989f78e63
Ohotnikov.A5 <ohotnikov.a5@wildberries.ru>
2021-01-29 18:08:30 +0300

    feat: add tests for label component [EPGUCORE-46716]

--- projects/epgu-constructor/src/app/shared/components/base-components/label/label.component.spec.ts
+++ projects/epgu-constructor/src/app/shared/components/base-components/label/label.component.spec.ts
@@ -1,27 +1,64 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LabelComponent } from './label.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SafePipe } from '../../../pipes/safe/safe.pipe';
+import { ImgPrefixerPipe } from '../../../pipes/img-prefixer/img-prefixer.pipe';
+import { ClickableLabelDirective } from '../../../directives/clickable-label/clickable-label.directive';
+import { ConfigService } from '../../../../core/services/config/config.service';
+import { ConfigServiceStub } from '../../../../core/services/config/config.service.stub';
+import { ModalService } from '../../../../modal/modal.service';
+import { ModalServiceStub } from '../../../../modal/modal.service.stub';
+import { ScreenService } from '../../../../screen/screen.service';
+import { ScreenServiceStub } from '../../../../screen/screen.service.stub';
+import { ActionService } from '../../../directives/action/action.service';
+import { ActionServiceStub } from '../../../directives/action/action.service.stub';
+import { By } from '@angular/platform-browser';
 
 describe('LabelComponent', () => {
   let component: LabelComponent;
   let fixture: ComponentFixture<LabelComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
-      declarations: [ LabelComponent ]
-    })
-    .compileComponents();
-  }));
+  let modalService: ModalService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LabelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
+      declarations: [LabelComponent, SafePipe, ImgPrefixerPipe, ClickableLabelDirective],
+      providers: [
+        { provide: ConfigService, useClass: ConfigServiceStub },
+        { provide: ModalService, useClass: ModalServiceStub },
+        { provide: ScreenService, useClass: ScreenServiceStub },
+        { provide: ActionService, useClass: ActionServiceStub },
+      ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(LabelComponent);
+      component = fixture.componentInstance;
+      modalService = TestBed.inject(ModalService);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should open modal if clarifications are set', () => {
+    component.label = '<p><a id="test">Ссылка</a></p>';
+    component.clarifications = { test: { text: '', title: '' }};
+    fixture.detectChanges();
+
+    const span: HTMLDivElement = fixture.debugElement.query(By.css('span')).nativeElement;
+
+    spyOn(modalService, 'openModal').and.callThrough();
+    span.querySelector('a').click();
+    expect(modalService.openModal).toHaveBeenCalled();
+  });
+
+  describe('div', () => {
+    it('should contain html', () => {
+      const testText = 'Test text';
+      const testHtml = `<p>${testText}</p>`;
+      component.label = testHtml;
+      fixture.detectChanges();
+
+      const span: HTMLDivElement = fixture.debugElement.query(By.css('span')).nativeElement;
+
+      expect(span.textContent).toContain(testText);
+      expect(span.innerHTML).toContain(testHtml);
+    });
   });
 });
