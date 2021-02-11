58e15576d4023ab54a7ece93370f184140c30c97
Edward Yudinkov <anstertum2@gmail.com>
2021-02-11 10:53:25 +0300

    [fix] Fix dictionary interceptor [ref epgucore-47319]

--- projects/epgu-constructor/src/app/core/interceptor/health/health.interceptor.ts
+++ projects/epgu-constructor/src/app/core/interceptor/health/health.interceptor.ts
@@ -65,19 +65,11 @@ export class HealthInterceptor implements HttpInterceptor {
             };
           }
 
-          if (!(Object.keys(this.configParams).length === 0)) {
-            const { id, name, orderId } = this.configParams;
-            successRequestPayload = { id, name, orderId };
-            successRequestPayload = this.utils.filterIncorrectObjectFields(
-              successRequestPayload,
-            ) as ConfigParams;
-          }
-
-          if (result?.error && result.error?.core !== 0 || undefined && result?.error?.message !== '' || undefined) {
+          if (this.utils.isDefined(result.error) && this.utils.isDefined(result.error.code) && Number(result.error.code) !== 0) {
             successRequestPayload = { 
               ...successRequestPayload, 
               error: result.error.code,
-              errorMessage: result.error.message,
+              errorMessage: this.utils.isDefined(result.error.message) ? result.error.message : null,
             };
             dictionaryValidationStatus = true;
           }
