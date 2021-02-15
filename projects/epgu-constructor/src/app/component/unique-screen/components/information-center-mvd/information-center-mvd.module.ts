726c7f4effe1d37fbd25f20604fa9fd23b58b617
shibkoas@elocont.ru <shibkoas@elocont.ru>
2021-02-15 11:57:02 +0300

    feat: добавлен wrapper для information-center-mvd [EPGUCORE-48142]

--- projects/epgu-constructor/src/app/component/unique-screen/components/information-center-mvd/information-center-mvd.module.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/information-center-mvd/information-center-mvd.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { InformationCenterMvdContainerComponent } from './container/information-center-mvd-container.component';
 import { CoreModule } from '../../../../core/core.module';
 import { ConstructorDropdownModule } from '../../../../shared/components/constructor-dropdown/constructor-dropdown.module';
-import { ScreenContainerModule } from '../../../../shared/components/screen-container/screen-container.module';
 import { ScreenPadModule } from '../../../../shared/components/screen-pad/screen-pad.module';
 import { ActionModule } from '../../../../shared/directives/action/action.module';
 import { InformationCenterCardComponent } from './components/information-center-card/information-center-card.component';
 import { BaseModule } from '../../../../shared/base.module';
 import { BaseComponentsModule } from '../../../../shared/components/base-components/base-components.module';
 import { InformationCenterFormComponent } from './components/information-center-form/information-center-form.component';
+import { DefaultUniqueScreenWrapperModule } from '../../shared/default-unique-screen-wrapper/default-unique-screen-wrapper.module';
 
 const COMPONENTS = [
   InformationCenterMvdContainerComponent,
@@ -21,11 +21,11 @@ const COMPONENTS = [
   imports: [
     CoreModule,
     ConstructorDropdownModule,
-    ScreenContainerModule,
     ScreenPadModule,
     BaseModule,
     ActionModule,
     BaseComponentsModule,
+    DefaultUniqueScreenWrapperModule,
   ],
   exports: [...COMPONENTS],
   entryComponents: [InformationCenterMvdContainerComponent],
