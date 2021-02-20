e3c64223c72fd06b7d02161bc3f03b83aa194877
Aleksandr Kushnerevich <kushnerevichaa@elocont.ru>
2021-02-20 16:45:01 +0300

    fix: html start [EPGUCORE-46675]

--- projects/epgu-constructor/src/app/shared/components/uploader/components/uploader-viewer/uploader-viewer.component.ts
+++ projects/epgu-constructor/src/app/shared/components/uploader/components/uploader-viewer/uploader-viewer.component.ts
@@ -1,23 +1,16 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
-  Injector,
   Input,
-  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
 
-import { Observable } from 'rxjs';
-
-import { filter, map, tap } from 'rxjs/operators';
 import { ModalBaseComponent } from '../../../../../modal/shared/modal-base/modal-base.component';
 import { FileItem } from '../../../../../component/unique-screen/components/file-upload-screen/sub-components/file-upload-item/data';
-import { FilesCollection, SuggestAction, ViewerInfo } from '../../data';
-// eslint-disable-next-line import/no-cycle
-import { ViewerService } from '../../services/viewer/viewer.service';
+import { FilesCollection, SuggestAction } from '../../data';
+
 import { ZoomComponent } from '../../../zoom/zoom.component';
 
 @Component({
@@ -26,7 +19,7 @@ import { ZoomComponent } from '../../../zoom/zoom.component';
   styleUrls: ['./uploader-viewer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UploaderViewerComponent extends ModalBaseComponent implements AfterViewInit, OnInit {
+export class UploaderViewerComponent extends ModalBaseComponent {
   @ViewChild('zoomComponent') zoomComponent!: ZoomComponent;
   @Input() type: FilesCollection;
 
@@ -36,78 +29,13 @@ export class UploaderViewerComponent extends ModalBaseComponent implements After
   @Output() next = new EventEmitter<FilesCollection>();
   @Output() prev = new EventEmitter<FilesCollection>();
 
-  selectedItem: Observable<ViewerInfo>;
-
-  item: FileItem;
-  imageURL: string;
   filesType = FilesCollection;
   isMoveZoom = false;
-  size: number;
-  position: number;
-  zoom: Observable<{ zoom: number; max: number }>;
-  constructor(public injector: Injector, private viewerService: ViewerService) {
-    super(injector);
-  }
 
   onClickComponent(event: MouseEvent): void {
     if (!this.isMoveZoom) {
       super.onClickComponent(event);
     }
     this.isMoveZoom = false;
-  }
-
-  zoomMoveEnd(): void {
-    this.isMoveZoom = true;
-  }
-
-  init({ file, size, position }): void {
-    this.size = size;
-    this.position = position;
-    this.item = file;
-    this.zoomComponent?.resetZoom();
-    this.imageURL = file.isImage ? file.urlToFile() : null;
-  }
-
-  zoomIn(): void {
-    this.zoomComponent?.zoomIn();
-  }
-
-  zoomOut(): void {
-    this.zoomComponent?.resetZoom();
-  }
-
-  downloadAction(): void {
-    this.download.emit(this.item);
-  }
-
-  nextAction(): void {
-    this.next.emit(this.type);
-  }
-
-  deleteAction(): void {
-    if (window.confirm('Удалить навсегда?')) {
-      this.delete.emit(this.item);
-    }
-  }
-
-  prevAction(): void {
-    this.prev.emit(this.type);
-  }
-
-  suggestAction(isAdd: boolean): void {
-    this.suggest.emit({ file: this.item, isAdd });
-  }
-
-  ngAfterViewInit(): void {
-    this.zoom = this.zoomComponent?.zoom$$.pipe(
-      map((zoom: number) => ({ zoom, max: this.zoomComponent.maxZoom })),
-    );
-  }
-
-  ngOnInit(): void {
-    this.selectedItem = this.viewerService.getSelectedFileByType(this.type).pipe(
-      filter((info) => !!info),
-      tap((info) => this.init(info)),
-    );
   }
 }
