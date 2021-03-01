aaa13489d59d6200a9d7bd836817a50df3e24459
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-01 17:12:54 +0300

    fix: Временно onPush с Checkbox т.к не отрабатывал disabled[EPGUCORE-49043]

--- projects/epgu-constructor/src/app/shared/components/constructor-checkbox/constructor-checkbox.component.ts
+++ projects/epgu-constructor/src/app/shared/components/constructor-checkbox/constructor-checkbox.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'epgu-constructor-constructor-checkbox',
   templateUrl: './constructor-checkbox.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConstructorCheckboxComponent {
   @Input() checkboxId: string;
