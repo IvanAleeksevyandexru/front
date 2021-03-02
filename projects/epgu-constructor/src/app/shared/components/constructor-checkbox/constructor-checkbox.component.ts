cf103d488c3b4665e411ef240d6b3ab66729d6e8
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-02 11:23:43 +0300

    fix: Правки по ревью[EPGUCORE-49043]

--- projects/epgu-constructor/src/app/shared/components/constructor-checkbox/constructor-checkbox.component.ts
+++ projects/epgu-constructor/src/app/shared/components/constructor-checkbox/constructor-checkbox.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'epgu-constructor-constructor-checkbox',
   templateUrl: './constructor-checkbox.component.html',
+  changeDetection: ChangeDetectionStrategy.Default, // @todo поменять на OnPush
 })
 export class ConstructorCheckboxComponent {
   @Input() checkboxId: string;
