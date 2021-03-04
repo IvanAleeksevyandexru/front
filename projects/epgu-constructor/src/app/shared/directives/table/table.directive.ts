41414b2e961b8dc63688d3f48982ae1f5551b791
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-04 17:12:05 +0300

    fix: Правки по ревью + тесты[EPGUCORE-48793]

--- projects/epgu-constructor/src/app/shared/directives/table/table.directive.ts
+++ projects/epgu-constructor/src/app/shared/directives/table/table.directive.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[epgu-constructor-table]'
@@ -6,28 +6,36 @@ import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 export class TableDirective implements AfterViewInit {
   tableClass = '.epgu-constructor-table';
   attrName = 'data-header';
-  constructor(private elRef: ElementRef) {}
 
-  ngAfterViewInit (): void {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
     const tables = this.elRef.nativeElement.querySelectorAll(this.tableClass) as HTMLTableElement[];
+
     if (tables.length) {
       tables.forEach(({ rows }: HTMLTableElement) => {
         let headAttrs;
+
         Array.from(rows).forEach((row: HTMLTableRowElement, index: number) => {
           const cellsArr = Array.from(row.cells);
+
           if (index < 1) {
             headAttrs = cellsArr.map((cell: HTMLTableDataCellElement) => cell.getAttribute(this.attrName));
           }
+
           cellsArr.forEach((cell: HTMLTableDataCellElement, i: number) => {
-            const headerEl = document.createElement('div');
-            headerEl.className = 'header';
-            if(!cell.hasAttribute(this.attrName)) {
-              headerEl.innerText = headAttrs[i].replace(/(<([^>]+)>)/gi, '');
-              cell.firstChild.before(headerEl);
-            } else {
-              cell.innerHTML = headAttrs[i];
-            }
-          });
+              const headerEl = this.renderer.createElement('div');
+              this.renderer.addClass(headerEl, 'header');
+
+              if (cell.hasAttribute(this.attrName)) {
+                this.renderer.setProperty(cell, 'innerHTML', headAttrs[i]);
+              } else {
+                const text = (headAttrs[i] || '').replace(/(<([^>]+)>)/gi, '');
+                const headerText = this.renderer.createText(text);
+                this.renderer.appendChild(headerEl, headerText);
+                this.renderer.insertBefore(cell.firstChild.parentNode, headerEl, cell.firstChild);
+              }
+            });
         });
       });
     }
