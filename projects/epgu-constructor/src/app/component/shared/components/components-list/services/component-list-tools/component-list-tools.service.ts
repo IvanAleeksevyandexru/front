73ae0009052d1eefe74c90f0e4573c19d108ff19
Valentin Perov <perovva@elocont.ru>
2021-03-09 10:43:55 +0400

    Правки по ревью

--- projects/epgu-constructor/src/app/component/shared/components/components-list/services/component-list-tools/component-list-tools.service.ts
+++ projects/epgu-constructor/src/app/component/shared/components/components-list/services/component-list-tools/component-list-tools.service.ts
@@ -531,8 +531,11 @@ export class ComponentListToolsService {
     if (dependentControl) {
       const relatedDate = component.value !== '' ? new Date(component.value) : null;
       const { attrs, id, value } = dependentControl.value;
-      const minDate = await this.dateRangeService.getMinDate(attrs.ref, id, relatedDate);
-      const maxDate = await this.dateRangeService.getMaxDate(attrs.ref, id, relatedDate);
+      const [minDate, maxDate] = await Promise.all([
+        this.dateRangeService.getMinDate(attrs.ref, id, relatedDate),
+        this.dateRangeService.getMaxDate(attrs.ref, id, relatedDate)
+      ]);
+      
       this.dateRangeService.changeDate(attrs.ref, relatedDate);
 
       dependentControl.get('attrs').patchValue({
