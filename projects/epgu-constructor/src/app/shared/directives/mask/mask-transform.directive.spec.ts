dc4f88006576e3dc2a2aadc975a986e9e97f6bf4
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-10 17:15:47 +0300

    fix: тесты[EPGUCORE-49675]

--- projects/epgu-constructor/src/app/shared/directives/mask/mask-transform.directive.spec.ts
+++ projects/epgu-constructor/src/app/shared/directives/mask/mask-transform.directive.spec.ts
@@ -1,32 +1,44 @@
 import { MaskTransformDirective } from './mask-transform.directive';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DecimalPipe } from '@angular/common';
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { NgControl } from '@angular/forms';
+import { FormControl, NgControl } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoreModule } from '../../../core/core.module';
+import { BaseModule } from '../../base.module';
+import { ScreenService } from '../../../screen/screen.service';
+import { ScreenServiceStub } from '../../../screen/screen.service.stub';
 
 @Component({
   selector: 'epgu-constructor-mask-transform-test-component',
-  template: `<input
-    type="text"
-    [epgu-constructor-mask-transform]="mask"
-    [maskOptions]="maskOptions"
-  />`,
+  template: `
+    <input
+      type="text"
+      [epgu-constructor-mask-transform]="mask"
+      [maskOptions]="maskOptions"
+      [formControl]="control"
+    />`,
 })
 class MaskTransformTestComponent {
   public mask = '';
   public maskOptions = {};
+  control =  new FormControl({ value: '' });
 }
 
 describe('MaskTransformDirective', () => {
-  let fixture: ComponentFixture<MaskTransformTestComponent>;
   let component: MaskTransformTestComponent;
+  let fixture: ComponentFixture<MaskTransformTestComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [MaskTransformDirective, MaskTransformTestComponent],
-      providers: [DecimalPipe, NgControl],
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [CoreModule, RouterTestingModule, BaseModule],
+      providers: [
+        DecimalPipe,
+        NgControl,
+        { provide: ScreenService, useClass: ScreenServiceStub },
+      ],
     })
       .compileComponents()
       .then(() => {
