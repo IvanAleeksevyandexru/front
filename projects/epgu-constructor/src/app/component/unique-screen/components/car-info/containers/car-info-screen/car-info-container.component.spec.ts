32490e72fbaa89c6e561d1ee0a09c8e1139f97c1
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-12 16:36:34 +0300

    fix: Вывод паспорта ownerInfo[EPGUCORE-49999]

--- projects/epgu-constructor/src/app/component/unique-screen/components/car-info/containers/car-info-screen/car-info-container.component.spec.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/car-info/containers/car-info-screen/car-info-container.component.spec.ts
@@ -33,7 +33,6 @@ import { CarOwnerInfoComponent } from '../../components/car-owner-info/car-owner
 import { CarOwnersComponent } from '../../components/car-owners/car-owners.component';
 import { LegalComplianceComponent } from '../../components/legal-compliance/legal-compliance.component';
 import { NotaryInfoComponent } from '../../components/notary-info/notary-info.component';
-import { DocumentInfoPipe } from '../../pipes/document-info.pipe';
 import { DefaultUniqueScreenWrapperModule } from '../../../../shared/default-unique-screen-wrapper/default-unique-screen-wrapper.module';
 
 describe('CarInfoContainerComponent', () => {
@@ -57,7 +56,6 @@ describe('CarInfoContainerComponent', () => {
         EnginePowerPipe,
         ModelMarkNamePipe,
         CarDatePipe,
-        DocumentInfoPipe,
         DefaultValuePipe,
       ],
       providers: [
