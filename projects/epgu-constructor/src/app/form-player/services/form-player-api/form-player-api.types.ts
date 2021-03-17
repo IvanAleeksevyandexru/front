88fe6fddf1ef11fe8de90d9ad8ae06e0e3a2215f
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-03-17 18:29:45 +0300

    feat: Добавлен canDeleteFirstScreen и доработан onlyFistSceen для repeatebleFields[EPGUCORE-50516]

--- projects/epgu-constructor/src/app/form-player/services/form-player-api/form-player-api.types.ts
+++ projects/epgu-constructor/src/app/form-player/services/form-player-api/form-player-api.types.ts
@@ -152,6 +152,7 @@ export interface ComponentAttrsDto {
   endDate?: ComponentDateTimeDto;
   beginTime?: ComponentDateTimeDto;
   endTime?: ComponentDateTimeDto;
+  canDeleteFirstScreen?: boolean;
 }
 
 export interface ComponentDateTimeDto {
