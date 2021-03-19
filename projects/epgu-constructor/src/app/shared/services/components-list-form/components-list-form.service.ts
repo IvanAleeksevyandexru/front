af95172984f132b35befb8c34a4d2c72ba5f4d34
Daniil Vysotskiy <dvysotskiy@it-one.ru>
2021-03-19 16:21:32 +0300

    fix: хотфикс с недостающими параметрами компонента [EPGUCORE-50406]

--- projects/epgu-constructor/src/app/shared/services/components-list-form/components-list-form.service.ts
+++ projects/epgu-constructor/src/app/shared/services/components-list-form/components-list-form.service.ts
@@ -278,13 +278,15 @@ export class ComponentsListFormService {
       validators.push(this.validationService.dateValidator(component));
     }
 
-    const { type, attrs, id } = component;
+    const { type, attrs, id, label, required } = component;
 
     const form: FormGroup = this.fb.group(
       {
         type,
         attrs,
         id,
+        label,
+        required,
         value: [
           {
             value: this.componentsListToolsService.convertedValue(component),
