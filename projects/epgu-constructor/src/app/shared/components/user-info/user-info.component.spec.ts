e33cbe7e6961ad5861eab2099cb5c08d21af36a8
rdashevskiy@it-one.ru <rdashevskiy@it-one.ru>
2021-03-29 07:50:09 +0000

    Merge branch 'epgucore-51829' into 'release/10'
    fix: change user info component [EPGUCORE-51829]
    
    See merge request luxoft/epgu2-form-frontend!1769
    
    (cherry picked from commit 408113cef11f95e41d13566645a365defe521346)
    
    66bb08b6 fix: change user info component [EPGUCORE-51829]
    61902ea7 fix: change test [EPGUCORE-51829]
    42e77960 fix: change condition [EPGUCORE-51829]

--- projects/epgu-constructor/src/app/shared/components/user-info/user-info.component.spec.ts
+++ projects/epgu-constructor/src/app/shared/components/user-info/user-info.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserInfoComponent } from './user-info.component';
 import { AgeType, Gender, UserInfoType } from './user-info.type';
 import { ConfigService } from '../../../core/services/config/config.service';
+import { UtilsService } from '../../../core/services/utils/utils.service';
+
 import { ConfigServiceStub } from '../../../core/services/config/config.service.stub';
 
 describe('UserInfoComponent', () => {
@@ -17,7 +19,7 @@ describe('UserInfoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UserInfoComponent],
-      providers: [{ provide: ConfigService, useClass: ConfigServiceStub }],
+      providers: [UtilsService, { provide: ConfigService, useClass: ConfigServiceStub }],
     }).compileComponents();
   }));
 
