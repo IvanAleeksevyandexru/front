c36cafdd0ccf2a8d86a768021a8566efc1648980
Roman Dashevskiy <rdashevskiy@it-one.ru>
2021-03-31 21:45:55 +0600

    fix: убрал onPush с DateInputComponent [EPGUCORE-00000]

--- projects/epgu-constructor/src/app/shared/components/components-list/components/date-input/date-input.component.ts
+++ projects/epgu-constructor/src/app/shared/components/components-list/components/date-input/date-input.component.ts
@@ -8,7 +8,7 @@ import { AbstractComponentListItemComponent } from '../abstract-component-list-i
 @Component({
   selector: 'epgu-constructor-date-input',
   templateUrl: './date-input.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default, // TODO: нужно сделать onPush
   providers: [UnsubscribeService],
 })
 export class DateInputComponent extends AbstractComponentListItemComponent {
