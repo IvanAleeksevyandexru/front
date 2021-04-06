5b5dac1dc1aa40673b296aec91598249da6ba0ca
Alexsandr Shibko <alexsandrshibko@MacBook-Pro-Alexsandr.local>
2021-04-06 17:45:05 +0300

    feat: test component-list-modal [EPGUCORE-47750]

--- projects/epgu-constructor/src/app/modal/screen-modal/components/component-list-modal/component-list-modal.component.spec.ts
+++ projects/epgu-constructor/src/app/modal/screen-modal/components/component-list-modal/component-list-modal.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { EpguLibModule } from 'epgu-lib';
-import { MockModule } from 'ng-mocks';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { ComponentListModalComponent } from './component-list-modal.component';
 import { ScreenService } from '../../../../screen/screen.service';
 import { ScreenTypes } from '../../../../screen/screen.types';
@@ -11,18 +11,16 @@ import { ScreenModalServiceStub } from '../../screen-modal.service.stub';
 import { CustomScreenService } from '../../../../screen/custom-screen/custom-screen.service';
 import { DatesToolsService } from '../../../../core/services/dates-tools/dates-tools.service';
 import { ComponentsListModule } from '../../../../component/custom-screen/components-list.module';
-import { UniqueComponentModalModule } from '../unique-component-modal/unique-component-modal.module';
-import { InfoComponentModalModule } from '../info-component-modal/info-component-modal.module';
-import { RouterTestingModule } from '@angular/router/testing';
 import { DictionaryToolsService } from '../../../../shared/services/dictionary/dictionary-tools.service';
 import { RefRelationService } from '../../../../shared/services/ref-relation/ref-relation.service';
+import { BaseModule } from '../../../../shared/base.module';
+import { NavigationModalServiceStub } from '../../../../core/services/navigation-modal/navigation-modal.service.stub';
 
-//TODO дописать тесты
 describe('ComponentListModalComponent', () => {
   let component: ComponentListModalComponent;
   let fixture: ComponentFixture<ComponentListModalComponent>;
   let screenService: ScreenService;
-
+  let navModalService: NavigationModalService;
   const screenDataMock = {
     components: [],
     terminal: false,
@@ -35,18 +33,16 @@ describe('ComponentListModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        MockModule(EpguLibModule),
-        ComponentsListModule,
-        UniqueComponentModalModule,
-        InfoComponentModalModule
-      ],
       declarations: [
         ComponentListModalComponent
       ],
+      imports: [
+        BaseModule,
+        ComponentsListModule,
+        RouterTestingModule
+      ],
       providers: [
-        NavigationModalService,
+        { provide: NavigationModalService, useClass: NavigationModalServiceStub },
         { provide: ScreenService, useClass: ScreenServiceStub },
         { provide: ScreenModalService, useClass: ScreenModalServiceStub },
         CustomScreenService,
@@ -55,19 +51,74 @@ describe('ComponentListModalComponent', () => {
         RefRelationService
       ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    screenService = TestBed.inject(ScreenService);
+    navModalService = TestBed.inject(NavigationModalService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ComponentListModalComponent);
     component = fixture.componentInstance;
-    screenService = TestBed.inject(ScreenService);
-
     screenService.display = screenDataMock as any;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be call navModalService.next', () => {
+    const params = {};
+    const spy = jest.spyOn(navModalService, 'next');
+    component.nextStep(params);
+    expect(spy).toHaveBeenCalledWith(params);
+  });
+
+  it('should be call navModalService.prevStep', () => {
+    const params = {};
+    const spy = jest.spyOn(navModalService, 'prev');
+    component.prevStep(params);
+    expect(spy).toHaveBeenCalledWith(params);
+  });
+
+  it('should be call nextStep', () => {
+    const payload = {
+      pd1: {
+        value: 'true',
+        visited: true
+      }
+    };
+    component.dataToSend = payload;
+    const spy = jest.spyOn(component, 'nextStep');
+    component.nextScreen();
+    expect(spy).toHaveBeenCalledWith({
+      payload
+    });
+  });
+
+  describe('changeComponentsList', () => {
+    const payload = {
+      pd1: {
+        value: 'true',
+        valid: true,
+        isValid: true,
+      }
+    };
+
+    it('should be change isValid', () => {
+      component.changeComponentsList(payload);
+      expect(component.isValid).toBeTruthy();
+    });
+
+    it('should be change dataToSend', () => {
+      component.changeComponentsList(payload);
+      expect(component.dataToSend).toEqual({
+        pd1: {
+          value: 'true',
+          visited: true
+        }
+      });
+    });
   });
 });
