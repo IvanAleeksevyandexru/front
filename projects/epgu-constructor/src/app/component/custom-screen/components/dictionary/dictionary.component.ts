0a7a6889af683dea813fcfe067236ec4e9a55644
Alexsandr Shibko <alexsandrshibko@MacBook-Pro-Alexsandr.local>
2021-04-14 12:00:16 +0300

    feat: добавлен onPush для dictionary [EPGUCORE-50838]

--- projects/epgu-constructor/src/app/component/custom-screen/components/dictionary/dictionary.component.ts
+++ projects/epgu-constructor/src/app/component/custom-screen/components/dictionary/dictionary.component.ts
@@ -1,31 +1,23 @@
 import { Component, ChangeDetectionStrategy, Injector } from '@angular/core';
 import { ValidationShowOn } from 'epgu-lib';
+import { map } from 'rxjs/operators';
 
 import { AbstractComponentListItemComponent } from '../abstract-component-list-item/abstract-component-list-item.component';
-import { SuggestHandlerService } from '../../../../shared/services/suggest-handler/suggest-handler.service';
 import { DictionaryToolsService } from '../../../../shared/services/dictionary/dictionary-tools.service';
-import { ScreenService } from '../../../../screen/screen.service';
-import { CustomComponent } from '../../components-list.types';
 import { UtilsService } from '../../../../core/services/utils/utils.service';
 
 @Component({
   selector: 'epgu-constructor-dictionary',
   templateUrl: './dictionary.component.html',
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DictionaryComponent extends AbstractComponentListItemComponent {
   validationShowOn = ValidationShowOn.TOUCHED_UNFOCUSED;
+  list$ = this.dictionaryToolsService.dictionaries$.pipe(
+    map((dictionaries) => dictionaries[UtilsService.getDictKeyByComp(this.control.value)]?.list),
+  );
 
-  constructor(
-    public injector: Injector,
-    public suggestHandlerService: SuggestHandlerService,
-    public dictionaryToolsService: DictionaryToolsService,
-    public screenService: ScreenService,
-  ) {
+  constructor(public injector: Injector, public dictionaryToolsService: DictionaryToolsService) {
     super(injector);
-  }
-
-  public getDictKeyByComp(component: CustomComponent): string {
-    return UtilsService.getDictKeyByComp(component);
   }
 }
