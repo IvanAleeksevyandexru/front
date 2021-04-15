69953e6b08ba3e1309a72dd73cdcf24ae5922d2a
Aleksandr Kushnerevich <kushnerevichaa@elocont.ru>
2021-04-15 21:35:23 +0300

    feat: added maxCountByTypes [EPGUCORE-50954]

--- projects/epgu-constructor/src/app/component/unique-screen/components/file-upload-screen/file-upload-screen.component.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/file-upload-screen/file-upload-screen.component.ts
@@ -70,6 +70,7 @@ export class FileUploadScreenComponent implements OnInit {
       .on('fileUploadValueChangedEvent')
       .pipe(takeUntil(this.ngUnsubscribe$))
       .subscribe((payload: FileResponseToBackendUploadsItem) => {
+        console.log(payload);
         this.handleNewValueSet(payload);
         this.currentAnswersService.state = this.value;
         this.cdr.markForCheck();
@@ -103,6 +104,7 @@ export class FileUploadScreenComponent implements OnInit {
    * @param $eventData - данные из компонента
    */
   private handleNewValueSet($eventData: FileResponseToBackendUploadsItem): void {
+    console.log($eventData);
     this.value.uploads = $eventData.files as FileUploadEmitValue[];
 
     /**
