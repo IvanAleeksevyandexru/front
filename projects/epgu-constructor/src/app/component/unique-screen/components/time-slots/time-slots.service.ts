63658d7f4318b070e8067b880c0f0c2190035c31
Valentin Perov <perovva@elocont.ru>
2021-04-19 16:52:46 +0400

    fix: исправил парсинг в слотах с visitTimeISO на visitTimeStr [EPGUCORE-54044]

--- projects/epgu-constructor/src/app/component/unique-screen/components/time-slots/time-slots.service.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/time-slots/time-slots.service.ts
@@ -407,7 +407,7 @@ export class TimeSlotsService {
 
   private initSlotsMap(slots: TimeSlot[]): void {
     slots.forEach((slot) => {
-      const slotDate = new Date(slot.visitTimeISO);
+      const slotDate = new Date(slot.visitTimeStr);
       if (!this.slotsMap[slotDate.getFullYear()]) {
         this.slotsMap[slotDate.getFullYear()] = {};
       }
