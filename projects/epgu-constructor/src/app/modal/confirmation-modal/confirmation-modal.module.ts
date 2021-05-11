c799e448a34e27ed6d2d18a85357078a789870d7
shibkoas@elocont.ru <shibkoas@elocont.ru>
2021-05-11 16:58:35 +0300

    fix: исправлен фокус в модалках [EPGUCORE-55418]

--- projects/epgu-constructor/src/app/modal/confirmation-modal/confirmation-modal.module.ts
+++ projects/epgu-constructor/src/app/modal/confirmation-modal/confirmation-modal.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+
 import { ModalService } from '../modal.service';
 import { ConfirmationModalBaseComponent } from './confirmation-modal-base/confirmation-modal-base.component';
 import { ConfirmationModalComponent } from './confirmation-modal.component';
 import { BaseModule } from '../../shared/base.module';
 
 @NgModule({
-  declarations: [
-    ConfirmationModalComponent,
-    ConfirmationModalBaseComponent
-  ],
-  providers: [
-    ModalService,
-  ],
-  exports: [
-    ConfirmationModalComponent,
-    ConfirmationModalBaseComponent
-  ],
-  imports: [BaseModule],
-  entryComponents: [
-    ConfirmationModalComponent,
-    ConfirmationModalBaseComponent
-  ],
+  declarations: [ConfirmationModalComponent, ConfirmationModalBaseComponent],
+  providers: [ModalService],
+  exports: [ConfirmationModalComponent, ConfirmationModalBaseComponent],
+  imports: [BaseModule, A11yModule],
+  entryComponents: [ConfirmationModalComponent, ConfirmationModalBaseComponent],
 })
 export class ConfirmationModalModule {}
