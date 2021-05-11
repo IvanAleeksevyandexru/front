dbe01a9e114c20696cf931f64c94f88300a05bea
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-05-11 18:36:34 +0600

    fix: убрал проверку того же экрана с ошибками для component-list-resolver.component [EPGUCORE-55762]

--- projects/epgu-constructor/src/app/component/custom-screen/component-list-resolver/component-list-resolver.component.ts
+++ projects/epgu-constructor/src/app/component/custom-screen/component-list-resolver/component-list-resolver.component.ts
@@ -57,7 +57,6 @@ export class ComponentListResolverComponent implements AfterViewInit, OnChanges 
   ngAfterViewInit(): void {
     this.screenService.display$
       .pipe(
-        filter(() => !this.screenService.isTheSameScreenWithErrors),
         tap(() => this.destroyComponent()),
         subscribeOn(asyncScheduler), // fix dirty checked errors
         takeUntil(this.ngUnsubscribe$),
@@ -71,7 +70,7 @@ export class ComponentListResolverComponent implements AfterViewInit, OnChanges 
   }
 
   private destroyComponent(): void {
-    if (this.componentRef && !this.screenService.isTheSameScreenWithErrors) {
+    if (this.componentRef) {
       this.componentRef.destroy();
       this.componentRef = null;
     }
