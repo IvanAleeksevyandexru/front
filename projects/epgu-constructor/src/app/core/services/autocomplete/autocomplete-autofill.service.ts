7f317e0beadb766edf23856ea88d8d5e2af03c5f
Daniil Vysotskiy <dvysotskiy@it-one.ru>
2021-05-12 15:11:41 +0300

    fix: мини-патч под автофил в EmployeeHistory [EPGUCORE-54034]

--- projects/epgu-constructor/src/app/core/services/autocomplete/autocomplete-autofill.service.ts
+++ projects/epgu-constructor/src/app/core/services/autocomplete/autocomplete-autofill.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ComponentDto } from 'epgu-constructor-types';
 import { UniqueScreenComponentTypes } from '../../../component/unique-screen/unique-screen-components.types';
 import { ScreenService } from '../../../screen/screen.service';
 import { autofillComponentsList } from './autocomplete.const';
+import { ISuggestionItem } from './autocomplete.inteface';
 
 @Injectable()
 export class AutocompleteAutofillService {
@@ -11,22 +11,22 @@ export class AutocompleteAutofillService {
   public autofillIfNeeded(component): void {
     if (autofillComponentsList.includes(component?.type as UniqueScreenComponentTypes)) {
       if (component.type === UniqueScreenComponentTypes.employeeHistory) {
-        this.screenService.component.value = this.prepareEmployeeHistoryComponentValue(component);
-        this.screenService.updateScreenStore(this.screenService);
+        const suggestions = this.screenService.suggestions[component.id];
+        if (suggestions) {
+          this.screenService.component.value = this.prepareEmployeeHistoryComponentValue(
+            suggestions,
+          );
+          this.screenService.updateScreenStore(this.screenService);
+        }
       }
     }
   }
 
-  private prepareEmployeeHistoryComponentValue(component: ComponentDto): string {
-    const suggestions = this.screenService.suggestions[component.id];
-    if (suggestions) {
-      return JSON.stringify(
-        suggestions.list.reduce((acc, value) => {
-          return [...acc, ...JSON.parse(value.originalItem)];
-        }, []),
-      );
-    }
-
-    return '[]';
+  private prepareEmployeeHistoryComponentValue(suggestions: ISuggestionItem): string {
+    return JSON.stringify(
+      suggestions.list.reduce((acc, value) => {
+        return [...acc, ...JSON.parse(value.originalItem)];
+      }, []),
+    );
   }
 }
