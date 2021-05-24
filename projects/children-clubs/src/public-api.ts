ce050b242f5340308fb8a90d823d7dd3fa40bce1
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2021-05-24 17:02:32 +0500

    feat: Скелетон для компонента children-clubs [EPGUCORE-57293]

---
+++ projects/children-clubs/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of children-clubs
+ */
+
+export * from './lib/children-clubs.service';
+export * from './lib/children-clubs.component';
+export * from './lib/children-clubs.module';
