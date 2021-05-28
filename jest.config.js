d672fb2fc89007e102f75e9578e2e55d461133fd
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-05-28 18:05:47 +0600

    test: подправил код кавередж [EPGUCORE-56732]

--- jest.config.js
+++ jest.config.js
@@ -21,14 +21,14 @@ module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   coverageReporters: ['text', 'cobertura', 'html'],
   collectCoverageFrom: [
-    'projects/epgu-constructor/src/app/**/*.service.ts',
-    'projects/epgu-constructor/src/app/**/*.component.ts',
-    'projects/epgu-constructor/src/app/**/*.directive.ts',
-    'projects/epgu-constructor/src/app/**/*.interceptor.ts',
-    'projects/epgu-constructor/src/app/**/*.pipe.ts',
-    'projects/epgu-constructor/src/app/**/*.strategy.ts',
-    'projects/epgu-constructor/src/app/**/*.validators.ts',
-    'projects/epgu-constructor/src/app/**/*.guard.ts'
+    'projects/**/*.service.ts',
+    'projects/**/*.component.ts',
+    'projects/**/*.directive.ts',
+    'projects/**/*.interceptor.ts',
+    'projects/**/*.pipe.ts',
+    'projects/**/*.strategy.ts',
+    'projects/**/*.validators.ts',
+    'projects/**/*.guard.ts'
   ],
   globals: {
     'ts-jest': {
