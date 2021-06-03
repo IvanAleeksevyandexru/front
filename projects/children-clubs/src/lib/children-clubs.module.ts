a476cce06c1fdd5bafa7af75fd35c491af09130b
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-06-03 13:58:10 +0300

    feat: Компонент поиска с фильтрами в SPA[EPGUCORE-56738]

--- projects/children-clubs/src/lib/children-clubs.module.ts
+++ projects/children-clubs/src/lib/children-clubs.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ChildrenClubsAppComponent } from './children-clubs-app.component';
 import { EpguCfUiKitModule } from '@epgu/epgu-constructor-ui-kit';
-
-
+import { ChildrenClubsFilterPanelModule } from './components/filter-panel/children-clubs-filter-panel.module';
 
 @NgModule({
   declarations: [ChildrenClubsAppComponent],
-  imports: [EpguCfUiKitModule],
+  imports: [EpguCfUiKitModule, ChildrenClubsFilterPanelModule],
   exports: [ChildrenClubsAppComponent]
 })
 export class ChildrenClubsModule { }
---
+++ projects/children-clubs/src/lib/components/filter-panel/children-clubs-filter-panel.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { distinctUntilChanged, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'children-clubs-filter-panel',
+  templateUrl: './children-clubs-filter-panel.component.html',
+  styleUrls: ['./children-clubs-filter-panel.component.scss', '../../../styles.scss'],
+})
+export class ChildrenClubsFilterPanelComponent implements OnInit {
+  @Input() filtersCount: number;
+  @Output() openFilters = new EventEmitter();
+  @Output() search = new EventEmitter<string>();
+  searchControl = new FormControl('');
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(
+        filter((value) => value.length > 3 || !value.length),
+        distinctUntilChanged(),
+      )
+      .subscribe((searchTxt) => this.search.next(searchTxt));
+  }
+}
