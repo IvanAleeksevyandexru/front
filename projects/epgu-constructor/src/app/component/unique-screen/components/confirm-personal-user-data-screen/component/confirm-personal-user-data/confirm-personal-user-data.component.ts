5c9c14fcde0c2eb31d20466c60d3891e08a7c3f7
Daria Perevezentseva <dperevezentseva@luxoft.com>
2021-06-09 11:33:45 +0300

    fix: Абстракция для ConfirmPersonalData[EPGUCORE-58827]

--- projects/epgu-constructor/src/app/component/unique-screen/components/confirm-personal-user-data-screen/component/confirm-personal-user-data/confirm-personal-user-data.component.ts
+++ projects/epgu-constructor/src/app/component/unique-screen/components/confirm-personal-user-data-screen/component/confirm-personal-user-data/confirm-personal-user-data.component.ts
@@ -1,15 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActionType } from '@epgu/epgu-constructor-types';
-import {
-  ConfirmUserData,
-  ConfirmUserDataErrorType,
-} from '../../confirm-personal-user-data-screen.types';
-import { ConfigService } from '../../../../../../core/services/config/config.service';
-import { ScreenService } from '../../../../../../screen/screen.service';
-import { CurrentAnswersService } from '../../../../../../screen/current-answers.service';
+import { ConfirmUserData } from '../../confirm-personal-user-data-screen.types';
 import { UnsubscribeService } from '../../../../../../core/services/unsubscribe/unsubscribe.service';
+import { AbstractConfirmPersonalUserDataDirective } from '../abstract-confirm-personal-user-data.directive';
 
 @Component({
   selector: 'epgu-constructor-confirm-personal-user-data',
@@ -18,31 +11,8 @@ import { UnsubscribeService } from '../../../../../../core/services/unsubscribe/
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [UnsubscribeService],
 })
-export class ConfirmPersonalUserDataComponent implements OnInit {
-  data$: Observable<ConfirmUserData> = this.screenService.component$ as Observable<ConfirmUserData>;
+export class ConfirmPersonalUserDataComponent extends AbstractConfirmPersonalUserDataDirective<
+  ConfirmUserData
+> {
   actionType = ActionType;
-
-  constructor(
-    public config: ConfigService,
-    public screenService: ScreenService,
-    public currentAnswersService: CurrentAnswersService,
-    private ngUnsubscribe$: UnsubscribeService,
-    private changeDetectionRef: ChangeDetectorRef,
-  ) {}
-
-  ngOnInit(): void {
-    this.data$.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((data) => {
-      this.updateValue(data.value);
-      this.changeDetectionRef.markForCheck();
-    });
-  }
-
-  private updateValue(value: string): void {
-    if (value) {
-      this.currentAnswersService.state = value;
-      const { errors = [] } = JSON.parse(value);
-      const hasErrors = errors.some((error) => error?.type === ConfirmUserDataErrorType.error);
-      this.currentAnswersService.isValid = !hasErrors;
-    }
-  }
 }
