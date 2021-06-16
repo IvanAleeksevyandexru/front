49c43e78df252c45d608ac39d968dc435042f127
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-06-16 21:04:16 +0600

    test: добавил app-routing.service.spec.ts [EPGUCORE-56747]

--- projects/epgu-constructor-ui-kit/src/lib/app/app-routing/app-routing.service.ts
+++ projects/epgu-constructor-ui-kit/src/lib/app/app-routing/app-routing.service.ts
@@ -1,19 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Type } from '@angular/core';
 import { AppRoutingComponentMap } from './app-routing';
 import { AppRouterState } from '@epgu/epgu-constructor-types';
 import { AppStateQuery } from '../app-state/app-state.query';
-import { distinctUntilKeyChanged, map } from 'rxjs/operators';
+import { distinctUntilKeyChanged, filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable()
 export class AppRoutingService {
 
-  public component$ = this.appStateQuery.state$.pipe(
-    distinctUntilKeyChanged('currentComponent' ),
-    map(({ currentComponent }) => {
-      return this.appRoutingComponentMap[currentComponent];
-    })
-  );
   private appRoutingComponentMap: AppRoutingComponentMap;
+  private state$ = this.appStateQuery.state$;
 
   constructor (
     private appStateQuery: AppStateQuery<unknown, AppRouterState>,
@@ -21,5 +17,15 @@ export class AppRoutingService {
 
   public initRouting(appRoutingComponentMap: AppRoutingComponentMap): void {
     this.appRoutingComponentMap = appRoutingComponentMap;
+  }
+
+  public get component$(): Observable<Type<unknown>> {
+    return this.state$.pipe(
+      distinctUntilKeyChanged('currentComponent' ),
+      filter((state) => !!state.currentComponent),
+      map(({ currentComponent }) => {
+        return this.appRoutingComponentMap[currentComponent];
+      })
+    );
   }
 }
