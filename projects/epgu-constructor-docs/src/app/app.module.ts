844c3edeee46af84e5653a63b41f698426146cce
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2021-06-17 15:59:30 +0300

    feat: добавлен скелет документации на примере одного epgu-компонента [EPGUCORE-58538]

--- projects/epgu-constructor-docs/src/app/app.module.ts
+++ projects/epgu-constructor-docs/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  TuiAddonDocModule,
+  TuiDocExampleModule,
+  TuiDocMainModule,
+  TuiDocPageModule,
+} from '@taiga-ui/addon-doc';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExampleEpguChipModule } from '../components/chip/chip.module';
+import { APP_PROVIDERS } from './app.providers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TuiDocMainModule,
+    TuiAddonDocModule,
+    TuiDocExampleModule,
+    TuiDocPageModule,
+    ExampleEpguChipModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: APP_PROVIDERS,
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
