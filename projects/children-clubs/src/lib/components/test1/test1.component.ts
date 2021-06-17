43a6cd7bd658bbc589751d1805caeaa1a22e92b5
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-06-17 15:56:12 +0600

    fix: рендер инициализации компанент под app-component-resolver.component [EPGUCORE-56747]

--- projects/children-clubs/src/lib/components/test1/test1.component.ts
+++ projects/children-clubs/src/lib/components/test1/test1.component.ts
@@ -1,14 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { AppNavigationService } from '@epgu/epgu-constructor-ui-kit';
 
 // TODO: remove it when moved to real components
 @Component({
   selector: 'children-clubs-test1',
   template:
-    '<h1>test1</h1><button (click)="prev()">Prev Out</button><button (click)="next()">Next component</button>',
+    '<h1>test1 {{ key1 }}</h1><button (click)="prev()">Prev Out</button><button (click)="next()">Next component</button>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Test1Component {
+export class Test1Component implements OnInit {
+  key1 = 'Some value 1';
   constructor(private appNavigationService: AppNavigationService) {}
 
   next(): void {
@@ -17,5 +18,9 @@ export class Test1Component {
 
   prev(): void {
     this.appNavigationService.prev();
+  }
+
+  ngOnInit(): void {
+    this.key1 = 'Some value 1 onInit';
   }
 }
--- projects/children-clubs/src/lib/components/test2/test2.component.ts
+++ projects/children-clubs/src/lib/components/test2/test2.component.ts
@@ -1,14 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { AppNavigationService } from '@epgu/epgu-constructor-ui-kit';
 
 // TODO: remove it when moved to real components
 @Component({
   selector: 'children-clubs-test2',
   template:
-    '<h1>test2</h1><button (click)="prev()">Prev component</button><button (click)="next()">Next out</button>',
+    '<h1>test2 {{key2}}</h1><button (click)="prev()">Prev component</button><button (click)="next()">Next out</button>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Test2Component {
+export class Test2Component implements OnInit {
+  key2 = 'Some value 2';
   constructor(private appNavigationService: AppNavigationService) {}
 
   prev(): void {
@@ -17,5 +18,9 @@ export class Test2Component {
 
   next(): void {
     this.appNavigationService.next();
+  }
+
+  ngOnInit(): void {
+    this.key2 = 'Some value 2 onInit';
   }
 }
