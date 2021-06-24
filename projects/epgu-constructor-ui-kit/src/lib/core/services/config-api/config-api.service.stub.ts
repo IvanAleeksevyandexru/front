5323db5e5879e2889b7677992a84af8b1cd6f124
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-06-24 19:58:52 +0600

    feat: фикс билда [EPGUCORE-60237]

--- projects/epgu-constructor-ui-kit/src/lib/core/services/config-api/config-api.service.stub.ts
+++ projects/epgu-constructor-ui-kit/src/lib/core/services/config-api/config-api.service.stub.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { ConfigApiService } from './config-api.service';
 
 @Injectable()
 export class ConfigApiServiceStub {
