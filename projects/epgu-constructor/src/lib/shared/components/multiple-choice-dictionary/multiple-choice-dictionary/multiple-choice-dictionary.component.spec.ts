8aa9d3666e4ee350b44f113a90622c2f10e1d71a
Serebrennikova <YSerebrennikova@it-one.ru>
2021-06-25 10:49:22 +0600

    test: дублирование заголовков в MultipleChoiceDictionary [EPGUCORE-59333]

--- projects/epgu-constructor/src/lib/shared/components/multiple-choice-dictionary/multiple-choice-dictionary/multiple-choice-dictionary.component.spec.ts
+++ projects/epgu-constructor/src/lib/shared/components/multiple-choice-dictionary/multiple-choice-dictionary/multiple-choice-dictionary.component.spec.ts
@@ -34,7 +34,6 @@ describe('MultipleChoiceDictionaryComponent', () => {
     component = fixture.componentInstance;
     component.label = 'Label';
     component.subLabel = 'SubLabel';
-    component.modalHeader = 'modalHeader';
     component.dictionaryType = 'STRANI_IST';
     fixture.detectChanges();
   });
@@ -66,11 +65,22 @@ describe('MultipleChoiceDictionaryComponent', () => {
   });
 
   describe('onClick', () => {
-    it('should be open modal', () => {
+    it('should be open modal with modalHeader', () => {
       jest.spyOn(modalService, 'openModal');
+      component.modalHeader = 'modalHeader';
       component.onClick();
       expect(modalService.openModal).toHaveBeenCalledWith(MultiChoiceDictionaryModalComponent, {
         title: component.modalHeader,
+        dictionaryList: undefined,
+        dictionaryType: component.dictionaryType,
+        selectedItems: component.selectedItems,
+      });
+    });
+    it('should be open modal without modalHeader', () => {
+      jest.spyOn(modalService, 'openModal');
+      component.onClick();
+      expect(modalService.openModal).toHaveBeenCalledWith(MultiChoiceDictionaryModalComponent, {
+        title: component.subLabel,
         dictionaryList: undefined,
         dictionaryType: component.dictionaryType,
         selectedItems: component.selectedItems,
