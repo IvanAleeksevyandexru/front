781e272a478135c2388a838f7bdd4959c4a614ac
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2021-07-16 12:07:56 +0300

    fix: правка в бизнес-логике информера + тесты [EPGUCORE-62641]

--- projects/epgu-constructor/src/lib/shared/directives/action/action.service.stub.ts
+++ projects/epgu-constructor/src/lib/shared/directives/action/action.service.stub.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ComponentActionDto } from '@epgu/epgu-constructor-types';
+import {
+  ActionApiResponse,
+  ActionType,
+  ComponentActionDto,
+  EaisdoResponse,
+} from '@epgu/epgu-constructor-types';
+import { EMPTY, Observable, of } from 'rxjs';
 
 @Injectable()
 export class ActionServiceStub {
   action: ComponentActionDto;
   componentId: string;
+  public get actionType$(): Observable<ActionType | null> {
+    return of(null);
+  }
 
   navigate(stepType: string): void {}
 
   navigateModal(stepType: string): void {}
 
   switchAction(): void {}
+
+  handleExternalIntegrationAction(
+    action: ComponentActionDto,
+    componentId: string,
+  ): Observable<ActionApiResponse<EaisdoResponse>> {
+    return of({
+      errorList: [],
+      responseData: {
+        value: { error: null, errorType: null, responseType: null, responseData: null },
+        type: null,
+      },
+    });
+  }
 }
