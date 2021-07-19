864b36c345d8edb1f65afced7111f4e5e2c0526f
Alexey Martyanov <AMartyanov@it-one.ru>
2021-07-19 16:10:08 +0300

    fix: добавлены подсказки валидации полей passport.component [EPGUCORE-61093]

--- projects/epgu-constructor/src/lib/shared/components/add-passport/passport.interface.ts
+++ projects/epgu-constructor/src/lib/shared/components/add-passport/passport.interface.ts
@@ -2,13 +2,13 @@ import { TextTransform } from '@epgu/epgu-constructor-types';
 
 export interface PassportAttr {
   participant: { role: string; mode: string };
-  fields: PassportFields[];
+  fields: PassportField[];
   fstuc?: TextTransform; // TODO проверить на наличие
 }
 
 export type PassportFieldName = 'rfPasportSeries' | 'rfPasportNumber';
 
-export interface PassportFields {
+export interface PassportField {
   mask: Array<string>;
   fieldName: PassportFieldName;
   label: string;
