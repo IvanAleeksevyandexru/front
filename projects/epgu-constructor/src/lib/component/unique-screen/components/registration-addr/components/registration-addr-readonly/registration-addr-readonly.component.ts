013cf95240d8e99ec89ddf227570c92818c167ad
kirillchelikov <kirill.chelikov@yandex.ru>
2021-08-27 16:13:46 +0500

    fix: добавил возможность валидировать компонент и отображать ошибки [EPGUCORE-65138]

--- projects/epgu-constructor/src/lib/component/unique-screen/components/registration-addr/components/registration-addr-readonly/registration-addr-readonly.component.ts
+++ projects/epgu-constructor/src/lib/component/unique-screen/components/registration-addr/components/registration-addr-readonly/registration-addr-readonly.component.ts
@@ -2,12 +2,15 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
 import { takeUntil } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { UnsubscribeService } from '@epgu/epgu-constructor-ui-kit';
+import { FormControl } from '@angular/forms';
 import { CurrentAnswersService } from '../../../../../../screen/current-answers.service';
 import { ScreenService } from '../../../../../../screen/screen.service';
 import {
   IRegistrationAddrReadonlyComponent,
   RegistrationAddrFormValue,
 } from '../../registration-addr-screen.types';
+import { ValidationService } from '../../../../../../shared/services/validation/validation.service';
+import { CustomComponent } from '../../../../../custom-screen/components-list.types';
 
 @Component({
   selector: 'epgu-constructor-registration-addr-readonly',
@@ -20,20 +23,34 @@ export class RegistrationAddrReadonlyComponent implements OnInit {
   data$: Observable<IRegistrationAddrReadonlyComponent> = this.screenService
     .component$ as Observable<IRegistrationAddrReadonlyComponent>;
 
-  registrationAddress: RegistrationAddrFormValue;
+  fullAddress = new FormControl('');
+  parsedValue: RegistrationAddrFormValue;
+  error: string;
 
   constructor(
     public screenService: ScreenService,
     public currentAnswersService: CurrentAnswersService,
     private ngUnsubscribe$: UnsubscribeService,
     private changeDetectionRef: ChangeDetectorRef,
+    private validationService: ValidationService,
   ) {}
 
   ngOnInit(): void {
     this.data$.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((data) => {
-      this.registrationAddress = JSON.parse(data?.value || '{}');
-      this.currentAnswersService.isValid = !!this.registrationAddress?.regAddr?.fullAddress;
+      this.parsedValue = JSON.parse(data?.value || '{}');
+      this.fullAddress.setValidators([
+        this.validationService.customValidator((data as unknown) as CustomComponent),
+      ]);
+      this.fullAddress.setValue(this.parsedValue?.regAddr?.fullAddress);
+      this.currentAnswersService.state = data.value;
+      this.error = this.getError(data);
+      this.currentAnswersService.isValid = !!this.fullAddress.value;
       this.changeDetectionRef.markForCheck();
     });
+  }
+
+  private getError(data: IRegistrationAddrReadonlyComponent): string {
+    const errors = this.screenService?.getStore()?.errors;
+    return errors?.[data.id] || this.fullAddress.getError('msg');
   }
 }
