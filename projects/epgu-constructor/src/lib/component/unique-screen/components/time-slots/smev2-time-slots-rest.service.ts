17b31d5a16974e23370de93ebf09a305aeff44dd
kushnerevichaa@elocont.ru <eUid3u663b>
2021-09-28 15:02:04 +0300

    fix: Добавлен параметр dictionaryType для smev2 [EPGUCORE-71121]

--- projects/epgu-constructor/src/lib/component/unique-screen/components/time-slots/smev2-time-slots-rest.service.ts
+++ projects/epgu-constructor/src/lib/component/unique-screen/components/time-slots/smev2-time-slots-rest.service.ts
@@ -6,15 +6,22 @@ import { Observable } from 'rxjs';
 
 import { DictionaryResponse } from '../../../../shared/services/dictionary/dictionary-api.types';
 import { DictionaryOptions } from '@epgu/epgu-constructor-types';
+import { ScreenService } from '../../../../screen/screen.service';
 
 @Injectable()
 export class Smev2TimeSlotsRestService {
   private urlPrefix = this.config.dictionaryUrl;
+  private additionalPath =
+    this.screenSerivce.component?.attrs?.dictionaryType || 'getAppointment2_mvdr01';
 
-  constructor(private http: HttpClient, private config: ConfigService) {}
+  constructor(
+    private screenSerivce: ScreenService,
+    private http: HttpClient,
+    private config: ConfigService,
+  ) {}
 
   public getTimeSlots(requestBody: DictionaryOptions): Observable<DictionaryResponse> {
-    const path = `${this.urlPrefix}/getAppointment2_mvdr01`;
+    const path = `${this.urlPrefix}/${this.additionalPath}`;
     return this.http.post<DictionaryResponse>(path, requestBody, { withCredentials: true });
   }
 }
