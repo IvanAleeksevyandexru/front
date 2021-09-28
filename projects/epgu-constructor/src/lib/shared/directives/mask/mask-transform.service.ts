1bcdd27ed3f4781827c3a90ff7a5a9ee0ad9ff6c
eshakhova@it-one.ru <eshakhova@it-one.ru>
2021-09-28 05:52:22 +0000

    feat: добавлена замена точки на запятую в маске [EPGUCORE-70902]

--- projects/epgu-constructor/src/lib/shared/directives/mask/mask-transform.service.ts
+++ projects/epgu-constructor/src/lib/shared/directives/mask/mask-transform.service.ts
@@ -4,36 +4,24 @@ import { numberMaskDefaultOptions, NumberMaskOptions } from '@epgu/epgu-construc
 
 @Injectable()
 export class MaskTransformService {
-
   private localeThousandSeparator = this.decimalPipe.transform(1234, '0.0-0').replace(/\d/g, '');
   private localeDecimalSeparator = this.decimalPipe.transform(1.23, '0.2-2').replace(/\d/g, '');
-  constructor (
-    private decimalPipe: DecimalPipe,
-  ) {
+  constructor(private decimalPipe: DecimalPipe) {}
 
-  }
+  public transformNumberMaskInput(value: string, maskOptions: Partial<NumberMaskOptions>): string {
+    const options = { ...numberMaskDefaultOptions, ...maskOptions };
 
-  public transformNumberMaskInput(
-    value: string,
-    maskOptions: Partial<NumberMaskOptions>,
-    ): string {
-      const options = { ...numberMaskDefaultOptions, ...maskOptions };
+    let result = value.replace(options.decimalSymbol, '.').replace(/[^\d.]/g, '') || '0';
 
-      let result =
-      value.replace(options.decimalSymbol, '.').replace(/[^\d.]/g, '') || '0';
-
-      if (!Number.isNaN(+result)) {
-        result = this.decimalPipe
-          .transform(
-            result.substring(0, 10),
-            `0.${options.decimalLimit}-${options.decimalLimit}`,
-          )
-          .replace(
-            new RegExp(this.localeThousandSeparator, 'g'),
-            options.includeThousandsSeparator ? options.thousandsSeparatorSymbol : '',
-          )
-          .replace(this.localeDecimalSeparator, options.decimalSymbol);
-      }
-      return result;
+    if (!Number.isNaN(+result)) {
+      result = this.decimalPipe
+        .transform(result.substring(0, 10), `0.${options.decimalLimit}-${options.decimalLimit}`)
+        .replace(
+          new RegExp(this.localeThousandSeparator, 'g'),
+          options.includeThousandsSeparator ? options.thousandsSeparatorSymbol : '',
+        )
+        .replace(this.localeDecimalSeparator, options.decimalSymbol);
+    }
+    return result;
   }
 }
