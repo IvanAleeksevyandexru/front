517681ff8af607f63c964d47f5a6ee89cf9cb31e
Daniil Vysotskiy <dvysotskiy@luxsoft.com>
2021-10-01 12:41:16 +0300

    fix: поправлена сборка docs [EPGUCORE-73172]

--- projects/epgu-constructor-docs/src/app/logo/logo.component.ts
+++ projects/epgu-constructor-docs/src/app/logo/logo.component.ts
@@ -1,12 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
+import { SafePipe } from '@epgu/epgu-constructor-ui-kit';
+import logoSvg from '!!raw-loader!../../assets/svg/gos_logo_mobile.svg';
 
 @Component({
   selector: 'example-logo',
   templateUrl: './logo.html',
   styleUrls: ['./logo.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SafePipe],
 })
-export class LogoComponent {}
+export class LogoComponent {
+  public logo = this.safePipe.transform(logoSvg, 'resourceUrl');
+
+  constructor(private safePipe: SafePipe) {}
+}
 
 export const LOGO_CONTENT = new PolymorpheusComponent(LogoComponent);
