dc86073f0607b2a0bfade9385fc5e84104df8252
oanisimov@it-one.ru <oanisimov@it-one.ru>
2021-11-01 13:38:34 +0000

    feat: Информер загрузки данных из ВС [EPGUCORE-62176]

---
+++ projects/epgu-constructor/src/lib/component/custom-screen/components/kinder-garten-draft-handler/KinderGartenDraftHandlerModelAttrs.ts
@@ -0,0 +1,8 @@
+import { CustomComponentAttr } from '../../components-list.types';
+import GenericAttrs from '../../component-list-resolver/GenericAttrs';
+
+export default class KinderGartenDraftHandlerModelAttrs extends GenericAttrs {
+  constructor(attrs: CustomComponentAttr) {
+    super(attrs);
+  }
+}
