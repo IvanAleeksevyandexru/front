9bcda9712b9c38d197ce2d14f2522250172f498f
vroik@it-one.ru <vroik@it-one.ru>
2021-11-03 12:51:35 +0000

    feat: Добавляет возможность объединения ошибок и удаления повторяющихся [EPGUCORE-77184]

--- projects/epgu-constructor/src/lib/shared/components/field-list/field-list.component.spec.ts
+++ projects/epgu-constructor/src/lib/shared/components/field-list/field-list.component.spec.ts
@@ -1,14 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SimpleChange } from '@angular/core';
 import { MockModule, MockProvider } from 'ng-mocks';
 import { FieldListComponent } from './field-list.component';
-import {
-  ImgPrefixerPipe,
-  ObjectHelperService,
-  UnsubscribeService,
-  UnsubscribeServiceStub,
-} from '@epgu/epgu-constructor-ui-kit';
-import { ConfigService } from '@epgu/epgu-constructor-ui-kit';
-import { ConfigServiceStub } from '@epgu/epgu-constructor-ui-kit';
+import { UnsubscribeService, UnsubscribeServiceStub } from '@epgu/epgu-constructor-ui-kit';
 import { OutputHtmlModule } from '../output-html/output-html.module';
 import { RankPipe, SafePipe } from '@epgu/epgu-constructor-ui-kit';
 import { configureTestSuite } from 'ng-bullet';
@@ -16,12 +10,10 @@ import { EaisdoGroupCostService } from '../../services/eaisdo-group-cost/eaisdo-
 import { CurrentAnswersService } from '../../../screen/current-answers.service';
 import { EaisdoStateTypes } from '../../../component/custom-screen/components/eaisdo-group-cost/eaisdo.interface';
 import { JsonHelperService } from '../../../core/services/json-helper/json-helper.service';
-import { CertificateEaisdoService } from '../../services/certificate-eaisdo/certificate-eaisdo.service';
-import { ScreenService } from '../../../screen/screen.service';
-import { ScreenServiceStub } from '../../../screen/screen.service.stub';
 import { By } from '@angular/platform-browser';
 import { EaisdoGroupCostServiceStub } from '../../services/eaisdo-group-cost/eaisdo-group-cost.service.stub';
 import { JsonHelperServiceStub } from '../../../core/services/json-helper/json-helper.service.stub';
+import { ConfirmUserDataErrorType } from '../../../component/unique-screen/components/confirm-personal-user-data-screen/confirm-personal-user-data-screen.types';
 
 describe('FieldListComponent', () => {
   let component: FieldListComponent;
@@ -72,19 +64,64 @@ describe('FieldListComponent', () => {
     },
   ];
 
+  const mockError = {
+    desc: 'Поле может содержать не более 10 символов',
+    icon: 'red-line',
+    title: 'Сократите количество символов',
+    type: ConfirmUserDataErrorType.error,
+  };
+
+  const mockErrors = [
+    {
+      desc: 'Поле может содержать не более 10 символов',
+      icon: 'red-line',
+      title: 'Сократите количество символов',
+      type: ConfirmUserDataErrorType.error,
+    },
+    {
+      desc: 'Поле может содержать не более 10 символов',
+      icon: 'red-line',
+      title: 'Сократите количество символов',
+      type: ConfirmUserDataErrorType.error,
+    },
+    {
+      desc: 'Поле не может содержать буквы',
+      icon: 'red-line',
+      title: 'Удалите лишние символы',
+      type: ConfirmUserDataErrorType.error,
+    },
+    {
+      desc: 'Поле не может содержать цифры',
+      icon: 'red-line',
+      title: 'Удалите лишние символы',
+      type: ConfirmUserDataErrorType.error,
+    }
+  ];
+
+  const mockGroupedErrors = [
+    {
+      desc: 'Поле может содержать не более 10 символов',
+      icon: 'red-line',
+      title: 'Сократите количество символов',
+      type: ConfirmUserDataErrorType.error,
+    },
+    {
+      desc: 'Поле не может содержать буквы <br> Поле не может содержать цифры',
+      icon: 'red-line',
+      title: 'Удалите лишние символы',
+      type: ConfirmUserDataErrorType.error,
+    }
+  ];
+
   configureTestSuite(() => {
     TestBed.configureTestingModule({
-      declarations: [FieldListComponent, SafePipe, ImgPrefixerPipe, RankPipe],
+      declarations: [FieldListComponent, RankPipe],
       imports: [MockModule(OutputHtmlModule)],
       providers: [
-        { provide: ConfigService, useClass: ConfigServiceStub },
-        { provide: ScreenService, useClass: ScreenServiceStub },
         { provide: EaisdoGroupCostService, useClass: EaisdoGroupCostServiceStub },
         { provide: UnsubscribeService, useClass: UnsubscribeServiceStub },
         { provide: JsonHelperService, useClass: JsonHelperServiceStub },
-        MockProvider(CertificateEaisdoService),
         MockProvider(CurrentAnswersService),
-        MockProvider(ObjectHelperService),
       ],
     }).compileComponents();
   });
@@ -100,6 +137,50 @@ describe('FieldListComponent', () => {
   it('should create', () => {
     component.ngOnChanges({});
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnChanges()', () => {
+    it('should call "groupErrors()" if "isNeedToGroupErrors" is true and errors amount more than 1', () => {
+      jest.spyOn<FieldListComponent, any>(component, 'groupErrors');
+      component.data.attrs.isNeedToGroupErrors = true;
+      component.data.value = JSON.stringify({
+        errors: mockErrors,
+      });
+      fixture.detectChanges();
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, 1, true)
+      });
+
+      expect(component['groupErrors']).toBeCalled();
+    });
+
+    it('should not call "groupErrors()" if "isNeedToGroupErrors" is false or errors amount less than 2', () => {
+      jest.spyOn<FieldListComponent, any>(component, 'groupErrors');
+      component.data.attrs.isNeedToGroupErrors = false;
+      component.data.value = JSON.stringify({
+        errors: mockErrors,
+      });
+      fixture.detectChanges();
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, 1, true)
+      });
+
+      expect(component['groupErrors']).not.toBeCalled();
+
+      component.data.attrs.isNeedToGroupErrors = true;
+      component.data.value = JSON.stringify({
+        errors: mockError,
+      });
+      fixture.detectChanges();
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, 1, true)
+      });
+
+      expect(component['groupErrors']).not.toBeCalled();
+    });
   });
 
   describe('calculateVisibility()', () => {
@@ -129,6 +210,18 @@ describe('FieldListComponent', () => {
       currentAnswersService.state = { id: { value: { value: { placeholder: 'someValue' }}}};
       const result = component['transformString'](str);
       expect(result).toBe('someValue');
+    });
+  });
+
+  describe('groupErrors()', () => {
+    it('should group errors with only similar title and leave 1 similar error by title and description', () => {
+      component.data.attrs.isNeedToGroupErrors = true;
+      component.errors = mockErrors;
+      fixture.detectChanges();
+
+      component['groupErrors']();
+
+      expect(component.errors).toEqual(mockGroupedErrors);
     });
   });
 
