2c11e542dee36fb01612da7eb298039c796c94bd
Vladislav Roik <vroik@it-one.ru>
2021-11-30 16:24:42 +0300

    feat: Добавляет возможность отображать screenButtons для указанных OS [EPGUCORE-78236]

--- projects/epgu-constructor/src/lib/shared/components/screen-buttons/screen-buttons.component.ts
+++ projects/epgu-constructor/src/lib/shared/components/screen-buttons/screen-buttons.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { ScreenButton } from '@epgu/epgu-constructor-types';
-import { BusEventType, EventBusService } from '@epgu/epgu-constructor-ui-kit';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ScreenButton, System } from '@epgu/epgu-constructor-types';
+import {
+  BusEventType,
+  DeviceDetectorService,
+  EventBusService,
+} from '@epgu/epgu-constructor-ui-kit';
 
 @Component({
   selector: 'epgu-constructor-screen-buttons',
@@ -8,17 +12,32 @@ import { BusEventType, EventBusService } from '@epgu/epgu-constructor-ui-kit';
   styleUrls: ['./screen-buttons.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScreenButtonsComponent {
+export class ScreenButtonsComponent implements OnInit {
   @Input() screenButtons: ScreenButton[];
   @Input() isLoading = false;
   @Input() disabled = false;
   @Input() disabledForAll = false;
 
   public clickedButton: ScreenButton;
+  public shownButtons: ScreenButton[];
+  private clientSystem: System;
 
-  constructor(private eventBusService: EventBusService) {}
+  constructor(
+    private eventBusService: EventBusService,
+    private deviceDetectorService: DeviceDetectorService,
+  ) {}
 
-  setClickedButton(button: ScreenButton): void {
+  public ngOnInit(): void {
+    this.clientSystem = this.deviceDetectorService.system;
+
+    this.shownButtons = this.screenButtons.filter((screenButton) => {
+      const showOnOS = screenButton.attrs?.showOnOS;
+
+      return !showOnOS || showOnOS.includes(this.clientSystem);
+    });
+  }
+
+  public setClickedButton(button: ScreenButton): void {
     this.clickedButton = button;
     this.eventBusService.emit(BusEventType.ScreenButtonClicked, button);
   }
