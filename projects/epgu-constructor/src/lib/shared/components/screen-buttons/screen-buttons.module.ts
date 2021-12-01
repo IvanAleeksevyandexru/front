64f7e06e5dd3c8a71b70c7626926a22ebf565dbd
Vladislav Roik <vroik@it-one.ru>
2021-12-01 18:08:33 +0300

    test: Правит тесты [EPGUCORE-78236]

--- projects/epgu-constructor/src/lib/shared/components/screen-buttons/screen-buttons.module.ts
+++ projects/epgu-constructor/src/lib/shared/components/screen-buttons/screen-buttons.module.ts
@@ -3,10 +3,12 @@ import { ScreenButtonsComponent } from './screen-buttons.component';
 import { BaseModule } from '../../base.module';
 import { DisabledButtonPipe } from './pipes/disabled-button.pipe';
 import { ShowLoaderButtonPipe } from './pipes/show-loader-button.pipe';
+import { DeviceDetectorService, WINDOW_PROVIDERS } from '@epgu/epgu-constructor-ui-kit';
 
 @NgModule({
   declarations: [ScreenButtonsComponent, DisabledButtonPipe, ShowLoaderButtonPipe],
   imports: [BaseModule],
   exports: [ScreenButtonsComponent],
+  providers: [DeviceDetectorService, WINDOW_PROVIDERS],
 })
 export class ScreenButtonsModule {}
