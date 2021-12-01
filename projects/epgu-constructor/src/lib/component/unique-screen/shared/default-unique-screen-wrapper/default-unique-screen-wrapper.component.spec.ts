5ea1f99ce713e09e4caa23b8b9706b67cec0400d
Vladislav Roik <vroik@it-one.ru>
2021-12-01 18:08:33 +0300

    test: Правит тесты [EPGUCORE-78236]

--- projects/epgu-constructor/src/lib/component/unique-screen/shared/default-unique-screen-wrapper/default-unique-screen-wrapper.component.spec.ts
+++ projects/epgu-constructor/src/lib/component/unique-screen/shared/default-unique-screen-wrapper/default-unique-screen-wrapper.component.spec.ts
@@ -13,30 +13,34 @@ import {
   ModalServiceStub,
   EventBusService,
 } from '@epgu/epgu-constructor-ui-kit';
-import { ComponentActionDto, DTOActionAction } from '@epgu/epgu-constructor-types';
 import { DefaultUniqueScreenWrapperComponent } from './default-unique-screen-wrapper.component';
 import { ScreenService } from '../../../../screen/screen.service';
 import { ScreenServiceStub } from '../../../../screen/screen.service.stub';
 import { ActionDirective } from '../../../../shared/directives/action/action.directive';
 import { PageNameComponent } from '../../../../shared/components/base-components/page-name/page-name.component';
-
 import { UserInfoLoaderModule } from '../../../../shared/components/user-info-loader/user-info-loader.module';
 import { AnswerButtonComponent } from '../../../../shared/components/answer-button/answer-button.component';
-
 import { ScreenButtonsModule } from '../../../../shared/components/screen-buttons/screen-buttons.module';
 import { ActionService } from '../../../../shared/directives/action/action.service';
 import { ActionServiceStub } from '../../../../shared/directives/action/action.service.stub';
 import { CurrentAnswersService } from '../../../../screen/current-answers.service';
 import { BaseModule } from '../../../../shared/base.module';
 
+import {
+  ActionType,
+  ButtonColor,
+  ComponentActionDto,
+  DTOActionAction,
+} from '@epgu/epgu-constructor-types';
 import { EaisdoGroupCostService } from '../../../../shared/services/eaisdo-group-cost/eaisdo-group-cost.service';
 import { CertificateEaisdoService } from '../../../../shared/services/certificate-eaisdo/certificate-eaisdo.service';
 
 const componentActionDtoSample1: ComponentActionDto = {
   label: 'label1',
   value: 'value1',
-  color: 'white',
+  color: ButtonColor.WHITE,
   action: DTOActionAction.editEmail,
+  type: ActionType.nextStep,
 };
 
 describe('DefaultUniqueScreenWrapperComponent', () => {
@@ -78,7 +82,7 @@ describe('DefaultUniqueScreenWrapperComponent', () => {
     fixture = TestBed.createComponent(DefaultUniqueScreenWrapperComponent);
     component = fixture.componentInstance;
     component.header = 'Header';
-    component.screenButtons = [];
+    component.screenButtons = null;
     component.isLoading = false;
     component.isValid = true;
     component.showNav = true;
