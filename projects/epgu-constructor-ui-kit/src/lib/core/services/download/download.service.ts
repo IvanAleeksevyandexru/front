aa73f9ec701188bb65a6e5f3c27b36e0f9735ea5
udinkovee@elocont.ru <udinkovee@elocont.ru>
2021-12-03 09:15:35 +0300

    fix: Изменил download.service [EPGUCORE-76673]

--- projects/epgu-constructor-ui-kit/src/lib/core/services/download/download.service.ts
+++ projects/epgu-constructor-ui-kit/src/lib/core/services/download/download.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { DeviceDetectorService } from '../device-detector/device-detector.service';
+import { saveAs } from 'file-saver';
 
 @Injectable()
-export class DownloadService {
-  constructor(private deviceDetector: DeviceDetectorService) {}
-  /**
+export class DownloadService {  /**
    * Скачивание файла
    */
   public downloadFile(value: string, type: string, fileName?: string): void {
@@ -12,24 +10,9 @@ export class DownloadService {
     const data = isBase64 ? this.convertBase64(value) : value;
     // на мобилке не работает, если не обрезать тип
     const fileType = isBase64 ? type.replace(';base64', '') : type;
-    const blob = new File([data], fileName || 'file', { type: fileType });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-
-    link.style.display = 'none';
-    link.href = url;
-
-    if (!this.deviceDetector.isWebView) {
-      link.setAttribute('download', fileName || 'file');
-    }
-
-    document.body.appendChild(link);
-    link.click();
-
-    setTimeout(() => {
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(url);
-    }, 200);
+    const blob = new Blob([data], { type: fileType });
+    
+    saveAs(blob, fileName);
   }
 
   private convertBase64(value: string): Uint8Array {
