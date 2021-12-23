8a4235b80b0ca6762d36ca51b75d31d51ab4541f
Alexey Martyanov <AMartyanov@it-one.ru>
2021-12-23 19:10:38 +0300

    feat: кастом текст в модальном окне просмотра картинок [EPGUCORE-82628]

--- projects/epgu-constructor/src/lib/component/unique-screen/components/file-upload-screen/file-upload-screen.component.ts
+++ projects/epgu-constructor/src/lib/component/unique-screen/components/file-upload-screen/file-upload-screen.component.ts
@@ -113,7 +113,11 @@ export class FileUploadScreenComponent implements OnInit {
         this.handleNewValueSet(payload);
         this.currentAnswersService.state = this.value;
         this.uploaderScreenService.updateLimits();
-        if (this.uploaderScreenService.showLimitsInfo()) this.cdr.markForCheck();
+        if (this.uploaderScreenService.showLimitsInfo()) {
+          window.requestAnimationFrame(() => {
+            this.cdr.markForCheck();
+          });
+        }
       });
 
     this.uploaderProcessing$.pipe(takeUntil(this.ngUnsubscribe$)).subscribe();
