87e638b154fc3dea743efc841fdd6097df9ee1d7
kushnerevichaa@elocont.ru <eUid3u663b>
2022-01-10 21:00:32 +0300

    feat: Добавлены тесты для UploaderProcessService [EPGUCORE-76620]

--- projects/epgu-constructor/src/lib/shared/components/file-upload/services/manager/uploader-manager.service.stub.ts
+++ projects/epgu-constructor/src/lib/shared/components/file-upload/services/manager/uploader-manager.service.stub.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class UploaderManagerServiceStub {}
+export class UploaderManagerServiceStub {
+  getMnemonic() {}
+
+  getError() {}
+}
