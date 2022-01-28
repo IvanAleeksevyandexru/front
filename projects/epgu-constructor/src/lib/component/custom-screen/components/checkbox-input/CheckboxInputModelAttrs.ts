08c923ac8fb77f57e32c10780e367da45b6a4363
OAnisimov <OAnisimov@it-one.ru>
2022-01-28 15:22:21 +0300

    fix: Изменил стили и набор атрибутов компонента чекбокс [EPGUCORE-75228]

--- projects/epgu-constructor/src/lib/component/custom-screen/components/checkbox-input/CheckboxInputModelAttrs.ts
+++ projects/epgu-constructor/src/lib/component/custom-screen/components/checkbox-input/CheckboxInputModelAttrs.ts
@@ -3,15 +3,12 @@ import GenericAttrs from '../../component-list-resolver/GenericAttrs';
 import { Clarifications } from '@epgu/epgu-constructor-types';
 
 export default class CheckboxInputModelAttrs extends GenericAttrs {
-  readonly isHorizontal: boolean;
-
   readonly hint: string;
 
   readonly clarifications: Clarifications;
 
   constructor(attrs: CustomComponentAttr) {
     super(attrs);
-    this.isHorizontal = attrs.isHorizontal;
     this.clarifications = attrs.clarifications;
     this.hint = attrs.hint;
   }
