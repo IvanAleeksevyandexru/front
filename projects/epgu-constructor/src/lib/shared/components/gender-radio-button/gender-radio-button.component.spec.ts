c9d46763a8e38842250e6f94a1165d12e49dc359
kushnerevichaa@elocont.ru <eUid3u663b>
2022-02-16 15:02:10 +0300

    feat: добавлены тесты для GenderRadioButtonComponent [EPGUCORE-88563]

--- projects/epgu-constructor/src/lib/shared/components/gender-radio-button/gender-radio-button.component.spec.ts
+++ projects/epgu-constructor/src/lib/shared/components/gender-radio-button/gender-radio-button.component.spec.ts
@@ -3,12 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { LoggerService, LoggerServiceStub } from '@epgu/epgu-constructor-ui-kit';
 
-import { GenderRadioButtonComponent } from './gender-radio-button.component';
+import { GenderOrder, GenderRadioButtonComponent } from './gender-radio-button.component';
 
 describe('GenderRadioButtonComponent', () => {
   let component: GenderRadioButtonComponent;
   let fixture: ComponentFixture<GenderRadioButtonComponent>;
-
+  let loggerService: LoggerService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: remove this line when resolve issue with @ifc/plugin and @ifc/common dependencies
@@ -18,12 +18,44 @@ describe('GenderRadioButtonComponent', () => {
   }));
 
   beforeEach(() => {
+    loggerService = TestBed.inject(LoggerService);
     fixture = TestBed.createComponent(GenderRadioButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be writeValue', () => {
+    component.writeValue('a');
+    expect(component.innerValue).toBe('a');
+    component.writeValue(false);
+    expect(component.innerValue).toBe(GenderOrder.male);
+    component.writeValue(2);
+    expect(component.innerValue).toBe(2);
+  });
+
+  it('should be registerOnChange', () => {
+    const fn = jest.fn();
+    component.registerOnChange(fn);
+    expect(component.onChange).toBe(fn);
+  });
+
+  it('should be registerOnTouched', () => {
+    const fn = jest.fn();
+    component.registerOnTouched(fn);
+    expect(component.onTouched).toBe(fn);
+  });
+
+  it('should be change', () => {
+    jest.spyOn(loggerService, 'error');
+    component.change({ value: 'a' });
+    expect(loggerService.error).toHaveBeenCalled();
+    const fnChange = jest.fn();
+    const fnTouched = jest.fn();
+    const test = 'a';
+    component.registerOnChange(fnChange);
+    component.registerOnTouched(fnTouched);
+    component.change({ value: test });
+    expect(fnChange).toHaveBeenCalledWith(test);
+    expect(fnTouched).toHaveBeenCalledWith(test);
   });
 });
