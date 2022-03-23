9473c8bcb6698cdc8ae74e39abb09e56abe2b486
Elizabeth Shakhova <eshakhova@it-one.ru>
2022-03-23 13:07:53 +0300

    feat: резолв конфликтов [EPGUCORE-89367]

--- projects/epgu-constructor/src/lib/component/custom-screen/components/form-multiple-choice-dictionary/MultipleChoiceDictionaryModel.ts
+++ projects/epgu-constructor/src/lib/component/custom-screen/components/form-multiple-choice-dictionary/MultipleChoiceDictionaryModel.ts
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import BaseModel from '../../component-list-resolver/BaseModel';
 import {
   CustomComponent,
   CustomComponentAttr,
   CustomComponentValidationConditions,
   CustomScreenComponentTypes,
 } from '../../components-list.types';
-=======
-import { CustomComponentAttr, CustomScreenComponentTypes } from '../../components-list.types';
->>>>>>> 5af8083da9490a48aeda311c21fd1c560f926aec
 import MultipleChoiceDictionaryModelAttrs from './MultipleChoiceDictionaryModelAttrs';
 import { isMultipleSelectedItemsValid } from './MultipleChoiceDictionaryValidations';
 import DictionaryLikeModel from '../../component-list-resolver/DictionaryLikeModel';
