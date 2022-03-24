0491098cc86d084898958a7ec122d55254b1fede
yserebrennikova@it-one.ru <yserebrennikova@it-one.ru>
2022-03-24 15:05:52 +0300

    feat: реализовать дисклеймер при отсутствии индекса в RegistrationLegalAddr [EPGUCORE-89049]

--- projects/epgu-constructor/src/lib/component/unique-screen/components/registration-addr/registration-addr-screen.types.ts
+++ projects/epgu-constructor/src/lib/component/unique-screen/components/registration-addr/registration-addr-screen.types.ts
@@ -1,5 +1,11 @@
 import { DurationTimeTypes } from '@epgu/epgu-constructor-ui-kit';
-import { Clarifications, ComponentActionDto, TextTransform } from '@epgu/epgu-constructor-types';
+import {
+  Clarifications,
+  ComponentActionDto,
+  DisclaimerDto,
+  HintDto,
+  TextTransform,
+} from '@epgu/epgu-constructor-types';
 import { RelativeDate } from '@epgu/ui/models/date-time';
 import { DadataResult } from '@epgu/ui/models';
 import { CustomComponentAttrValidation } from '../../../custom-screen/components-list.types';
@@ -7,6 +13,7 @@ import { ComponentBase } from '../../../../screen/screen.types';
 
 export interface IRegistrationAddrComponent extends ComponentBase {
   attrs: RegistrationAddrComponentAttrs;
+  errors?: RegAddrError[];
 }
 
 export interface IRegistrationAddrReadonlyComponent extends ComponentBase {
@@ -28,6 +35,9 @@ export interface RegistrationAddrComponentAttrs {
   hideLevels?: string[];
   hint: string;
   clarifications: Clarifications;
+  errors?: RegAddrError;
+  disclaimer?: DisclaimerDto;
+  defaultHint?: HintDto;
 }
 
 /**
@@ -81,4 +91,10 @@ export interface ConfirmAddressErrorsInterface {
   icon?: string;
   title?: string;
   type?: string;
+}
+
+export interface RegAddrError {
+  type?: string;
+  title?: string;
+  desc?: string;
 }
